45b98cf8d3b8cf263a8477b6ede1822d23d03ad5
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-07-16 13:48:55 -0400

    added comments to code and known bugs to file header

--- src/main/kotlin/widgets/LanguageSelection.kt
+++ src/main/kotlin/widgets/LanguageSelection.kt
@@ -1,9 +1,7 @@
 package widgets
 
-import com.github.thomasnield.rxkotlinfx.actionEvents
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.ObservableList
-import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.geometry.Insets
 import javafx.geometry.Pos
@@ -25,12 +23,17 @@ import tornadofx.*
 
 /**
  * This class is used to make the drop-downs for adding new
- * target or source languages
+ * target or source languages and adds highlightable and
+ * deletable buttons for each language selected
  *
  *
  *   -------- PLEASE NOTE ---------
  * This will eventually need a reference to the Profile variable
  * observables once it is made ready in the common component
+ *
+ * KNOWN BUGS:
+ * Deleting / using enter / clicking will keep / place the language related
+ * language in the ComboBox text field
  */
 
 class LanguageSelection(list : ObservableList<String>,
@@ -80,6 +83,7 @@ class LanguageSelection(list : ObservableList<String>,
 
             promptText = hint
 
+            // Find out how to shorten lambda syntax
             addEventFilter(ComboBox.ON_HIDDEN, {
                 if( input.isNotEmpty.value && list.contains(input.value) && !selectedLanguages.contains(input.value) ) {
                     selectedLanguages.add(0, input.value )
@@ -90,7 +94,7 @@ class LanguageSelection(list : ObservableList<String>,
 
         separator {}
 
-        add(fp)
+        add(fp) // should find better solution, but reference to flowplane is needed outside of root
 
         padding = Insets(40.0)
         spacing = 10.0
@@ -101,19 +105,21 @@ class LanguageSelection(list : ObservableList<String>,
 
         val background = Rectangle()
         background.fill = Paint.valueOf(UIColors.UI_PRIMARY)
+        // will need to make the dimensions dynamic
         background.arcWidth = 20.0
         background.arcHeight = 20.0
         background.width = 200.0
         background.height = 40.0
         background.effect = DropShadow(3.0, Color.DARKGRAY)
 
+        // dynamic padding?
         val label = Label(language)
         val labelHBox = HBox(label)
         labelHBox.alignment = Pos.CENTER_LEFT
         labelHBox.padding = Insets(20.0)
 
         val deleteButton = Button("X")
-        deleteButton.userData = language // pro tip
+        deleteButton.userData = language // pro tip (thanks Carl)
         deleteButton.action {
             selectedLanguages.remove( deleteButton.userData as String )
             if (background.fill == Paint.valueOf(UIColors.UI_PRIMARY) && selectedLanguages.isNotEmpty()) {
@@ -126,11 +132,14 @@ class LanguageSelection(list : ObservableList<String>,
         labelDelB.padding = Insets(10.0)
 
         val sp = StackPane(background, labelHBox, labelDelB)
+        // dynamic scaling needed
         sp.prefHeight = 40.0
         sp.prefWidth = 200.0
         if (fp.children.isNotEmpty()) {
             newSelected(language)
         }
+
+        // Find out how to shorten lambda syntax
         sp.addEventFilter(MouseEvent.MOUSE_CLICKED, EventHandler<MouseEvent> { mouseEvent -> newSelected(language)})
         return sp
     }
@@ -150,23 +159,30 @@ class LanguageSelection(list : ObservableList<String>,
     }
 
     /**
-     *
+     * Change the highlighted tag to the one most recently clicked
      */
     private fun newSelected(tag : String) {
 
         val elements = fp.children
         var rectangleReference = Rectangle()
 
-        for (nods in elements) {
-            for (nodeOut in nods.getChildList().orEmpty()) {
+        // for all objects in the flowpane
+        for (children in elements) {
 
+            // for all children in each flowplane object
+            for (nodeOut in children.getChildList().orEmpty()) {
+
+                // change the rectangle color
+                // and kep a reference
                 if (nodeOut is Rectangle) {
                     rectangleReference = nodeOut
                     nodeOut.fill = Color.WHITE
                 } else if (nodeOut is HBox) {
 
+                    // find the label and check if it equals the selected label
                     for (nodeIn in nodeOut.children) {
 
+                        // if so, then highlight the rectangle color
                         if (nodeIn is Label) {
                             if (nodeIn.text == tag) {
                                 rectangleReference.fill = Paint.valueOf(UIColors.UI_PRIMARY)
