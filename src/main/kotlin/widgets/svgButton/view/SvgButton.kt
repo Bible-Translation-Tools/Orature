48f972a854c000d735bdc14e8d9e5350ce423837
fucata <sliong325@gmail.com>
2018-07-16 22:30:58 -0400

    Added backgroundColor as parameter. Added "How to use"

--- src/main/kotlin/widgets/svgButton/view/SvgButton.kt
+++ src/main/kotlin/widgets/svgButton/view/SvgButton.kt
@@ -4,32 +4,40 @@ import afester.javafx.svg.SvgLoader
 import tornadofx.*
 import javafx.scene.Group
 import javafx.scene.layout.StackPane
+import javafx.scene.paint.Paint
 
 
-//Class to create button with SVG graphic inside
+//Class to create circle button with SVG graphic inside
 //Use "::functionName" to pass function as parameter
+//Example how to use: SvgButton(64.0, c("#CC4141"), "forwardArrowIcon", operation = ::abc)
+//For suggestions the best values to put in the parameter for certain buttons, scroll to bottom of this file
 class SvgButton(var buttonSize: Double= 64.0,
+                var myVariable: Paint = c("#ffffff"),
                 var svgFileName: String,
                 var svgScaleXCompareToButton: Double = 0.50484,
                 var svgScaleYCompareToButton: Double = 0.50484,
                 operation: () -> Unit ): StackPane() {
+
     //get resource method is adapted from: https://stackoverflow.com/questions/15749192/how-do-i-load-a-file-from-resource-folder
     var svgGroup = SvgLoader().loadSvg(Thread.currentThread().contextClassLoader.getResource("$svgFileName.svg").path)
-    init{
 
-        val newProfileIcon = button(graphic = svgGroup) {
-            importStylesheet(SvgButtonStyle:: class)
+    init {
+        val svgButton = button(graphic = svgGroup) {
+            importStylesheet(SvgButtonStyle::class)
             addClass(SvgButtonStyle.SvgIcon)
             prefWidth = buttonSize
             prefHeight = buttonSize
+            style {
+                backgroundColor += myVariable
+            }
             resizeSvg(svgGroup, buttonSize)
             action {
                 operation()
             }
         }
-        add(newProfileIcon)
-    }
 
+        add(svgButton)
+    }
 
     fun resizeSvg(svgGroup: Group, size: Double = buttonSize) {
 
@@ -37,9 +45,38 @@ class SvgButton(var buttonSize: Double= 64.0,
         val currentWidth = svgGroup.prefWidth(-1.0) // get the default preferred width
         val currentHeight = svgGroup.prefHeight(currentWidth) // get default preferred height
 
-        //Suggested (svgScaleX/svgScaleY)CompareToButton for "Create New Profile" is (0.50484/0.2734)
-        //Suggested (svgScaleX/svgScaleY)CompareToButton for "Mic Icon" is (0.3088/0.4192)
+
         svgGroup.scaleX = (size * svgScaleXCompareToButton) / currentWidth
-        svgGroup.scaleY = (size* svgScaleYCompareToButton) / currentHeight
+        svgGroup.scaleY = (size * svgScaleYCompareToButton) / currentHeight
     }
 }
+
+//How to use:
+//
+//In parent view file:
+//    1. "import widgets.svgButton.view.SvgButton"
+//    2. "add(SvgButton(UseOneOfTheValuesBelow))" inside the layout
+//
+//    For Example:
+//        1. in MainView.kt:
+//
+//                import tornadofx.*
+//                import widgets.svgButton.view.SvgButton
+//
+//                class MainView : View() {
+//                    override val root = stackpane {
+//                        add(SvgButton(64.0, c("#ffffff"), "addNewProfileIcon", 0.50484, 0.2734, operation = ::abc))
+//                    }
+//                }
+//
+//                fun abc() {
+//                    println("test")
+//                }
+
+
+//Suggested values according to design mockup https://xd.adobe.com/spec/dd9d45e2-6d29-4524-5dc0-61fe5a4e466a-abac/
+
+//"Create New Profile" button = SvgButton(64.0, c("#ffffff"), "addNewProfileIcon", 0.50484, 0.2734, operation = ::functionName)
+//"Big Mic Icon" button = SvgButton(152.68, c("#ffffff"), "micIcon", 0.3088, 0.4192, operation = ::functionName)
+//"Commit recored name" button = SvgButton(65.81, c("#CC4141"), "forwardArrowIcon", 0.2367, 0.2307, operation = ::functionName)
+
--- src/main/kotlin/widgets/svgButton/view/SvgButtonStyle.kt
+++ src/main/kotlin/widgets/svgButton/view/SvgButtonStyle.kt
@@ -4,13 +4,13 @@ import javafx.scene.effect.DropShadow
 import javafx.scene.paint.Color
 import tornadofx.*
 
+
 class SvgButtonStyle: Stylesheet() {
     companion object {
         val SvgIcon by cssclass()
     }
     init {
         SvgIcon {
-            backgroundColor += c("#ffffff")
             backgroundRadius += box(100.percent)
             borderRadius += box(100.percent)
             effect = DropShadow(10.0, Color.GRAY)
