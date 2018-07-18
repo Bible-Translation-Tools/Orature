7784595d811630d1039aabb38da8fcc6057d1ef2
Matthew Russell <mbr4477@gmail.com>
2018-07-18 16:57:53 -0400

    Removed default parameters from interface

--- src/main/kotlin/filesystem/IDirectoryProvider.kt
+++ src/main/kotlin/filesystem/IDirectoryProvider.kt
@@ -3,8 +3,8 @@ package filesystem
 interface IDirectoryProvider {
 
     // create a directory to store the user's application projects/documents
-    fun getUserDataDirectory(appendedPath: String = "", createIfNotExists: Boolean = true) : String
+    fun getUserDataDirectory(appendedPath: String, createIfNotExists: Boolean) : String
 
     // create a directory to store the application's private data
-    fun getAppDataDirectory(appendedPath: String = "", createIfNotExists: Boolean = true) : String
+    fun getAppDataDirectory(appendedPath: String, createIfNotExists: Boolean) : String
 }
