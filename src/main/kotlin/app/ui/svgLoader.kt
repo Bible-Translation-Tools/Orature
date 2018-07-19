7137caa20f53e177807785946c26b0785364a09d
fucata <sliong325@gmail.com>
2018-07-19 09:57:46 -0400

    unable nullable String and else

--- src/main/kotlin/app/ui/svgLoader.kt
+++ src/main/kotlin/app/ui/svgLoader.kt
@@ -4,4 +4,4 @@ import afester.javafx.svg.SvgLoader
 
 //function to load an svg with the svg file path located at resources folder
 
-fun svgLoader(svgFileName: String?) = SvgLoader().loadSvg(Thread.currentThread().contextClassLoader.getResource("$svgFileName.svg").path)
+fun svgLoader(svgFileName: String) = SvgLoader().loadSvg(Thread.currentThread().contextClassLoader.getResource(svgFileName).path)
