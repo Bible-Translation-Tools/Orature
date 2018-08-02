9a3ae9a633f75f4ac914c28ffe4a293e27b89ce5
Matthew Russell <mbr4477@gmail.com>
2018-08-02 16:02:07 -0400

    Removed createIfNotExists parameter and added default appended path (#26)
    * Removed createIfNotExists parameter and added default appended path
    
    * Changed return type to File; moved to persistence package

---
+++ src/main/kotlin/persistence/IDirectoryProvider.kt
@@ -0,0 +1,12 @@
+package persistence
+
+import java.io.File
+
+interface IDirectoryProvider {
+
+    // create a directory to store the user's application projects/documents
+    fun getUserDataDirectory(appendedPath: String = "") : File
+
+    // create a directory to store the application's private data
+    fun getAppDataDirectory(appendedPath: String = "") : File
+}
