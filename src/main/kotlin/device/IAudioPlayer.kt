65950c1064e3594a0d40ebbc058c1892ec4b0ed2
Acliu1498 <alexanderchen.liu@calbaptist.edu>
2018-08-07 14:55:35 -0400

    Dev audio player (#29)
    * Added Audio Player Interface
    
    * Added Audio Player Interface

---
+++ src/main/kotlin/device/IAudioPlayer.kt
@@ -0,0 +1,23 @@
+package device
+
+import io.reactivex.Completable
+import java.io.File
+
+interface IAudioPlayer {
+
+    fun load(file: File): Completable
+
+    fun play()
+
+    fun pause()
+
+    fun stop()
+
+    fun getAbsoluteDurationInFrames(): Int
+
+    fun getAbsoluteDurationMs(): Int
+
+    fun getAbsoluteLocationInFrames(): Int
+
+    fun getAbsoluteLocationMs(): Int
+}
