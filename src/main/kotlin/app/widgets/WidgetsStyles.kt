eb1397e2d6e6bd3750fc198587891d26684b5bf3
Juan Lopez <anton.lopez09@gmail.com>
2018-08-07 14:52:43 -0400

    reusable widget styles added (#149)

--- src/main/kotlin/app/widgets/WidgetsStyles.kt
+++ src/main/kotlin/app/widgets/WidgetsStyles.kt
@@ -22,79 +22,54 @@ class WidgetsStyles : Stylesheet() {
     }
 
     init {
-        rectangleButtonDefault {
-            fill = c("#CC4141")
-            backgroundColor += c("#FFF")
+        val rectangleButtonStyle = mixin {
             minWidth = 100.0.px
             cursor = Cursor.HAND
             effect = DropShadow(10.0, c(UIColorsObject.Colors["baseBackground"]))
-
             and(hover) {
                 scaleX = 1.1
                 scaleY = 1.1
             }
         }
-
-        alternateRectangleButton {
-            fill = c("#FFF")
-            backgroundColor += c("#CC4141")
-            minWidth = 100.0.px
-            cursor = Cursor.HAND
-            effect = DropShadow(10.0, Color.GRAY)
-
-            and(hover) {
-                scaleX = 1.1
-                scaleY = 1.1
-            }
-        }
-
-        ProfileIcon {
-            backgroundColor += c("#FFF")
+        val roundButtonStyle = mixin {
+            backgroundColor += Color.WHITE
             backgroundRadius += box(100.percent)
             borderRadius += box(100.percent)
             cursor = Cursor.HAND
             effect = DropShadow(10.0, Color.GRAY)
-            cursor = Cursor.HAND
-
+            borderColor += box(Color.TRANSPARENT)
             and(hover) {
                 opacity = 0.90
                 scaleX = 1.1
                 scaleY = 1.1
             }
         }
-
+        rectangleButtonDefault {
+            +rectangleButtonStyle
+            fill = c("#CC4141")
+            backgroundColor += c("#FFF")
+        }
+        alternateRectangleButton {
+            +rectangleButtonStyle
+            fill = c("#FFF")
+            backgroundColor += c("#CC4141")
+        }
+        ProfileIcon {
+            +roundButtonStyle
+        }
         roundButtonLarge {
-            backgroundRadius += box(100.percent)
-            borderRadius += box(100.percent)
-            effect = DropShadow(10.0, Color.GRAY)
+            +roundButtonStyle
             minWidth = 64.0.px
             minHeight = 64.0.px
             prefWidth = 150.0.px
             prefHeight = 150.0.px
-            cursor = Cursor.HAND
-            backgroundColor += Color.WHITE
-            borderColor += box(Color.TRANSPARENT)
-            and(hover) {
-                scaleX = 1.1
-                scaleY = 1.1
-            }
         }
-
         roundButtonMedium {
-            backgroundRadius += box(100.percent)
-            borderRadius += box(100.percent)
-            effect = DropShadow(10.0, Color.GRAY)
+            +roundButtonStyle
             minWidth = 64.0.px
             minHeight = 64.0.px
             prefWidth = 120.0.px
             prefHeight = 120.0.px
-            cursor = Cursor.HAND
-            backgroundColor += Color.WHITE
-            borderColor += box(Color.TRANSPARENT)
-            and(hover) {
-                scaleX = 1.1
-                scaleY = 1.1
-            }
         }
         roundButtonMini {
             backgroundRadius += box(100.percent)
@@ -108,8 +83,7 @@ class WidgetsStyles : Stylesheet() {
             backgroundColor += Color.WHITE
             borderColor += box(Color.TRANSPARENT)
         }
-
-        UsersListGrid{
+        UsersListGrid {
             verticalCellSpacing = 25.0.px
             backgroundColor += c("#DFDEE3")
             prefHeight = 800.0.px
@@ -117,21 +91,11 @@ class WidgetsStyles : Stylesheet() {
             maxCellsInRow = 3
             horizontalCellSpacing = 32.0.px
         }
-
         roundButton {
-            backgroundRadius += box(100.percent)
-            borderRadius += box(100.percent)
-            effect = DropShadow(10.0,  Color.GRAY)
+            +roundButtonStyle
             minWidth = 64.0.px
             minHeight = 64.0.px
-            cursor = Cursor.HAND
-            backgroundColor +=  c("#FFF")
             borderColor += box(c("#0000"))
-
-            and(hover) {
-                scaleX = 1.1
-                scaleY = 1.1
-            }
         }
         usersListCell {
             backgroundColor += Color.valueOf("#DFDEE3")
