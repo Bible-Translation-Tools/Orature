053b98a5142e7c38762893fe2cc8045a5c32862c
Nathan Shanko <natibekele@gmail.com>
2018-08-23 16:57:56 -0400

    rever project card to control

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/ProjectCard.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/ProjectCard.kt
@@ -1,26 +1,31 @@
 package org.wycliffeassociates.otter.jvm.app.widgets
 
 import javafx.geometry.Pos
+import javafx.scene.control.Button
 import javafx.scene.layout.VBox
 import org.wycliffeassociates.otter.common.data.model.Project
 import org.wycliffeassociates.otter.common.ui.widgets.IProjectCard
 import tornadofx.*
 import tornadofx.Stylesheet.Companion.root
 
-class ProjectCard(project: Project) : Fragment(), IProjectCard {
-    override val root = vbox(20) {
-        alignment = Pos.CENTER
-        label(project.book.toString())
-        label(project.targetLanguage.toString())
-        button("LOAD") {
-            action {
-                openProject()
+class ProjectCard(project: Project) : Button(), IProjectCard {
+    init {
+        with(root) {
+            vbox(20) {
+                alignment = Pos.CENTER
+                label(project.book.toString())
+                label(project.targetLanguage.toString())
+                button("LOAD") {
+                    action {
+                        openProject()
+                    }
+                }
             }
         }
-    }
+}
 
-    override fun openProject() {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-       // workspace.dock<next view>()
-    }
+override fun openProject() {
+    //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // workspace.dock<next view>()
+}
 }
