c9bf980cdda47a5dc8f28f8e9731d57e4db1d9c2
Joe <jsarabia1247@gmail.com>
2018-10-10 09:44:34 -0400

    Add getBySlugAndContainer (#52)

--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/ICollectionRepository.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/repositories/ICollectionRepository.kt
@@ -1,11 +1,14 @@
 package org.wycliffeassociates.otter.common.persistence.repositories
 
 import io.reactivex.Completable
+import io.reactivex.Maybe
 import io.reactivex.Single
 import org.wycliffeassociates.otter.common.data.model.Collection
+import org.wycliffeassociates.otter.common.data.model.ResourceMetadata
 
 interface ICollectionRepository : IRepository<Collection> {
     fun insert(collection: Collection): Single<Int>
+    fun getBySlugAndContainer(slug: String, container: ResourceMetadata): Maybe<Collection>
     fun getChildren(collection: Collection): Single<List<Collection>>
     fun updateSource(collection: Collection, newSource: Collection): Completable
     fun updateParent(collection: Collection, newParent: Collection): Completable
