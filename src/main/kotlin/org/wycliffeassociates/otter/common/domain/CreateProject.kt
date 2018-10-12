46aa842e1f6fc19c72b6a6d47a2d5c81d1faa943
natibekele <natibekele@gmail.com>
2018-10-12 09:23:36 -0400

    Ns usecases (#54)
    * add use cases, update jackson dependency
    
    * Change updateSource to return completable
    
    * correct package names
    
    * Move out of usecases package
    
    * Revert deletion
    
    * remov non-nullable
    
    * indent + change packages

---
+++ src/main/kotlin/org/wycliffeassociates/otter/common/domain/CreateProject.kt
@@ -0,0 +1,53 @@
+package org.wycliffeassociates.otter.common.domain
+
+import io.reactivex.Completable
+import io.reactivex.Single
+import org.wycliffeassociates.otter.common.data.model.Collection
+import org.wycliffeassociates.otter.common.data.model.Language
+import org.wycliffeassociates.otter.common.data.model.SourceCollection
+import org.wycliffeassociates.otter.common.persistence.repositories.ICollectionRepository
+import org.wycliffeassociates.otter.common.persistence.repositories.ILanguageRepository
+import org.wycliffeassociates.otter.common.persistence.repositories.IProjectRepository
+import org.wycliffeassociates.otter.common.persistence.repositories.ISourceRepository
+
+class CreateProject(
+        val languageRepo: ILanguageRepository,
+        val sourceRepo: ISourceRepository,
+        val collectionRepo: ICollectionRepository,
+        val projectRepo: IProjectRepository
+) {
+    fun getAllLanguages(): Single<List<Language>> {
+        return languageRepo.getAll()
+    }
+
+    fun getSourceRepos(): Single<List<Collection>> {
+        return sourceRepo.getAllRoot()
+    }
+
+    fun getAll(): Single<List<Collection>> {
+        return collectionRepo.getAll()
+    }
+
+    fun newProject(collection: Collection): Single<Int> {
+        return projectRepo.insert(collection)
+    }
+
+    fun getResourceChildren(identifier: SourceCollection): Single<List<Collection>> {
+        return sourceRepo.getChildren(identifier)
+    }
+
+    fun updateSource(projectId: Int, sourceCollection: Collection): Completable {
+        return projectRepo
+                .getAll()
+                .map {
+                    it.filter { it.id == projectId }
+                }
+                .flatMapCompletable {
+                    if (it.isNotEmpty()) {
+                        projectRepo.updateSource(it.first(), sourceCollection)
+                    } else {
+                        Completable.complete()
+                    }
+                }
+    }
+}
