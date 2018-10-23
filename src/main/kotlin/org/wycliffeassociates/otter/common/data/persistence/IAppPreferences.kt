f699ee1a4ff21479a19ed546fe0e82fa8bcb6c10
Matthew Russell <mbr4477@gmail.com>
2018-10-23 16:26:26 -0400

    Add app init value to preferences interface (#60)
    - Also removed old `AppPreferences` interface

--- src/main/kotlin/org/wycliffeassociates/otter/common/data/persistence/IAppPreferences.kt
+++
@@ -1,7 +0,0 @@
-package org.wycliffeassociates.otter.common.data.persistence
-
-// interface to getting user-independent app preferences
-interface IAppPreferences {
-    fun getCurrentUserId(): Int?
-    fun setCurrentUserId(userId: Int)
-}
--- src/main/kotlin/org/wycliffeassociates/otter/common/persistence/IAppPreferences.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/common/persistence/IAppPreferences.kt
@@ -1,7 +1,9 @@
 package org.wycliffeassociates.otter.common.persistence
 
 // interface to getting user-independent app preferences
-interface AppPreferences {
+interface IAppPreferences {
     fun getCurrentUserId(): Int?
     fun setCurrentUserId(userId: Int)
+    fun getAppInitialized(): Boolean
+    fun setAppInitialized(initialized: Boolean)
 }
