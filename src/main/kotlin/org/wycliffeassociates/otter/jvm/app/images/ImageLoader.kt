55a46f0c08995f80c7dac466e3ec3fe4c9d67500
Matthew Russell <mbr4477@gmail.com>
2018-11-19 08:34:09 -0500

    Update image loader

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/images/ImageLoader.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/images/ImageLoader.kt
@@ -4,25 +4,21 @@ import afester.javafx.svg.SvgLoader
 import javafx.scene.Node
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
-import java.io.File
+import java.io.InputStream
 
 //Loads an image with a given file path
-//How to use is described below
-fun imageLoader(imagePathToLoad: File): Node {
-    val ext: String = imagePathToLoad.extension
-    when (ext) {
-
-        //if file extension is ".svg", return a Group node
-        "svg" -> {
-            return SVGImage(SvgLoader().loadSvg(imagePathToLoad.toString()))
-        }
-
-        //if file extension is ".png" or ".jpg", return an ImageView node
-        "png", "jpg" -> return ImageView(Image(imagePathToLoad.inputStream()))
-
-        else -> {
-            println("Error: Image file extension found is not svg, png, or jpg")
-            return ImageView(/*put a default image here*/)
+class ImageLoader {
+    enum class Format {
+        SVG,
+        PNG,
+        JPG
+    }
+    companion object {
+        fun load(imageStream: InputStream, format: Format): Node {
+            return when (format) {
+                Format.SVG -> SVGImage(SvgLoader().loadSvg(imageStream))
+                Format.PNG, Format.JPG -> ImageView(Image(imageStream))
+            }
         }
     }
 }
