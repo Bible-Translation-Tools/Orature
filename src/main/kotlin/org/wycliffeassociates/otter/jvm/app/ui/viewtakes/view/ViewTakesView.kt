4fe85bdf441a4b4e83396d93c5b93ebb8277201d
Nathan Shanko <natibekele@gmail.com>
2019-02-14 16:14:18 -0500

    update view takes view layout

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/viewtakes/view/ViewTakesView.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/viewtakes/view/ViewTakesView.kt
@@ -12,11 +12,13 @@ import javafx.geometry.Pos
 import javafx.scene.Node
 import javafx.scene.control.Button
 import javafx.scene.control.ButtonType
+import javafx.scene.control.ContentDisplay
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.FlowPane
 import javafx.scene.layout.Priority
 import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
 import org.wycliffeassociates.otter.common.data.model.Take
 import org.wycliffeassociates.otter.jvm.app.theme.AppStyles
 import org.wycliffeassociates.otter.jvm.app.ui.inject.Injector
@@ -66,91 +68,76 @@ class ViewTakesView : View() {
                 topAnchor = 0.0
                 bottomAnchor = 0.0
             }
-            hbox {
-                addClass(ViewTakesStyles.headerContainer)
-                // Title label
-                label(viewModel.titleProperty) {
-                    hgrow = Priority.ALWAYS
-                    maxWidth = Double.MAX_VALUE
-                    addClass(ViewTakesStyles.viewTakesTitle)
-                    viewModel.contentProperty.toObservable().subscribe {
-                        graphic = if (it?.labelKey == "chapter") {
-                            AppStyles.chapterIcon("40px")
-                        } else { null }
-                    }
-
-                }
-
-                // Back button
-                add(JFXButton(messages["back"], AppStyles.backIcon()).apply {
-                    action { workspace.navigateBack() }
-                    isDisableVisualFocus = true
-                    addClass(AppStyles.backButton)
-                })
-            }
 
             // Top items above the alternate takes
-            // Drag target and/or selected take
-            stackpane {
-                addClass(ViewTakesStyles.selectedTakeContainer)
-
-                // drag target glow
-                stackpane {
-                    addClass(ViewTakesStyles.dragTarget, ViewTakesStyles.glow)
-                    visibleProperty().bind(draggingTakeProperty.booleanBinding { it != null })
+            // Drag target and/or selected take, Next Verse Button, Previous Verse Button
+            hbox(15.0) {
+                alignment = Pos.CENTER
+                vgrow = Priority.ALWAYS
+                button(messages["previousVerse"], AppStyles.backIcon()) {
+                    addClass(ViewTakesStyles.navigationButton)
+                    //todo create verse to verse navigation
+                    isDisable = true
                 }
-                vbox {
-                    // Check if the selected take card has changed
-                    isFillWidth = false
-                    val placeholder = vbox {
-                        addClass(ViewTakesStyles.placeholder)
-                        vgrow = Priority.NEVER
+                stackpane {
+                    addClass(ViewTakesStyles.selectedTakeContainer)
+                    // drag target glow
+                    stackpane {
+                        addClass(ViewTakesStyles.dragTarget, ViewTakesStyles.glow)
+                        visibleProperty().bind(draggingTakeProperty.booleanBinding { it != null })
                     }
+                    vbox {
+                        alignment = Pos.CENTER
+                        // Check if the selected take card has changed
+                        isFillWidth = false
+                        val placeholder = vbox {
+                            addClass(ViewTakesStyles.placeholder)
+                            vgrow = Priority.NEVER
+                        }
 
-                    // Listen for changes when the drag and drop occurs
-                    selectedTakeProperty.onChange {
-                        clear()
-                        if (it == null) {
-                            // No currently selected take
-                            add(placeholder)
-                        } else {
-                            // Add the selected take card
-                            viewModel.acceptTake(it.take)
-                            add(it)
+                        // Listen for changes when the drag and drop occurs
+                        selectedTakeProperty.onChange {
+                            clear()
+                            if (it == null) {
+                                // No currently selected take
+                                add(placeholder)
+                            } else {
+                                // Add the selected take card
+                                viewModel.acceptTake(it.take)
+                                add(it)
+                            }
+                        }
+
+                        viewModel.selectedTakeProperty.onChange {
+                            // The view model wants us to use this selected take
+                            // This take will not appear in the flow pane items
+                            if (it != null && selectedTakeProperty.value == null) {
+                                selectedTakeProperty.value = createTakeCard(it)
+                            } else if (it == null) selectedTakeProperty.value = null
                         }
                     }
 
-                    viewModel.selectedTakeProperty.onChange {
-                        // The view model wants us to use this selected take
-                        // This take will not appear in the flow pane items
-                        if (it != null && selectedTakeProperty.value == null) {
-                            selectedTakeProperty.value = createTakeCard(it)
-                        } else if (it == null) selectedTakeProperty.value = null
+                    // Create the drag target
+                    dragTarget = stackpane {
+                        addClass(ViewTakesStyles.dragTarget)
+                        add(MaterialIconView(MaterialIcon.ADD, "30px"))
+                        // Initially hide the drag target
+                        visibleProperty().bind(draggingTakeProperty.booleanBinding { it != null })
                     }
                 }
-
-                // Create the drag target
-                dragTarget = stackpane {
-                    addClass(ViewTakesStyles.dragTarget)
-                    add(MaterialIconView(MaterialIcon.ADD, "30px"))
-                    // Initially hide the drag target
-                    visibleProperty().bind(draggingTakeProperty.booleanBinding { it != null })
+                button(messages["nextVerse"], AppStyles.forwardIcon()) {
+                    addClass(ViewTakesStyles.navigationButton)
+                    contentDisplay = ContentDisplay.RIGHT
+                    //todo create verse to verse navigation
+                    isDisable = true
                 }
             }
 
             // Add the available takes flow pane
-            add(takesFlowPane)
-        }
-
-        // Record button?
-        recordButton = button("", AppStyles.recordIcon("25px")) {
-            addClass(ViewTakesStyles.recordButton)
-            anchorpaneConstraints {
-                bottomAnchor = 25.0
-                rightAnchor = 25.0
-            }
-            action {
-                viewModel.recordContent()
+            scrollpane {
+                isFitToWidth = true
+                addClass(ViewTakesStyles.scrollpane)
+                add(takesFlowPane)
             }
         }
 
@@ -234,8 +221,29 @@ class ViewTakesView : View() {
         }
     }
 
+    private fun createRecordCard(): VBox {
+        return vbox(10.0) {
+            alignment = Pos.CENTER
+            addClass(ViewTakesStyles.newTakeCard)
+            label(messages["newTake"])
+            button(messages["record"], AppStyles.recordIcon("25px")) {
+                action {
+                    viewModel.recordContent()
+                }
+            }
+        }
+    }
+
     private fun sortTakesFlowPane(flowPane: FlowPane) {
-        flowPane.children.setAll(flowPane.children.sortedBy { (it as TakeCard).take.number })
+        flowPane.children.setAll(flowPane.children.filter {
+            it is TakeCard?
+        }
+                .sortedBy {
+                    (it as TakeCard).take.number
+                }
+        )
+        //add the newTakeCard here after we have sorted all other takes by take number
+        flowPane.children.add(0, createRecordCard())
     }
 
     private fun createTakeCard(take: Take): TakeCard {
