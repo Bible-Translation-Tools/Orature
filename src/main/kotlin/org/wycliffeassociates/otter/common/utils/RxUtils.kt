3e6f970ae708b3f06b62644910c8b02e0bc32eac
Joe <jsarabia1247@gmail.com>
2019-06-04 14:28:52 -0400

    Kj resources list (#102)
    * Separate functionalities for retrieving collection resources
    
    * Chapter and chunk titles
    
    * Revert "Chapter and chunk titles"
    
    This reverts commit 799b208c880c1fc51e42a8950a9b12925630129b.
    
    * Revert "Separate functionalities for retrieving collection resources"
    
    This reverts commit bc7bc96b39a3beda97200d9254ea56222147aeed.
    
    * Create RxUtils.kt
    
    * Add kdoc to RxUtils.mapNotNull

---
+++ src/main/kotlin/org/wycliffeassociates/otter/common/utils/RxUtils.kt
@@ -0,0 +1,14 @@
+package org.wycliffeassociates.otter.common.utils
+
+import io.reactivex.Observable
+
+/**
+ * Apply the given function as with `map()`, but remove any resulting nulls from the
+ * observable stream.
+ *
+ * This is how `observable.map(f).filter { it != null }` should behave, but it keeps
+ * nulls from ever appearing in an Observable, which would cause a crash in that
+ * two-step map/filter example.
+ */
+fun <T, R:Any> Observable<T>.mapNotNull(f: (T) -> R?): Observable<R> =
+    concatMapIterable { listOfNotNull(f(it)) }
