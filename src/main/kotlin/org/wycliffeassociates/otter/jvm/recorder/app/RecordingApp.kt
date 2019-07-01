d38c145e02e877264752ba720b5402f3593a9f96
Joe <joe_sarabia@wycliffeassociates.org>
2019-07-01 15:12:22 -0400

    default cli parameters

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/recorder/app/RecordingApp.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/recorder/app/RecordingApp.kt
@@ -1,22 +1,28 @@
 package org.wycliffeassociates.otter.jvm.recorder.app
 
-import javafx.scene.text.Font
 import org.wycliffeassociates.otter.jvm.recorder.app.view.RecorderView
 import tornadofx.App
 import tornadofx.launch
 
+/**
+ * @param args array of cli arguments
+ * options include:
+ * 
+ * --wav= the filepath to write to
+ * --lang= the language name to display
+ * --book= the book name to display
+ * --chap= the text for "Chapter"
+ * --cnum= the chapter number
+ * --unit= the text for either "Verse" or "Chunk"
+ * --unum= the unit number
+ *
+ * All arguments are optional. By default the recording will be written to recording.wav and the
+ * display text will be the filename.
+ *
+ */
 fun main(args: Array<String>) {
-    launch<RecordingApp>(
-        arrayOf(
-            "--wav=hello.wav",
-            "--lang=English",
-            "--book=Matthew",
-            "--chap=Chapter",
-            "--cnum=5",
-            "--unit=Verse",
-            "--unum=10"
-        )
-    )
+    val arguments = if(args.isNotEmpty()) args else arrayOf("--wav=recording.wav")
+    launch<RecordingApp>(arguments)
 }
 
 class RecordingApp: App(RecorderView::class)
