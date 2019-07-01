916c827aab70d3d663de7b4f754380ea35e758a1
Joe <jsarabia1247@gmail.com>
2019-07-01 11:01:24 -0400

    change 8woc to otter (#462)
    * change 8woc to otter
    
    * update travis
    
    * update init sh

--- init_common.sh
+++ init_common.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Get the currently checked out branch
 jvm_branch=$BRANCH
-echo "Detected 8woc2018-jvm on $jvm_branch"
+echo "Detected otter-jvm on $jvm_branch"
 # Check if branch exists in common
-pushd ../8woc2018-common
-echo "Looking for origin/$jvm_branch in 8woc2018-common..."
+pushd ../otter-common
+echo "Looking for origin/$jvm_branch in otter-common..."
 git rev-parse --verify origin/$jvm_branch
 if [ $? -eq 0 ]; then
     # Branch exists
