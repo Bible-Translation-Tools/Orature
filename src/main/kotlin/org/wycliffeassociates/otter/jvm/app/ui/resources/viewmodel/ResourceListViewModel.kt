33a352b84a76d8ffc674aad37dc9c5574da69a56
Kevin Joslyn <KJoslyn29@gmail.com>
2019-07-29 14:55:02 -0400

    Add TornadoFXUtils.observeOnFxSafe and remove accidental function.
    observeOnFxSafe allows a way for unit tests to call functions that would normally observe on the FX thread to call those functions without observing on the FX thread.

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/resources/viewmodel/ResourceListViewModel.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/resources/viewmodel/ResourceListViewModel.kt
@@ -1,12 +1,12 @@
 package org.wycliffeassociates.otter.jvm.app.ui.resources.viewmodel
 
-import com.github.thomasnield.rxkotlinfx.observeOnFx
 import org.wycliffeassociates.otter.common.data.workbook.*
 import org.wycliffeassociates.otter.common.utils.mapNotNull
 import org.wycliffeassociates.otter.jvm.app.ui.resourcetakes.viewmodel.RecordResourceViewModel
 import org.wycliffeassociates.otter.jvm.app.ui.workbook.viewmodel.WorkbookViewModel
 import org.wycliffeassociates.otter.jvm.app.widgets.resourcecard.model.ResourceGroupCardItemList
 import org.wycliffeassociates.otter.jvm.app.widgets.resourcecard.model.resourceGroupCardItem
+import org.wycliffeassociates.otter.jvm.utils.observeOnFxSafe
 import org.wycliffeassociates.otter.jvm.utils.onChangeAndDoNow
 import tornadofx.*
 
@@ -38,7 +38,7 @@ class ResourceListViewModel : ViewModel() {
                 resourceGroupCardItem(it, workbookViewModel.resourceSlug, onSelect = this::navigateToTakesPage)
             }
             .buffer(2) // Buffering by 2 prevents the list UI from jumping while groups are loading
-            .observeOnFx()
+            .observeOnFxSafe()
             .subscribe {
                 resourceGroupCardItemList.addAll(it)
             }
--- src/main/kotlin/org/wycliffeassociates/otter/jvm/utils/TornadoFXUtils.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/utils/TornadoFXUtils.kt
@@ -1,9 +1,11 @@
 package org.wycliffeassociates.otter.jvm.utils
 
-import com.github.thomasnield.rxkotlinfx.toObservableChangesNonNull
+import com.github.thomasnield.rxkotlinfx.observeOnFx
+import io.reactivex.Observable
+import javafx.application.Platform
 import javafx.beans.value.ObservableValue
 import tornadofx.onChange
-import tornadofx.onChangeOnce
+import java.lang.IllegalStateException
 
 /**
  * Runs the given operation now and also calls tornadofx's [onChange] with the given operation to set up an
@@ -16,10 +18,32 @@ fun <T> ObservableValue<T>.onChangeAndDoNow(op: (T?) -> Unit) {
     }
 }
 
-fun <T> ObservableValue<T>.waitForNotNull(op: (T) -> Unit) {
-    this.value?.let {
-        op(it)
-    } ?: this.onChangeOnce {
-        this.waitForNotNull(op)
+/**
+ * Calls tornadofx's [observeOnFx] if JavaFX is running. Using this method instead of [observeOnFx] directly
+ * makes it safe to run unit tests when the FX platform is not running.
+ */
+fun <T> Observable<T>.observeOnFxSafe(): Observable<T> {
+    return when (IsFXInitialized.isInitialized()) {
+        true -> observeOnFx()
+        false -> this
+    }
+}
+
+private object IsFXInitialized {
+    private var mIsInitialized: Boolean? = null
+    fun isInitialized(): Boolean {
+        mIsInitialized?.let {
+            return it
+        } ?: return testIsInitialized()
+    }
+    private fun testIsInitialized(): Boolean {
+        try {
+            Platform.runLater{}
+        } catch (e: IllegalStateException) {
+            mIsInitialized = false
+            return false
+        }
+        mIsInitialized = true
+        return true
     }
 }
