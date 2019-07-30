0159a5dba8846f557bedf54ed7eff00eb06b85d4
Kevin Joslyn <KJoslyn29@gmail.com>
2019-07-30 15:49:06 -0400

    Remove unwanted entry point files

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/testapp/app/RecordableVMTestApp.kt
+++
@@ -1,9 +0,0 @@
-package org.wycliffeassociates.otter.jvm.testapp.app
-
-import tornadofx.*
-
-class RecordableVMTestApp: App(RecordableVMTestView::class) {
-}
-fun main(args: Array<String>) {
-    launch<RecordableVMTestApp>(args)
-}
