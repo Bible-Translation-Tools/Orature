22189ce34231f54e1d3759851db4e431dbfef094
Joe <joe_sarabia@wycliffeassociates.org>
2019-10-14 11:18:39 -0400

    Import Default Recorder

---
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/initialization/InitializeApp.kt
@@ -0,0 +1,84 @@
+package org.wycliffeassociates.otter.common.domain.initialization
+
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import org.wycliffeassociates.otter.common.data.audioplugin.AudioPluginData
+import org.wycliffeassociates.otter.common.domain.languages.ImportLanguages
+import org.wycliffeassociates.otter.common.domain.plugins.IAudioPluginRegistrar
+import org.wycliffeassociates.otter.common.domain.plugins.ImportAudioPlugins
+import org.wycliffeassociates.otter.common.persistence.IAppPreferences
+import org.wycliffeassociates.otter.common.persistence.IDirectoryProvider
+import org.wycliffeassociates.otter.common.persistence.repositories.IAudioPluginRepository
+import org.wycliffeassociates.otter.common.persistence.repositories.ILanguageRepository
+import org.wycliffeassociates.otter.common.persistence.repositories.ITakeRepository
+import java.io.File
+import java.io.FileOutputStream
+
+class InitializeApp(
+    val preferences: IAppPreferences,
+    val directoryProvider: IDirectoryProvider,
+    val audioPluginRegistrar: IAudioPluginRegistrar,
+    val pluginRepository: IAudioPluginRepository,
+    val languageRepo: ILanguageRepository,
+    val takeRepository: ITakeRepository
+) {
+    fun initApp(): Observable<Double> {
+        return Observable
+            .fromPublisher<Double> {
+                it.onNext(0.0)
+                it.onNext(0.25)
+
+                val initialized = preferences.appInitialized().blockingGet()
+                if (!initialized) {
+                    importLanguages()
+                    importOtterRecorder()
+                    preferences.setAppInitialized(true).blockingAwait()
+                }
+                it.onNext(0.5)
+
+                // Always import new plugins
+                ImportAudioPlugins(audioPluginRegistrar, directoryProvider)
+                    .importAll()
+                    .andThen(pluginRepository.initSelected())
+                    .blockingAwait()
+                it.onNext(0.75)
+
+                // Always clean up database
+                takeRepository
+                    .removeNonExistentTakes()
+                    .blockingAwait()
+                it.onNext(1.0)
+            }.subscribeOn(Schedulers.io())
+    }
+
+    private fun importLanguages() {
+        ImportLanguages(
+            ClassLoader.getSystemResourceAsStream("content/langnames.json"),
+            languageRepo
+        )
+            .import()
+            .onErrorComplete()
+            .blockingAwait()
+    }
+
+    private fun importOtterRecorder() {
+        val pluginsDir = directoryProvider.audioPluginDirectory
+        val jar = File(pluginsDir, "OtterRecorder.jar")
+        ClassLoader.getSystemResourceAsStream("plugins/jars/recorderapp.jar")
+            ?.transferTo(FileOutputStream(jar))
+        pluginRepository.insert(
+            AudioPluginData(
+                0,
+                "OtterRecorder",
+                "1.0.0",
+                false,
+                true,
+                jar.absolutePath,
+                listOf(),
+                null
+            )
+        ).subscribe { id: Int ->
+            preferences.setRecorderPluginId(id).blockingAwait()
+        }
+    }
+}
--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/splash/viewmodel/SplashScreenViewModel.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/splash/viewmodel/SplashScreenViewModel.kt
@@ -5,6 +5,7 @@ import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleDoubleProperty
+import org.wycliffeassociates.otter.common.domain.initialization.InitializeApp
 import org.wycliffeassociates.otter.common.domain.languages.ImportLanguages
 import org.wycliffeassociates.otter.common.domain.plugins.ImportAudioPlugins
 import org.wycliffeassociates.otter.common.navigation.TabGroupType
@@ -15,57 +16,32 @@ import org.wycliffeassociates.otter.jvm.workbookapp.ui.menu.view.MainMenu
 import tornadofx.*
 
 class SplashScreenViewModel : ViewModel() {
+    private val injector: Injector = find()
+    private val initApp = InitializeApp(
+        injector.preferences,
+        injector.directoryProvider,
+        injector.audioPluginRegistrar,
+        injector.pluginRepository,
+        injector.languageRepo,
+        injector.takeRepository
+    )
     val progressProperty = SimpleDoubleProperty(0.0)
     val shouldCloseProperty = SimpleBooleanProperty(false)
     private val chromeableStage: ChromeableStage by inject()
 
     init {
-        initApp()
-                .observeOnFx()
-                .subscribe {
-                    progressProperty.value = it
-                    if (it == 1.0) {
-                        workspace.header.removeFromParent()
-                        workspace.add(MainMenu())
-                        workspace.dock<MainScreenView>()
-                        workspace.openWindow(owner = null)
-                        chromeableStage.navigateTo(TabGroupType.PROJECT)
-                        shouldCloseProperty.value = true
-                    }
+        initApp.initApp()
+            .observeOnFx()
+            .subscribe {
+                progressProperty.value = it
+                if (it == 1.0) {
+                    workspace.header.removeFromParent()
+                    workspace.add(MainMenu())
+                    workspace.dock<MainScreenView>()
+                    workspace.openWindow(owner = null)
+                    chromeableStage.navigateTo(TabGroupType.PROJECT)
+                    shouldCloseProperty.value = true
                 }
-    }
-
-    private fun initApp(): Observable<Double> {
-        return Observable
-                .fromPublisher<Double> {
-                    it.onNext(0.0)
-                    val injector: Injector = find()
-                    it.onNext(0.25)
-
-                    val initialized = injector.preferences.appInitialized().blockingGet()
-                    if (!initialized) {
-                        // Needs initialization
-                        ImportLanguages(ClassLoader.getSystemResourceAsStream("content/langnames.json"), injector.languageRepo)
-                                .import()
-                                .onErrorComplete()
-                                .blockingAwait()
-
-                        injector.preferences.setAppInitialized(true).blockingAwait()
-                    }
-                    it.onNext(0.5)
-
-                    // Always import new plugins
-                    ImportAudioPlugins(injector.audioPluginRegistrar, injector.directoryProvider)
-                            .importAll()
-                            .andThen(injector.pluginRepository.initSelected())
-                            .blockingAwait()
-                    it.onNext(0.75)
-
-                    // Always clean up database
-                    injector.takeRepository
-                            .removeNonExistentTakes()
-                            .blockingAwait()
-                    it.onNext(1.0)
-                }.subscribeOn(Schedulers.io())
+            }
     }
 }
