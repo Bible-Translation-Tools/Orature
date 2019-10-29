da6176ecaa66c2c9eca7e59a9cca4c3761683e05
Russell Aunger <rba@live.com>
2019-10-29 04:02:07 -0400

    Write selected-takes file in export.

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/export/ProjectExporter.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/export/ProjectExporter.kt
@@ -1,9 +1,12 @@
 package org.wycliffeassociates.otter.common.domain.resourcecontainer.export
 
+import io.reactivex.Observable
 import io.reactivex.Single
+import org.wycliffeassociates.otter.common.data.workbook.Take
 import org.wycliffeassociates.otter.common.data.workbook.Workbook
 import org.wycliffeassociates.otter.common.persistence.IDirectoryProvider
 import org.wycliffeassociates.otter.common.persistence.repositories.IResourceRepository
+import org.wycliffeassociates.otter.common.utils.mapNotNull
 import java.io.BufferedWriter
 import java.io.File
 import java.time.LocalDateTime
@@ -12,6 +15,7 @@ import java.time.format.DateTimeFormatter
 const val APP_SPECIFIC_DIR = ".apps/otter"
 const val TAKE_DIR = "$APP_SPECIFIC_DIR/takes"
 const val SOURCE_DIR = "$APP_SPECIFIC_DIR/source"
+const val SELECTED_TAKES_FILE = "$APP_SPECIFIC_DIR/selected.txt"
 
 class ProjectExporter(
     private val workbook: Workbook,
@@ -30,6 +34,7 @@ class ProjectExporter(
                     writeManifest(zipWriter)
                     copyTakeFiles(zipWriter)
                     copySourceResources(zipWriter)
+                    writeSelectedTakes(zipWriter)
                 }
 
                 return@fromCallable ExportResult.SUCCESS
@@ -60,10 +65,31 @@ class ProjectExporter(
         writer.write("TODO")
     }
 
+    private fun writeSelectedTakes(zipWriter: IZipFileWriter) {
+        zipWriter.bufferedWriter(SELECTED_TAKES_FILE).use { fileWriter ->
+            fetchSelectedTakes()
+                .map(::relativeTakePath)
+                .blockingSubscribe {
+                    fileWriter.appendln(it)
+                }
+        }
+    }
+
     private fun makeExportFilename(): String {
         val lang = workbook.target.language.slug
         val project = workbook.target.slug
         val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmm"))
         return "$lang-$project-$timestamp.zip"
+    }
+
+    private fun fetchSelectedTakes(): Observable<Take> {
+        return workbook.target.chapters
+            .concatMap { chapter -> chapter.children.startWith(chapter) }
+            .mapNotNull { chapterOrChunk -> chapterOrChunk.audio.selected.value?.value }
+    }
+
+    private fun relativeTakePath(take: Take): String {
+        val relativeFile = take.file.relativeToOrSelf(projectAudioDirectory)
+        return relativeFile.invariantSeparatorsPath
     }
 }
