70ff781b866649923000dee41549a26980f289cd
Joe Sarabia <jsarabia1247@gmail.com>
2019-11-01 16:49:40 -0500

    WIP

---
+++ jvm/workbookapp/src/integration-test/kotlin/integrationtest/initialization/TestInitializeUlb.kt
@@ -0,0 +1,40 @@
+package integrationtest.initialization
+
+import io.reactivex.Completable
+import io.reactivex.observers.TestObserver
+import io.reactivex.subscribers.TestSubscriber
+import org.junit.Assert
+import org.junit.Test
+import org.wycliffeassociates.otter.assets.initialization.InitializeUlb
+
+
+class TestInitializeUlb {
+
+    @Test
+    fun testImportEnUlb() {
+        val testSub = TestObserver<Completable>()
+        val env = InitializeUlbEnvironment()
+        val inj = env.injector
+
+        val init = InitializeUlb(
+            inj.installedEntityRepository,
+            inj.resourceContainerRepository,
+            inj.directoryProvider,
+            inj.zipEntryTreeBuilder
+        )
+        init
+            .exec()
+            .subscribe(testSub)
+
+        testSub.assertComplete()
+        testSub.assertNoErrors()
+
+        Assert.assertEquals(init.version, env.db.installedEntityDao.fetchVersion(init))
+    }
+}
+
+private class InitializeUlbEnvironment : PersistenceEnvironment() {
+    override fun setUpDatabase() {
+        initLanguages()
+    }
+}
