de90c3439fba41f6e9e132306f30d2f5be631f3e
Joe Sarabia <jsarabia1247@gmail.com>
2020-01-14 10:04:32 -0500

    Add Exception Handler fix
    default tornadofx exception handler has a line commented out that would otherwise print a stacktrace to the screen; this removes that.

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/Main.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/Main.kt
@@ -5,12 +5,15 @@ import javafx.stage.StageStyle
 import org.wycliffeassociates.otter.jvm.workbookapp.di.persistence.DaggerPersistenceComponent
 import org.wycliffeassociates.otter.jvm.workbookapp.logging.ConfigureLogger
 import org.wycliffeassociates.otter.jvm.workbookapp.theme.AppStyles
+import org.wycliffeassociates.otter.jvm.workbookapp.ui.OtterExceptionHandler
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.splash.view.SplashScreen
 import tornadofx.*
+import java.lang.RuntimeException
 
 class MyApp : App(SplashScreen::class) {
     init {
         importStylesheet<AppStyles>()
+        Thread.setDefaultUncaughtExceptionHandler(OtterExceptionHandler())
     }
 
     override fun start(stage: Stage) {
