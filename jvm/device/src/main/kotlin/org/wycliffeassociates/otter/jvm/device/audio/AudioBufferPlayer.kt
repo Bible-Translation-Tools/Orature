f779777b6cf27b19c9c40048ccace5dc86dda3a0
Joe Sarabia <jsarabia1247@gmail.com>
2020-01-27 11:05:10 -0500

    Remove AudioBufferPlayer try/catch and remove lateinit
    Removed the try/catch that swallows the exception here, as this should be handled at a higher level.
    
    Removes the need for a lateinit

--- jvm/device/src/main/kotlin/org/wycliffeassociates/otter/jvm/device/audio/AudioBufferPlayer.kt
+++ jvm/device/src/main/kotlin/org/wycliffeassociates/otter/jvm/device/audio/AudioBufferPlayer.kt
@@ -1,7 +1,6 @@
 package org.wycliffeassociates.otter.jvm.device.audio
 
 import org.wycliffeassociates.otter.common.io.AudioFileReader
-import java.lang.Exception
 import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.SourceDataLine
@@ -11,24 +10,19 @@ class AudioBufferPlayer(private val reader: AudioFileReader) {
     private val bytes = ByteArray(reader.sampleRate * reader.channels)
     private var startPosition: Int = 0
 
-    private lateinit var player: SourceDataLine
+    private var player: SourceDataLine = AudioSystem.getSourceDataLine(
+        AudioFormat(
+            reader.sampleRate.toFloat(),
+            reader.sampleSize,
+            reader.channels,
+            true,
+            false
+        )
+    )
     private lateinit var playbackThread: Thread
 
     init {
-        try {
-            player = AudioSystem.getSourceDataLine(
-                    AudioFormat(
-                            reader.sampleRate.toFloat(),
-                            reader.sampleSize,
-                            reader.channels,
-                            true,
-                            false
-                    )
-            )
-            player.open()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        player.open()
     }
 
     fun play() {
