31e6559a8167effb0b8edd5ae96529da6a2f8119
Russell Aunger <rba@live.com>
2020-02-03 12:32:27 -0500

    Remove blockingGet.

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/collections/CreateProject.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/collections/CreateProject.kt
@@ -1,6 +1,7 @@
 package org.wycliffeassociates.otter.common.domain.collections
 
 import io.reactivex.Single
+import io.reactivex.rxkotlin.flatMapIterable
 import org.wycliffeassociates.otter.common.data.model.Collection
 import org.wycliffeassociates.otter.common.data.model.Language
 import org.wycliffeassociates.otter.common.persistence.repositories.ICollectionRepository
@@ -22,13 +23,23 @@ class CreateProject(
         // Find the source RC and its linked (help) RCs
         val sourceRc = sourceProject.resourceContainer
             ?: throw NullPointerException("Source project has no metadata")
-        val sourceLinkedRcs = resourceMetadataRepo.getLinked(sourceRc).blockingGet()
-        val sourceAndLinkedRcs = (sequenceOf(sourceRc) + sourceLinkedRcs)
-            .filter { resourceId == null || resourceId == it.identifier }
-            .toList()
+        val sourceLinkedRcs = resourceMetadataRepo.getLinked(sourceRc)
+            .toObservable()
+            .flatMapIterable()
+        val sourceAndLinkedRcs = sourceLinkedRcs.startWith(sourceRc)
+
+        // If a resourceId filter is requested, apply it.
+        val matchingRcs = when (resourceId) {
+            null -> sourceAndLinkedRcs
+            else -> sourceAndLinkedRcs.filter { resourceId == it.identifier }
+        }
 
         // Create derived projects for each of the sources
-        val derived = collectionRepo.deriveProject(sourceAndLinkedRcs, sourceProject, targetLanguage)
+        val derived = matchingRcs
+            .toList()
+            .flatMap {
+                collectionRepo.deriveProject(it, sourceProject, targetLanguage)
+            }
 
         return derived
     }
