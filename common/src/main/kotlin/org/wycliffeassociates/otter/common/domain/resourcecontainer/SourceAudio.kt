8d74f7f9cba2a1d603d736f3db13303000cb6f5a
Joe <jsarabia1247@gmail.com>
2020-02-19 15:25:21 -0500

    Fix reading source audio

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/SourceAudio.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/SourceAudio.kt
@@ -11,9 +11,9 @@ class SourceAudio(
     fun get(project: String, chapter: Int): File? {
         if (rc.media != null) {
             val mediaProject = rc.media!!.projects.find { it.identifier == project }
-            var media = mediaProject?.media?.find { it.identifier == ".mp3" }
+            var media = mediaProject?.media?.find { it.identifier == "mp3" }
             if (media == null) {
-                media = mediaProject?.media?.find { it.identifier == ".wav" }
+                media = mediaProject?.media?.find { it.identifier == "wav" }
             }
             if (media != null) {
                 return get(media, chapter)
@@ -23,14 +23,18 @@ class SourceAudio(
     }
 
     fun get(media: Media, chapter: Int): File? {
-        return if (rc.media != null && rc.accessor.fileExists(media.chapterUrl)) {
+        return if (rc.media != null) {
             val path = media.chapterUrl.replace("{chapter}", chapter.toString())
-            val reader = rc.accessor.getReader(path)
-            val extension = File(path).extension
-            val temp = File.createTempFile("source", extension)
-            temp.deleteOnExit()
-            reader.transferTo(temp.bufferedWriter())
-            return temp
+            if(rc.accessor.fileExists(path)) {
+                val reader = rc.accessor.getReader(path)
+                val extension = File(path).extension
+                val temp = File.createTempFile("source", ".$extension")
+                temp.deleteOnExit()
+                reader.transferTo(temp.bufferedWriter())
+                temp
+            } else {
+                null
+            }
         } else {
             null
         }
