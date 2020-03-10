2dc92c29516840cefd8df190237ba5839fb5085d
Joe Sarabia <jsarabia1247@gmail.com>
2020-03-10 16:02:37 -0400

    Fix audio plugin test

---
+++ jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/audioplugin/AudioPluginTest.kt
@@ -0,0 +1,64 @@
+package org.wycliffeassociates.otter.jvm.workbookapp.audioplugin
+
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito
+import org.wycliffeassociates.otter.common.data.PluginParameters
+import org.wycliffeassociates.otter.common.data.config.AudioPluginData
+import java.io.File
+
+class AudioPluginTest {
+
+    val parameters = Mockito.mock(PluginParameters::class.java)
+
+    var exe = File.createTempFile("test", ".exe").apply {
+        setExecutable(true)
+        deleteOnExit()
+    }
+
+    var inputAudioPluginData = AudioPluginData(
+        0,
+        "Beethoven",
+        "3.4.2",
+        true,
+        false,
+        "bash",
+        listOf("-c", "echo hello"),
+        exe
+    )
+    var missingExecutablePlugin = AudioPluginData(
+        0,
+        "Beethoven",
+        "3.4.2",
+        true,
+        false,
+        "./my-missing-executable",
+        listOf("hello"),
+        File("some/fake/file/path.yaml")
+    )
+    val inputFile = File("somefile.wav")
+
+    @Test
+    fun testCompletableFinishesForValidCommand() {
+        // Create the plugin
+        val audioPlugin = AudioPlugin(inputAudioPluginData)
+        audioPlugin
+            .launch(inputFile, parameters)
+            .blockingAwait()
+        // Test only finishes if completable finishes
+    }
+
+    @Test
+    fun testExceptionThrownWhenExecutableNotFound() {
+        // Create the plugin
+        val audioPlugin = AudioPlugin(missingExecutablePlugin)
+        try {
+            audioPlugin
+                .launch(inputFile, parameters)
+                .blockingAwait()
+            Assert.fail()
+        } catch (e: RuntimeException) {
+            // IOException thrown as RuntimeException
+        }
+    }
+}
