f7cf840edce5dcd6701625d7e6960f5c2ad1c182
Joe <jsarabia1247@gmail.com>
2020-07-24 14:39:14 -0400

    Add marker model (#137)
    * Sets up base view of the verse marker app
    
    * Linter
    
    * End with newline (linter)
    
    * Add viewmodel
    
    * Move applying CSS to top level view
    
    * Icon variable renaming
    
    * Icon renaming
    
    * Add audio controller
    
    * Add marker model
    
    Holds in progress changes to the marker state, computes next and previous marker from a given position
    
    * Linter
    
    * Update audio controller method name to seek

--- jvm/markerapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/markerapp/app/viewmodel/VerseMarkerViewModel.kt
+++ jvm/markerapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/markerapp/app/viewmodel/VerseMarkerViewModel.kt
@@ -7,6 +7,7 @@ import javafx.scene.control.Slider
 import org.wycliffeassociates.otter.common.io.wav.WavFile
 import org.wycliffeassociates.otter.jvm.controls.controllers.AudioPlayerController
 import org.wycliffeassociates.otter.jvm.device.audio.AudioBufferPlayer
+import org.wycliffeassociates.otter.jvm.markerapp.app.model.VerseMarkers
 import org.wycliffeassociates.otter.jvm.utils.onChangeAndDoNow
 import org.wycliffeassociates.otter.jvm.workbookplugin.plugin.ParameterizedScope
 import tornadofx.*
@@ -14,10 +15,10 @@ import java.io.File
 
 class VerseMarkerViewModel : ViewModel() {
 
+    val markers: VerseMarkers
     val audioPlayer = AudioBufferPlayer()
     var audioController: AudioPlayerController? = null
     val isPlayingProperty = SimpleBooleanProperty(false)
-    val markerCountProperty = SimpleIntegerProperty()
     val markerRatioProperty = SimpleStringProperty()
     val headerTitle = SimpleStringProperty()
     val headerSubtitle = SimpleStringProperty()
@@ -26,11 +27,12 @@ class VerseMarkerViewModel : ViewModel() {
         val scope = scope as ParameterizedScope
         val audioFile = File(scope.parameters.named["wav"])
         val wav = WavFile(audioFile)
-        markerCountProperty.value = wav.metadata.getCues().size
+        val initialMarkerCount = wav.metadata.getCues().size
         val totalMarkers: Int =
-            scope.parameters.named["marker_total"]?.toInt() ?: markerCountProperty.value
-        markerCountProperty.onChangeAndDoNow {
-            markerRatioProperty.value = "$it/$totalMarkers"
+            scope.parameters.named["marker_total"]?.toInt() ?: initialMarkerCount
+        markers = VerseMarkers(wav, totalMarkers)
+        markers.markerCountProperty.onChangeAndDoNow {
+            markerRatioProperty.set("${it}/$totalMarkers")
         }
         audioPlayer.load(audioFile)
     }
@@ -45,8 +47,10 @@ class VerseMarkerViewModel : ViewModel() {
     }
 
     fun seekNext() {
+        audioController?.seek(markers.seekNext(audioPlayer.getAbsoluteLocationInFrames()))
     }
 
     fun seekPrevious() {
+        audioController?.seek(markers.seekPrevious(audioPlayer.getAbsoluteLocationInFrames()))
     }
 }
