132e9f63e19e771e49956aa5a3819f1cc84e10b2
Joe <jsarabia1247@gmail.com>
2020-07-29 17:21:04 -0400

    Update WavIOTest.kt

--- common/src/test/kotlin/org/wycliffeassociates/otter/common/audio/wav/WavIOTest.kt
+++ common/src/test/kotlin/org/wycliffeassociates/otter/common/audio/wav/WavIOTest.kt
@@ -3,27 +3,36 @@ package org.wycliffeassociates.otter.common.audio.wav
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.File
+import java.io.FileInputStream
 
 class WavIOTest {
     private val testCues = listOf(
-        WavCue(123_943_347,"marker 1"),
-        WavCue(200_000_000,"marker 2"),
+        WavCue(123_943_347, "marker 1"),
+        WavCue(200_000_000, "marker 2"),
         WavCue(300_000_000, "marker 3 ")
     )
 
-    @Test
-    fun `test writing a file produces proper header`() {
-        val temp = File.createTempFile("testwav", "wav")
-        temp.deleteOnExit()
-        val wav = WavFile(temp, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
+    private fun writeDataToFile(file: File, samplesToWrite: Int, cues: List<WavCue>): WavFile {
+        val wav = WavFile(file, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
+        for (cue in cues) {
+            wav.metadata.addCue(cue.location, cue.label)
+        }
         WavOutputStream(wav).use {
-            it.write(1)
-            it.write(2)
-            it.write(3)
-            it.write(4)
+            for (i in 0 until samplesToWrite) {
+                it.write(i)
+            }
         }
         wav.update()
-        assertEquals("Audio size:", 4, wav.totalAudioLength)
+        return wav
+    }
+
+    @Test
+    fun `writing a file produces proper header`() {
+        val temp = File.createTempFile("testwav", "wav")
+        temp.deleteOnExit()
+        val audioSamples = 4
+        val wav = writeDataToFile(temp, audioSamples, listOf())
+        assertEquals("Audio size:", audioSamples, wav.totalAudioLength)
         assertEquals("Total size:", temp.length().toInt() - CHUNK_HEADER_SIZE, wav.totalDataLength)
         assertEquals("Metadata exists:", false, wav.hasMetadata)
         assertEquals("Metadata size:", 0, wav.metadata.totalSize)
@@ -31,51 +40,119 @@ class WavIOTest {
     }
 
     @Test
-    fun `test writing a metadata produces proper header`() {
+    fun `writing a file with metadata produces proper header`() {
         val temp = File.createTempFile("testwav", "wav")
         temp.deleteOnExit()
-        val wav = WavFile(temp, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
-        WavOutputStream(wav).use {
-            it.write(1)
-            it.write(2)
-            it.write(3)
-            it.write(4)
-        }
-        assertEquals("Data size", 36 + wav.totalAudioLength, wav.totalDataLength)
+        val audioSamples = 4
+        val wav = writeDataToFile(temp, audioSamples, listOf(testCues[0], testCues[1]))
+        val cues = wav.metadata.getCues()
 
-        wav.metadata.addCue(testCues[0].location, testCues[0].label)
-        wav.metadata.addCue(testCues[1].location, testCues[1].label)
-        wav.update()
+        assertEquals("Cue count:", 2, cues.size)
+        for (i in 0..1) {
+            assertEquals("Cue $i label:", testCues[i].label, cues[i].label)
+            assertEquals("Cue $i location:", testCues[i].location, cues[i].location)
+        }
 
         assertEquals("Audio size:", 4, wav.totalAudioLength)
-        assertEquals("Data size:", 36 + wav.totalAudioLength + wav.metadata.totalSize, wav.totalDataLength)
         assertEquals("Metadata exists:", true, wav.hasMetadata)
         assertEquals("Metadata size:", 112, wav.metadata.totalSize)
+        assertEquals("Total size:", temp.length().toInt() - CHUNK_HEADER_SIZE, wav.totalDataLength)
+        temp.delete()
+    }
 
-        var cues = wav.metadata.getCues()
-        assertEquals("Cue count:", 2, cues.size)
-        assertEquals("Cue 1 label", testCues[0].label, cues[0].label)
-        assertEquals("Cue 2 label", testCues[1].label, cues[1].label)
-
-        assertEquals("Cue 1 location", testCues[0].location, cues[0].location)
-        assertEquals("Cue 2 location", testCues[1].location, cues[1].location)
-
+    @Test
+    fun `add cue after initally writing file`() {
+        val temp = File.createTempFile("testwav", "wav")
+        temp.deleteOnExit()
+        val audioSamples = 4
+        val wav = writeDataToFile(temp, audioSamples, listOf(testCues[0], testCues[1]))
         wav.metadata.addCue(testCues[2].location, testCues[2].label)
-        cues = wav.metadata.getCues()
-        assertEquals("Cue count:", 3, cues.size)
-        assertEquals("Cue 1 label:", testCues[0].label, cues[0].label)
-        assertEquals("Cue 2 label:", testCues[1].label, cues[1].label)
-        assertEquals("Cue 3 label:", testCues[2].label, cues[2].label)
-
-        assertEquals("Cue 1 location:", testCues[0].location, cues[0].location)
-        assertEquals("Cue 2 location:", testCues[1].location, cues[1].location)
-        assertEquals("Cue 3 location:", testCues[2].location, cues[2].location)
         wav.update()
+        val cues = wav.metadata.getCues()
 
-        assertEquals("Audio size:", 4, wav.totalAudioLength)
+        assertEquals("Cue count:", 3, cues.size)
+        for (i in 0..2) {
+            assertEquals("Cue $i label:", testCues[i].label, cues[i].label)
+            assertEquals("Cue $i location:", testCues[i].location, cues[i].location)
+        }
+
+        assertEquals("Audio size:", audioSamples, wav.totalAudioLength)
         assertEquals("Metadata exists:", true, wav.hasMetadata)
         assertEquals("Metadata size:", 160, wav.metadata.totalSize)
         assertEquals("Total size:", temp.length().toInt() - CHUNK_HEADER_SIZE, wav.totalDataLength)
         temp.delete()
+    }
+
+    @Test
+    fun `buffered wav produces equivalent file`() {
+        val temp = File.createTempFile("testwav", "wav")
+        val temp2 = File.createTempFile("test2wav", "wav")
+        val wav = WavFile(temp, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
+        val wav2 = WavFile(temp2, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
+
+        val audioSamples = 700_000
+
+        WavOutputStream(wav, append = false, buffered = false).use {
+            for (i in 1..audioSamples) {
+                it.write(i)
+            }
+        }
+        WavOutputStream(wav2, append = false, buffered = true).use {
+            for (i in 1..audioSamples) {
+                it.write(i)
+            }
+        }
+
+        FileInputStream(temp).use { ifs ->
+            FileInputStream(temp2).use { ifs2 ->
+                val array = ifs.readAllBytes()
+                val array2 = ifs2.readAllBytes()
+                assertEquals("file size is the same", array.size, array2.size)
+                for (i in array.indices) {
+                    assertEquals(array[i], array2[i])
+                }
+            }
+        }
+        temp.delete()
+        temp2.delete()
+    }
+
+    @Test
+    fun `writing byte array buffered wav produces equivalent file`() {
+        val temp = File.createTempFile("testwav", "wav")
+        val temp2 = File.createTempFile("test2wav", "wav")
+        val wav = WavFile(temp, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
+        val wav2 = WavFile(temp2, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
+
+        val audioSamples = 700_000
+        val byteArray = ByteArray(audioSamples)
+        for (i in 0 until audioSamples) {
+            byteArray[i] = (i % 255).toByte()
+        }
+
+        WavOutputStream(wav, append = false, buffered = false).use {
+            it.write(byteArray)
+        }
+        WavOutputStream(wav2, append = false, buffered = true).use {
+            it.write(byteArray)
+        }
+
+        FileInputStream(temp).use { ifs ->
+            FileInputStream(temp2).use { ifs2 ->
+                val array = ifs.readAllBytes()
+                val array2 = ifs2.readAllBytes()
+                assertEquals("file size is the same", array.size, array2.size)
+                for (i in array.indices) {
+                    assertEquals(array[i], array2[i])
+
+                    // arrays will be offset by 44 because of the wav header
+                    if (i > 44) {
+                        assertEquals("read file matches written bytes", array[i], byteArray[i - 44])
+                    }
+                }
+            }
+        }
+        temp.delete()
+        temp2.delete()
     }
 }
