4962d79374b1ca59af1ab8f803965f6c9876b94b
Joe <jsarabia1247@gmail.com>
2020-07-29 16:36:49 -0400

    Update WavIOTest.kt

--- common/src/test/kotlin/org/wycliffeassociates/otter/common/audio/wav/WavIOTest.kt
+++ common/src/test/kotlin/org/wycliffeassociates/otter/common/audio/wav/WavIOTest.kt
@@ -5,6 +5,11 @@ import org.junit.Test
 import java.io.File
 
 class WavIOTest {
+    private val testCues = listOf(
+        WavCue(123_943_347,"marker 1"),
+        WavCue(200_000_000,"marker 2"),
+        WavCue(300_000_000, "marker 3 ")
+    )
 
     @Test
     fun `test writing a file produces proper header`() {
@@ -18,9 +23,10 @@ class WavIOTest {
             it.write(4)
         }
         wav.update()
-        assertEquals("Audio size should be 4", 4, wav.totalAudioLength)
+        assertEquals("Audio size:", 4, wav.totalAudioLength)
+        assertEquals("Total size:", temp.length().toInt() - CHUNK_HEADER_SIZE, wav.totalDataLength)
         assertEquals("Metadata exists:", false, wav.hasMetadata)
-        assertEquals("Metadata size should be 0", 0, wav.metadata.totalSize)
+        assertEquals("Metadata size:", 0, wav.metadata.totalSize)
         temp.delete()
     }
 
@@ -37,38 +43,39 @@ class WavIOTest {
         }
         assertEquals("Data size", 36 + wav.totalAudioLength, wav.totalDataLength)
 
-        wav.metadata.addCue(123_943_347,"marker 1")
-        wav.metadata.addCue(200_000_000,"marker 2")
+        wav.metadata.addCue(testCues[0].location, testCues[0].label)
+        wav.metadata.addCue(testCues[1].location, testCues[1].label)
         wav.update()
-        assertEquals("Audio size", 4, wav.totalAudioLength)
-        assertEquals("Data size", 36 + wav.totalAudioLength + wav.metadata.totalSize, wav.totalDataLength)
+
+        assertEquals("Audio size:", 4, wav.totalAudioLength)
+        assertEquals("Data size:", 36 + wav.totalAudioLength + wav.metadata.totalSize, wav.totalDataLength)
         assertEquals("Metadata exists:", true, wav.hasMetadata)
-        assertEquals("Metadata size should be ", 112, wav.metadata.totalSize)
+        assertEquals("Metadata size:", 112, wav.metadata.totalSize)
 
         var cues = wav.metadata.getCues()
-        assertEquals("Cue count should be 2:", 2, cues.size)
-        assertEquals("Cue 1 label is marker 1", "marker 1", cues[0].label)
-        assertEquals("Cue 1 label is marker 2", "marker 2", cues[1].label)
+        assertEquals("Cue count:", 2, cues.size)
+        assertEquals("Cue 1 label", testCues[0].label, cues[0].label)
+        assertEquals("Cue 2 label", testCues[1].label, cues[1].label)
 
-        assertEquals("Cue 1 location is 123_943_347", 123_943_347, cues[0].location)
-        assertEquals("Cue 1 location is 200_000_000", 200_000_000, cues[1].location)
+        assertEquals("Cue 1 location", testCues[0].location, cues[0].location)
+        assertEquals("Cue 2 location", testCues[1].location, cues[1].location)
 
-        wav.metadata.addCue(300_000_000, "marker 3")
+        wav.metadata.addCue(testCues[2].location, testCues[2].label)
         cues = wav.metadata.getCues()
-        assertEquals("Cue count should be 3:", 3, cues.size)
-        assertEquals("Cue 1 label is marker 1", "marker 1", cues[0].label)
-        assertEquals("Cue 2 label is marker 2", "marker 2", cues[1].label)
-        assertEquals("Cue 3 label is marker 3", "marker 3", cues[2].label)
+        assertEquals("Cue count:", 3, cues.size)
+        assertEquals("Cue 1 label:", testCues[0].label, cues[0].label)
+        assertEquals("Cue 2 label:", testCues[1].label, cues[1].label)
+        assertEquals("Cue 3 label:", testCues[2].label, cues[2].label)
 
-        assertEquals("Cue 1 location is 123_943_347", 123_943_347, cues[0].location)
-        assertEquals("Cue 2 location is 200_000_000", 200_000_000, cues[1].location)
-        assertEquals("Cue 3 location is 300_000_000", 300_000_000, cues[2].location)
+        assertEquals("Cue 1 location:", testCues[0].location, cues[0].location)
+        assertEquals("Cue 2 location:", testCues[1].location, cues[1].location)
+        assertEquals("Cue 3 location:", testCues[2].location, cues[2].location)
         wav.update()
 
-        assertEquals("Audio size should be 4", 4, wav.totalAudioLength)
+        assertEquals("Audio size:", 4, wav.totalAudioLength)
         assertEquals("Metadata exists:", true, wav.hasMetadata)
-        assertEquals("Metadata size should be ", 156, wav.metadata.totalSize)
-        wav.totalAudioLength
+        assertEquals("Metadata size:", 160, wav.metadata.totalSize)
+        assertEquals("Total size:", temp.length().toInt() - CHUNK_HEADER_SIZE, wav.totalDataLength)
         temp.delete()
     }
 }
