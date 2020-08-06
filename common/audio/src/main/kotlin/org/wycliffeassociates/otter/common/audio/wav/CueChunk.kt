4b44f75074806418451d7467386a3bb642764de7
Joe <jsarabia1247@gmail.com>
2020-08-06 10:19:51 -0400

    Extract audio package to audio module

--- common/audio/src/main/kotlin/org/wycliffeassociates/otter/common/audio/wav/CueChunk.kt
+++ common/audio/src/main/kotlin/org/wycliffeassociates/otter/common/audio/wav/CueChunk.kt
@@ -230,7 +230,9 @@ class CueChunk : RiffChunk {
         chunk.order(ByteOrder.LITTLE_ENDIAN)
 
         // Skip List Chunks that are not subtype "adtl"
-        if (chunk.remaining() < CHUNK_LABEL_SIZE || ADTL_LABEL != chunk.getText(CHUNK_LABEL_SIZE)) {
+        if (chunk.remaining() < CHUNK_LABEL_SIZE || ADTL_LABEL != chunk.getText(
+                CHUNK_LABEL_SIZE
+            )) {
             return
         }
 
@@ -263,13 +265,17 @@ private class CueListBuilder {
     fun addLocation(id: Int, location: Int?) {
         map[id]?.let {
             it.location = location
-        } ?: map.put(id, TempCue(location, null))
+        } ?: map.put(id,
+            TempCue(location, null)
+        )
     }
 
     fun addLabel(id: Int, label: String) {
         map[id]?.let {
             it.label = label
-        } ?: map.put(id, TempCue(null, label))
+        } ?: map.put(id,
+            TempCue(null, label)
+        )
     }
 
     fun build(): List<WavCue> {
