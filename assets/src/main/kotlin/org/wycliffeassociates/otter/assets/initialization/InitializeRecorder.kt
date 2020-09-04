51deb1b91ac4aac3fee3d693fabeecd226a9fa7c
Joe <jsarabia1247@gmail.com>
2020-09-04 11:12:58 -0400

    Fix plugin migration (#168)
    * Update RecordingApp.kt
    
    * Update AudioPluginDao.kt

--- assets/src/main/kotlin/org/wycliffeassociates/otter/assets/initialization/InitializeRecorder.kt
+++ assets/src/main/kotlin/org/wycliffeassociates/otter/assets/initialization/InitializeRecorder.kt
@@ -19,7 +19,7 @@ class InitializeRecorder(
 ) : Installable {
 
     override val name = "RECORDER"
-    override val version = 1
+    override val version = 2
 
     val log = LoggerFactory.getLogger(InitializeRecorder::class.java)
 
@@ -54,7 +54,7 @@ class InitializeRecorder(
             AudioPluginData(
                 0,
                 "OratureRecorder",
-                "1.0.0",
+                "$version.0.0",
                 canEdit = false,
                 canRecord = true,
                 executable = jar.absolutePath,
--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/database/daos/AudioPluginDao.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/database/daos/AudioPluginDao.kt
@@ -1,7 +1,9 @@
 package org.wycliffeassociates.otter.jvm.workbookapp.persistence.database.daos
 
 import jooq.Tables.AUDIO_PLUGIN_ENTITY
+import org.jooq.Constraint
 import org.jooq.DSLContext
+import org.jooq.impl.DSL
 import org.jooq.impl.DSL.max
 import org.wycliffeassociates.otter.jvm.workbookapp.persistence.database.InsertionException
 import org.wycliffeassociates.otter.jvm.workbookapp.persistence.entities.AudioPluginEntity
@@ -13,28 +15,48 @@ class AudioPluginDao(
     fun insert(entity: AudioPluginEntity, dsl: DSLContext = instanceDsl): Int {
         if (entity.id != 0) throw InsertionException("Entity ID is not 0")
 
-        // Insert the plugin entity
-        dsl
-            .insertInto(
-                AUDIO_PLUGIN_ENTITY,
-                AUDIO_PLUGIN_ENTITY.NAME,
-                AUDIO_PLUGIN_ENTITY.VERSION,
-                AUDIO_PLUGIN_ENTITY.BIN,
-                AUDIO_PLUGIN_ENTITY.ARGS,
-                AUDIO_PLUGIN_ENTITY.EDIT,
-                AUDIO_PLUGIN_ENTITY.RECORD,
-                AUDIO_PLUGIN_ENTITY.PATH
-            )
-            .values(
-                entity.name,
-                entity.version,
-                entity.bin,
-                entity.args,
-                entity.edit,
-                entity.record,
-                entity.path
-            )
-            .execute()
+        // find an existing plugin matching the name, in which we'll update
+        val existing = dsl.select(AUDIO_PLUGIN_ENTITY.ID)
+            .from(AUDIO_PLUGIN_ENTITY)
+            .where(AUDIO_PLUGIN_ENTITY.NAME.eq(entity.name))
+            .fetchOne()
+
+        if (existing != null) {
+            val id = existing.getValue(AUDIO_PLUGIN_ENTITY.ID)
+            dsl
+                .update(AUDIO_PLUGIN_ENTITY)
+                .set(AUDIO_PLUGIN_ENTITY.VERSION, entity.version)
+                .set(AUDIO_PLUGIN_ENTITY.BIN, entity.bin)
+                .set(AUDIO_PLUGIN_ENTITY.ARGS, entity.args)
+                .set(AUDIO_PLUGIN_ENTITY.EDIT, entity.edit)
+                .set(AUDIO_PLUGIN_ENTITY.RECORD, entity.record)
+                .set(AUDIO_PLUGIN_ENTITY.PATH, entity.path)
+                .where(AUDIO_PLUGIN_ENTITY.ID.eq(id))
+                .execute()
+        } else {
+            // Insert the plugin entity
+            dsl
+                .insertInto(
+                    AUDIO_PLUGIN_ENTITY,
+                    AUDIO_PLUGIN_ENTITY.NAME,
+                    AUDIO_PLUGIN_ENTITY.VERSION,
+                    AUDIO_PLUGIN_ENTITY.BIN,
+                    AUDIO_PLUGIN_ENTITY.ARGS,
+                    AUDIO_PLUGIN_ENTITY.EDIT,
+                    AUDIO_PLUGIN_ENTITY.RECORD,
+                    AUDIO_PLUGIN_ENTITY.PATH
+                )
+                .values(
+                    entity.name,
+                    entity.version,
+                    entity.bin,
+                    entity.args,
+                    entity.edit,
+                    entity.record,
+                    entity.path
+                )
+                .execute()
+        }
 
         // Fetch and return the resulting ID
         return dsl
