480f2e48c8dbda46fcadce8d7c685ae08d85b678
Joe <jsarabia1247@gmail.com>
2020-09-04 11:03:45 -0400

    Add modal for version to menu (#167)
    * Add modal for version to menu
    
    * Update MainMenu.kt
    
    * Update AppVersionView.kt

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/menu/view/MainMenu.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/menu/view/MainMenu.kt
@@ -12,6 +12,7 @@ import org.wycliffeassociates.otter.jvm.workbookapp.ui.addplugin.view.AddPluginV
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.menu.viewmodel.MainMenuViewModel
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.removeplugins.view.RemovePluginsView
 import org.wycliffeassociates.otter.jvm.controls.progressdialog.progressdialog
+import org.wycliffeassociates.otter.jvm.workbookapp.controls.dialogs.AppVersionView
 import tornadofx.*
 import tornadofx.FX.Companion.messages
 
@@ -81,6 +82,12 @@ class MainMenu : MenuBar() {
                             viewModel.exportProject(it)
                         }
                     }
+                separator()
+                item(messages["version"]) {
+                    setOnAction {
+                        find<AppVersionView>().openModal()
+                    }
+                }
             }
             menu(messages["audioPlugins"]) {
                 onShowing = EventHandler {
