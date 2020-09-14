9ea4ac4268a740de5818a34ab8c18e3b93e8c40f
Joe <jsarabia1247@gmail.com>
2020-09-14 15:10:44 -0400

    Add logging for ResourceMetadataRepository

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/repositories/ResourceMetadataRepository.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/repositories/ResourceMetadataRepository.kt
@@ -4,45 +4,62 @@ import io.reactivex.Completable
 import io.reactivex.Maybe
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import org.slf4j.LoggerFactory
 import org.wycliffeassociates.otter.common.data.model.ResourceMetadata
 import org.wycliffeassociates.otter.common.persistence.repositories.IResourceMetadataRepository
 import org.wycliffeassociates.otter.jvm.workbookapp.persistence.database.AppDatabase
 import org.wycliffeassociates.otter.jvm.workbookapp.persistence.entities.ResourceMetadataEntity
 import org.wycliffeassociates.otter.jvm.workbookapp.persistence.repositories.mapping.LanguageMapper
 import org.wycliffeassociates.otter.jvm.workbookapp.persistence.repositories.mapping.ResourceMetadataMapper
+import kotlin.math.log
 
 class ResourceMetadataRepository(
     database: AppDatabase,
     private val metadataMapper: ResourceMetadataMapper = ResourceMetadataMapper(),
     private val languageMapper: LanguageMapper = LanguageMapper()
 ) : IResourceMetadataRepository {
+    private val logger = LoggerFactory.getLogger(ResourceMetadataRepository::class.java)
+
     private val resourceMetadataDao = database.resourceMetadataDao
     private val languageDao = database.languageDao
 
     override fun exists(metadata: ResourceMetadata): Single<Boolean> {
-        return Single.fromCallable {
-            val languageFk = languageDao.fetchBySlug(metadata.language.slug).id
-            resourceMetadataDao.exists(languageFk, metadata.identifier, metadata.version, metadata.creator)
-        }.subscribeOn(Schedulers.io())
+        return Single
+            .fromCallable {
+                val languageFk = languageDao.fetchBySlug(metadata.language.slug).id
+                resourceMetadataDao.exists(languageFk, metadata.identifier, metadata.version, metadata.creator)
+            }
+            .doOnError { e ->
+                logger.error("Error in exists for metadata: $metadata", e)
+            }
+            .subscribeOn(Schedulers.io())
     }
 
     override fun get(metadata: ResourceMetadata): Single<ResourceMetadata> {
-        return Single.fromCallable {
-            val languageEntity = languageDao.fetchBySlug(metadata.language.slug)
-            val metadataEntity = resourceMetadataDao.fetch(
-                languageEntity.id,
-                metadata.identifier,
-                metadata.version,
-                metadata.creator
-            )
-            metadataMapper.mapFromEntity(metadataEntity, languageMapper.mapFromEntity(languageEntity))
-        }.subscribeOn(Schedulers.io())
+        return Single
+            .fromCallable {
+                val languageEntity = languageDao.fetchBySlug(metadata.language.slug)
+                val metadataEntity = resourceMetadataDao.fetch(
+                    languageEntity.id,
+                    metadata.identifier,
+                    metadata.version,
+                    metadata.creator
+                )
+                metadataMapper.mapFromEntity(metadataEntity, languageMapper.mapFromEntity(languageEntity))
+            }
+            .doOnError { e ->
+                logger.error("Error in get for metadata: $metadata", e)
+            }
+            .subscribeOn(Schedulers.io())
     }
 
     override fun insert(metadata: ResourceMetadata): Single<Int> {
         return Single
             .fromCallable {
                 resourceMetadataDao.insert(metadataMapper.mapToEntity(metadata))
+            }
+            .doOnError { e ->
+                logger.error("Error in insert for metadata: $metadata", e)
             }
             .subscribeOn(Schedulers.io())
     }
@@ -54,6 +71,9 @@ class ResourceMetadataRepository(
                     .fetchAll()
                     .map(this::buildMetadata)
             }
+            .doOnError { e ->
+                logger.error("Error in getAll", e)
+            }
             .subscribeOn(Schedulers.io())
     }
 
@@ -63,6 +83,9 @@ class ResourceMetadataRepository(
                 resourceMetadataDao.fetchById(metadata.id).derivedFromFk
             }
             .map { buildMetadata(resourceMetadataDao.fetchById(it)) }
+            .doOnError { e ->
+                logger.error("Error in getSource for metadata: $metadata", e)
+            }
             .subscribeOn(Schedulers.io())
     }
 
@@ -73,6 +96,9 @@ class ResourceMetadataRepository(
                     .fetchLinks(metadata.id)
                     .map(this::buildMetadata)
             }
+            .doOnError { e ->
+                logger.error("Error in getLinked for metadata: $metadata", e)
+            }
             .subscribeOn(Schedulers.io())
     }
 
@@ -81,6 +107,9 @@ class ResourceMetadataRepository(
             .fromAction {
                 val updated = metadataMapper.mapToEntity(metadata, source?.id)
                 resourceMetadataDao.update(updated)
+            }
+            .doOnError { e ->
+                logger.error("Error in updateSource for metadata: $metadata, source: $source", e)
             }
             .subscribeOn(Schedulers.io())
     }
@@ -92,6 +121,9 @@ class ResourceMetadataRepository(
                 val updated = metadataMapper.mapToEntity(obj, existing.derivedFromFk)
                 resourceMetadataDao.update(updated)
             }
+            .doOnError { e ->
+                logger.error("Error in update for metadata: $obj", e)
+            }
             .subscribeOn(Schedulers.io())
     }
 
@@ -99,6 +131,9 @@ class ResourceMetadataRepository(
         return Completable
             .fromAction {
                 resourceMetadataDao.addLink(firstMetadata.id, secondMetadata.id)
+            }
+            .doOnError { e ->
+                logger.error("Error in addLink for first: $firstMetadata, second: $secondMetadata", e)
             }
             .subscribeOn(Schedulers.io())
     }
@@ -108,6 +143,9 @@ class ResourceMetadataRepository(
             .fromAction {
                 resourceMetadataDao.removeLink(firstMetadata.id, secondMetadata.id)
             }
+            .doOnError { e ->
+                logger.error("Error in removeLink for first: $firstMetadata, second: $secondMetadata", e)
+            }
             .subscribeOn(Schedulers.io())
     }
 
@@ -115,6 +153,9 @@ class ResourceMetadataRepository(
         return Completable
             .fromAction {
                 resourceMetadataDao.delete(metadataMapper.mapToEntity(obj))
+            }
+            .doOnError { e ->
+                logger.error("Error in delete for metadata: $obj", obj)
             }
             .subscribeOn(Schedulers.io())
     }
