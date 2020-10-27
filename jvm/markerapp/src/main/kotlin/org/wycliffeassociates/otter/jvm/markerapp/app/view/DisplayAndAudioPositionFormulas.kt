8297a203ef06e1fed7651784fe172991ad15416c
Joe <jsarabia1247@gmail.com>
2020-10-27 15:23:20 -0400

    Update DisplayAndAudioPositionFormulas.kt

--- jvm/markerapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/markerapp/app/view/DisplayAndAudioPositionFormulas.kt
+++ jvm/markerapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/markerapp/app/view/DisplayAndAudioPositionFormulas.kt
@@ -1,11 +1,11 @@
 package org.wycliffeassociates.otter.jvm.markerapp.app.view
 
-const val epsilon = 10
-const val MS_IN_SECOND = 1000
+private const val EPSILON = 10
+private const val MS_IN_SECOND = 1000
 
 internal fun pixelsInSecond(width: Double, durationMs: Int): Int {
     val msInPixels = durationMs / width
-    return (MS_IN_SECOND / msInPixels + epsilon).toInt()
+    return (MS_IN_SECOND / msInPixels + EPSILON).toInt()
 }
 
 internal fun positionToMs(x: Int, width: Double, durationMs: Int): Int {
