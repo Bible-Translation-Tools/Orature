90dcd758e1a0376713230652ec53c0153b63d057
Joe <jsarabia1247@gmail.com>
2020-12-14 16:17:15 -0500

    Update DisplayAndAudioPositionFormulas.kt

--- jvm/markerapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/markerapp/app/view/DisplayAndAudioPositionFormulas.kt
+++ jvm/markerapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/markerapp/app/view/DisplayAndAudioPositionFormulas.kt
@@ -1,10 +1,13 @@
 package org.wycliffeassociates.otter.jvm.markerapp.app.view
 
 import com.sun.glass.ui.Screen
+import org.wycliffeassociates.otter.common.audio.wav.DEFAULT_SAMPLE_RATE
 import org.wycliffeassociates.otter.jvm.markerapp.app.viewmodel.SECONDS_ON_SCREEN
 
 private const val EPSILON = 10
 private const val MS_IN_SECOND = 1000
+private const val SAMPLE_RATE = DEFAULT_SAMPLE_RATE
+
 
 internal fun pixelsInSecond(width: Double, durationMs: Int): Int {
     val msInPixels = durationMs / width
@@ -17,15 +20,13 @@ internal fun positionToMs(x: Int, width: Double, durationMs: Int): Int {
 }
 
 internal fun pixelsToFrames(pixels: Double): Int {
-    val framerate = 44100
-    val framesOnScreen = SECONDS_ON_SCREEN * framerate
+    val framesOnScreen = SECONDS_ON_SCREEN * SAMPLE_RATE
     val framesInPixel = framesOnScreen / Screen.getMainScreen().platformWidth.toDouble()
     return (pixels * framesInPixel).toInt()
 }
 
 internal fun framesToPixels(frames: Int): Int {
-    val framerate = 44100
-    val framesOnScreen = SECONDS_ON_SCREEN * framerate
+    val framesOnScreen = SECONDS_ON_SCREEN * SAMPLE_RATE
     val framesInPixel = framesOnScreen / Screen.getMainScreen().platformWidth.toDouble()
     return (frames / framesInPixel).toInt()
 }
