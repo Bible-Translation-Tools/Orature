60ae4527f47032dec1ee8c0066bb9748a78622a8
Joe <jsarabia1247@gmail.com>
2021-02-10 15:55:27 -0500

    wip

--- jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/takemanagement/viewmodel/RecordScriptureViewModelTest.kt
+++ jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/takemanagement/viewmodel/RecordScriptureViewModelTest.kt
@@ -1,67 +1,67 @@
-package org.wycliffeassociates.otter.jvm.workbookapp.ui.takemanagement.viewmodel
-
-import com.jakewharton.rxrelay2.ReplayRelay
-import javafx.beans.property.SimpleObjectProperty
-import org.junit.Test
-import org.wycliffeassociates.otter.common.data.model.ContentType
-import org.wycliffeassociates.otter.common.data.model.MimeType
-import org.wycliffeassociates.otter.common.data.workbook.*
-import org.wycliffeassociates.otter.jvm.utils.onChangeAndDoNow
-import tornadofx.*
-import java.io.File
-import java.time.LocalDate
-
-class RecordScriptureViewModelTest {
-
-    private val activeChunk = Chunk(
-        sort = 1,
-        audio = createAssociatedAudio(),
-        textItem = TextItem("Chunk 1", MimeType.USFM),
-        start = 1,
-        end = 1,
-        contentType = ContentType.TEXT,
-        resources = listOf(),
-        label = "Chunk"
-    )
-
-    private var selectedTake: Take?
-        get() = activeChunk.audio.selected.value?.value
-        set(take) {
-            activeChunk.audio.selected.accept(TakeHolder(take))
-        }
-
-    var selectedTakeProperty = SimpleObjectProperty<Take?>()
-    init {
-        activeChunk.audio.selected.value?.value?.getProperty(RecordScriptureViewModelTest::selectedTakeProperty)
-    }
-
-    private fun createAssociatedAudio() = AssociatedAudio(ReplayRelay.create<Take>())
-
-    @Test
-    fun test() {
-        val take1 = Take(
-            "testTake1.wav",
-            File("testTake1.wav"),
-            1,
-            MimeType.WAV,
-            LocalDate.now()
-        )
-        val take2 = Take(
-            "testTake2.wav",
-            File("testTake2.wav"),
-            2,
-            MimeType.WAV,
-            LocalDate.now()
-        )
-
-        selectedTakeProperty.onChangeAndDoNow {
-            println("From on change: ${selectedTake?.name}")
-        }
-
-        selectedTake = take1
-        println("Hardcode 1: ${selectedTake?.name}")
-
-        selectedTake = take2
-        println("Hardcode 2: ${selectedTake?.name}")
-    }
-}
+//package org.wycliffeassociates.otter.jvm.workbookapp.ui.takemanagement.viewmodel
+//
+//import com.jakewharton.rxrelay2.ReplayRelay
+//import javafx.beans.property.SimpleObjectProperty
+//import org.junit.Test
+//import org.wycliffeassociates.otter.common.data.model.ContentType
+//import org.wycliffeassociates.otter.common.data.model.MimeType
+//import org.wycliffeassociates.otter.common.data.workbook.*
+//import org.wycliffeassociates.otter.jvm.utils.onChangeAndDoNow
+//import tornadofx.*
+//import java.io.File
+//import java.time.LocalDate
+//
+//class RecordScriptureViewModelTest {
+//
+//    private val activeChunk = Chunk(
+//        sort = 1,
+//        audio = createAssociatedAudio(),
+//        textItem = TextItem("Chunk 1", MimeType.USFM),
+//        start = 1,
+//        end = 1,
+//        contentType = ContentType.TEXT,
+//        resources = listOf(),
+//        label = "Chunk"
+//    )
+//
+//    private var selectedTake: Take?
+//        get() = activeChunk.audio.selected.value?.value
+//        set(take) {
+//            activeChunk.audio.selected.accept(TakeHolder(take))
+//        }
+//
+//    var selectedTakeProperty = SimpleObjectProperty<Take?>()
+//    init {
+//        activeChunk.audio.selected.value?.value?.getProperty(RecordScriptureViewModelTest::selectedTakeProperty)
+//    }
+//
+//    private fun createAssociatedAudio() = AssociatedAudio(ReplayRelay.create<Take>())
+//
+//    @Test
+//    fun test() {
+//        val take1 = Take(
+//            "testTake1.wav",
+//            File("testTake1.wav"),
+//            1,
+//            MimeType.WAV,
+//            LocalDate.now()
+//        )
+//        val take2 = Take(
+//            "testTake2.wav",
+//            File("testTake2.wav"),
+//            2,
+//            MimeType.WAV,
+//            LocalDate.now()
+//        )
+//
+//        selectedTakeProperty.onChangeAndDoNow {
+//            println("From on change: ${selectedTake?.name}")
+//        }
+//
+//        selectedTake = take1
+//        println("Hardcode 1: ${selectedTake?.name}")
+//
+//        selectedTake = take2
+//        println("Hardcode 2: ${selectedTake?.name}")
+//    }
+//}
