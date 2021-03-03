7353d752bf1cbc02fd7117fbdfca90532c36a6a6
Joe <jsarabia1247@gmail.com>
2021-03-03 13:59:45 -0500

    Separates main from the app class (#246)

---
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/Main.kt
@@ -0,0 +1,8 @@
+package org.wycliffeassociates.otter.jvm.workbookapp
+
+import org.wycliffeassociates.otter.jvm.workbookapp.ui.OtterApp
+import tornadofx.launch
+
+fun main(args: Array<String>) {
+    launch<OtterApp>(args)
+}
