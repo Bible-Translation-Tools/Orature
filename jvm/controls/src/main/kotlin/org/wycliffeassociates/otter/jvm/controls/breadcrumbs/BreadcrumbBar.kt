6091679a565c92b9ef2efd45c3fa0c80fb412ac9
mxaln <murmax82@gmail.com>
2021-03-29 15:16:54 -0400

    Added BreadcrumbBar

---
+++ jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/breadcrumbs/BreadcrumbBar.kt
@@ -0,0 +1,41 @@
+package org.wycliffeassociates.otter.jvm.controls.breadcrumbs
+
+import javafx.scene.layout.HBox
+import tornadofx.*
+
+class BreadcrumbBar: HBox() {
+
+    internal val items = observableListOf<BreadCrumb>()
+
+    init {
+        importStylesheet(javaClass.getResource("/css/breadcrumb-bar.css").toExternalForm())
+        styleClass.setAll("breadcrumb-bar")
+
+        items.onChange {
+            children.clear()
+
+            it.list.forEach { item ->
+                item.isActiveProperty.set(false)
+                children.add(item)
+            }
+
+            it.list.lastOrNull()?.isActiveProperty?.set(true)
+        }
+    }
+
+    fun addItem(item: BreadCrumb) {
+        if (items.contains(item).not()) {
+            items.add(item)
+        }
+        removeItemAfter(item)
+    }
+
+    fun removeItem(item: BreadCrumb) {
+        items.remove(item)
+    }
+
+    private fun removeItemAfter(item: BreadCrumb) {
+        val fromIndex = items.indexOf(item) + 1
+        items.remove(fromIndex, items.size)
+    }
+}
