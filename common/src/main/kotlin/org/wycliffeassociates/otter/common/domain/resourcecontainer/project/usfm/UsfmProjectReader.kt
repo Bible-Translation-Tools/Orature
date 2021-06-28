284bff19f52e26a443e55cef910eba00f92832c9
Joe <jsarabia1247@gmail.com>
2021-06-28 16:51:46 -0400

    Update UsfmProjectReader.kt

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/usfm/UsfmProjectReader.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/usfm/UsfmProjectReader.kt
@@ -178,7 +178,11 @@ fun Marker.getTextBlocks(): MutableList<TextBlock> {
         when (marker) {
             is FMarker -> continue
             is TextBlock -> textBlocks.add(marker)
-            else -> marker.getChildMarkers(TextBlock::class.java).forEach { textBlocks.addAll(it.getTextBlocks()) }
+            else -> {
+                marker
+                    .getChildMarkers(TextBlock::class.java)
+                    .forEach { textBlocks.addAll(it.getTextBlocks()) }
+            }
         }
     }
     return textBlocks
