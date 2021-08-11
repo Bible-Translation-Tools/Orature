013c919dacbf67dbcfc8a47927e2d582d336c59e
anonymouswalker <hoanganhtran1998@gmail.com>
2021-08-11 14:43:06 -0500

    remove comment

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/artwork/ImagesDataSource.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/artwork/ImagesDataSource.kt
@@ -3,9 +3,6 @@ package org.wycliffeassociates.otter.common.domain.resourcecontainer.artwork
 import org.wycliffeassociates.otter.common.data.primitives.ResourceMetadata
 import java.io.File
 
-/*
- * Chain of Responsibility - Base handler class
- */
 interface ImagesDataSource {
     fun getImage(
         metadata: ResourceMetadata,
