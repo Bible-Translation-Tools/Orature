7577a4e922015dc2e2bcd55092e26322052aed45
Maxim <murmax82@gmail.com>
2021-09-08 17:55:50 +0400

    Update dateModified of the take file when promoted (#350)
    * Update modifiedData of the take file when promoted

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/screens/ChapterPage.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/screens/ChapterPage.kt
@@ -249,6 +249,7 @@ class ChapterPage : Fragment() {
 
     private fun onTakeSelected(chunk: CardData, take: TakeModel) {
         chunk.chunkSource?.audio?.selectTake(take.take)
+        take.take.file.setLastModified(System.currentTimeMillis())
     }
 
     private fun getPlayer(): IAudioPlayer {
