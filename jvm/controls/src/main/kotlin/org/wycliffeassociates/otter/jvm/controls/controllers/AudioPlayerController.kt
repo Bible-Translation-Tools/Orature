401c401f058811f71981a69e8cd7c9a9181ddddc
Joe <jsarabia1247@gmail.com>
2021-09-09 11:42:19 -0400

    Bugfix: glitch on pause/play button
    isPlaying is tied to listeners, and actively setting them on button presses causes some conflict. This removes the explicit setting on playback state and instead defers completely to listeners.

--- jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/controllers/AudioPlayerController.kt
+++ jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/controllers/AudioPlayerController.kt
@@ -54,11 +54,9 @@ class AudioPlayerController(
         disposable?.dispose()
         player?.let { _player ->
             if (_player.isPlaying()) {
-                isPlayingProperty.set(false)
                 pause()
             } else {
                 disposable = startProgressUpdate()
-                isPlayingProperty.set(true)
                 play()
                 _player.addEventListener {
                     if (
@@ -76,7 +74,6 @@ class AudioPlayerController(
                         }
                     }
                 }
-                isPlayingProperty.set(true)
             }
         }
     }
