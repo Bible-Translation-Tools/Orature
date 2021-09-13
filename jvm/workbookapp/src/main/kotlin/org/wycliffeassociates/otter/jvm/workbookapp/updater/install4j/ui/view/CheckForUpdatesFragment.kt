bb2a1eb4c2956187a83f3ec93d48affaf3160901
Joe <jsarabia1247@gmail.com>
2021-09-13 13:30:10 -0400

    In app install4j update (#348)
    Adds an in app updater via Install4j

---
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/updater/install4j/ui/view/CheckForUpdatesFragment.kt
@@ -0,0 +1,45 @@
+package org.wycliffeassociates.otter.jvm.workbookapp.updater.install4j.ui.view
+
+import com.jfoenix.controls.JFXButton
+import org.wycliffeassociates.otter.jvm.workbookapp.ui.system.AppInfo
+import org.wycliffeassociates.otter.jvm.workbookapp.updater.install4j.ui.viewmodel.AppUpdaterViewModel
+import tornadofx.*
+
+class CheckForUpdatesFragment : Fragment() {
+
+    val vm: AppUpdaterViewModel by inject()
+    val info = AppInfo()
+
+    override val root = vbox {
+        fitToParentSize()
+
+        styleClass.add("app-drawer__section")
+
+        visibleProperty().bind(vm.showCheckForUpdate)
+        managedProperty().bind(visibleProperty())
+
+        vbox {
+            styleClass.addAll("app-drawer__version", "app-drawer__section")
+            label(messages["currentVersion"]) {
+                styleClass.add("app-drawer__subtitle")
+            }
+            label("${info.getVersion()}") {
+                styleClass.add("app-drawer__text")
+            }
+            label(messages["updateFailedNoInternet"]) {
+                styleClass.addAll("app-drawer__text", "app-drawer__text--error")
+                visibleProperty().bind(vm.showOffline)
+                managedProperty().bind(visibleProperty())
+            }
+        }
+
+        add(
+            JFXButton(messages["checkForUpdates"]).apply {
+                styleClass.addAll("btn", "btn--secondary")
+                setOnAction {
+                    vm.checkForUpdates()
+                }
+            }
+        )
+    }
+}
---
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/updater/install4j/ui/viewmodel/AppUpdaterViewModel.kt
@@ -0,0 +1,86 @@
+package org.wycliffeassociates.otter.jvm.workbookapp.updater.install4j.ui.viewmodel
+
+import java.io.FileNotFoundException
+import javafx.application.Platform
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import org.slf4j.LoggerFactory
+import org.wycliffeassociates.otter.jvm.workbookapp.updater.install4j.AppUpdater
+import org.wycliffeassociates.otter.jvm.workbookapp.updater.install4j.UpdateLauncher
+import org.wycliffeassociates.otter.jvm.workbookapp.updater.install4j.UpdateProgressListener
+import tornadofx.ViewModel
+
+class AppUpdaterViewModel : ViewModel() {
+
+    private val logger = LoggerFactory.getLogger(AppUpdaterViewModel::class.java)
+
+    val showOffline = SimpleBooleanProperty(false)
+    val showCheckForUpdate = SimpleBooleanProperty(true)
+    val showUpdateAvailable = SimpleBooleanProperty(false)
+    val showUpdateDownloading = SimpleBooleanProperty(false)
+    val showUpdateCompleted = SimpleBooleanProperty(false)
+    val showUpdateScheduled = SimpleBooleanProperty(false)
+    val showNoUpdatesAvailable = SimpleBooleanProperty(false)
+
+    val percentCompleteProperty = SimpleIntegerProperty()
+    val statusMessageProperty = SimpleStringProperty()
+    val detailedMessageProperty = SimpleStringProperty()
+
+    val updateUrlText = SimpleStringProperty()
+    val updateSize = SimpleStringProperty()
+    val updateVersion = SimpleStringProperty()
+
+    val progressListener = UpdateProgressListener(
+        percentCompletedProperty = percentCompleteProperty,
+        statusMessageProperty = statusMessageProperty,
+        detailMessageProperty = detailedMessageProperty
+    )
+    val updater = AppUpdater()
+    val launcher = UpdateLauncher(progressListener)
+
+    fun checkForUpdates() {
+        try {
+            showOffline.set(false)
+            val result = updater.checkUpdate()
+            showCheckForUpdate.set(false)
+            if (result.possibleUpdateEntry != null) {
+                showUpdateAvailable.set(true)
+                updateVersion.set(result.possibleUpdateEntry.newVersion)
+                updateSize.set(result.possibleUpdateEntry.fileSizeVerbose)
+                updateUrlText.set(result.possibleUpdateEntry.url.toString())
+            } else {
+                showNoUpdatesAvailable.set(true)
+            }
+        } catch (e: FileNotFoundException) {
+            logger.error("Error checking for updates, config file not found.", e)
+        } catch (e: Exception) {
+            logger.error("Error checking for updates.", e)
+            showOffline.set(true)
+        }
+    }
+
+    fun downloadUpdate() {
+        showUpdateAvailable.set(false)
+        showUpdateDownloading.set(true)
+        launcher.update {
+            Platform.runLater {
+                showUpdateDownloading.set(false)
+                showUpdateCompleted.set(true)
+            }
+        }
+    }
+
+    fun updateAndRestart() {
+        updater.updateAndRestart()
+    }
+
+    fun updateLater() {
+        showUpdateCompleted.set(false)
+        showUpdateScheduled.set(true)
+    }
+
+    fun applyScheduledUpdate() {
+        updater.updateAndRestart()
+    }
+}
