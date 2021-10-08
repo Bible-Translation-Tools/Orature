b1c99bbd7bb9c06d364c8e307bae18bebb71d7a6
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-10-08 08:39:57 -0400

    missing reset in SimpleAudioPlayer

--- jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/media/SimpleAudioPlayer.kt
+++ jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/media/SimpleAudioPlayer.kt
@@ -95,7 +95,7 @@ class SimpleAudioPlayer(
     private fun initController() {
         playerProperty.onChange {
             it?.let {
-                audioPlayerController.load(it)
+                audioPlayerController.load(it, true)
             }
         }
 
