f9296ca458ed064bf0e92a07abc9f93db4615aaa
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-12-08 16:28:37 -0500

    HomePageVM - load translations

--- jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/viewmodel/HomePageViewModelTest.kt
+++ jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/viewmodel/HomePageViewModelTest.kt
@@ -1,16 +1,24 @@
 package org.wycliffeassociates.otter.jvm.workbookapp.ui.viewmodel
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.times
 import io.reactivex.Maybe
 import io.reactivex.Single
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
-import org.mockito.Mockito.*
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.atLeastOnce
+import org.mockito.Mockito.verify
 import org.testfx.api.FxToolkit
 import org.wycliffeassociates.otter.common.data.primitives.Collection
+import org.wycliffeassociates.otter.common.data.primitives.Language
+import org.wycliffeassociates.otter.common.data.workbook.Translation
 import org.wycliffeassociates.otter.common.data.workbook.Workbook
 import org.wycliffeassociates.otter.common.persistence.repositories.IAppPreferencesRepository
 import org.wycliffeassociates.otter.common.persistence.repositories.ICollectionRepository
+import org.wycliffeassociates.otter.common.persistence.repositories.ILanguageRepository
 import org.wycliffeassociates.otter.common.persistence.repositories.IWorkbookRepository
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.viewmodel.Utilities.Companion.notifyListenerExecuted
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.viewmodel.Utilities.Companion.waitForListenerExecution
@@ -22,6 +30,7 @@ class HomePageViewModelTest {
     private val mockCollectionRepo = mock(ICollectionRepository::class.java)
     private val mockWorkbookRepo = mock(IWorkbookRepository::class.java)
     private val mockSettingsVM = mock(SettingsViewModel::class.java)
+    private val mockLanguageRepo = mock(ILanguageRepository::class.java)
 
     init {
         FxToolkit.registerPrimaryStage()
@@ -36,6 +45,7 @@ class HomePageViewModelTest {
         vm.preferencesRepository = mockPreferenceRepo
         vm.collectionRepo = mockCollectionRepo
         vm.workbookRepo = mockWorkbookRepo
+        vm.languageRepository = mockLanguageRepo
     }
 
     private val resumeProjectId = 1
@@ -77,5 +87,57 @@ class HomePageViewModelTest {
 
         verify(mockSettingsVM).refreshPlugins()
         verify(mockWorkbook)
+    }
+
+    private val languages = listOf(
+        Language(
+            "en-test",
+            "English-test",
+            "English-test",
+            "ltr",
+            true,
+            "NA",
+            1
+        ),
+        Language(
+            "ar-test",
+            "Arabic-test",
+            "Arabic-test",
+            "rtl",
+            true,
+            "ME",
+            2
+        )
+    )
+
+    private val translations = listOf(
+        Translation(languages[0], languages[1], null, 0),
+        Translation(languages[1], languages[0], null, 1)
+    )
+
+    @Test
+    fun loadTranslation() {
+        val mockProject = mock(Workbook::class.java)
+        `when`(mockWorkbookRepo.getProjects(any()))
+            .thenReturn(Single.just(listOf(mockProject)))
+        `when`(mockLanguageRepo.getAllTranslations())
+            .thenReturn(Single.just(translations))
+
+        assertEquals(0, vm.translations.size)
+        assertEquals(0, vm.translationModels.size)
+
+        val lockObject = Object()
+        vm.translations.onChange {
+            notifyListenerExecuted(lockObject)
+        }
+        vm.loadTranslations()
+
+        waitForListenerExecution(lockObject) {
+            assertEquals(2, vm.translations.size)
+            assertEquals(2, vm.translationModels.size)
+        }
+
+        verify(mockLanguageRepo).getAllTranslations()
+        verify(mockWorkbookRepo, atLeastOnce()).getProjects(any())
     }
 }
