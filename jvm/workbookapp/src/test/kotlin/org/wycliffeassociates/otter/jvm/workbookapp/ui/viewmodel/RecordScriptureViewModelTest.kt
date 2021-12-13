464cf7b1a2103483212cdac6af613942a7f19aab
mxaln <murmax82@gmail.com>
2021-12-13 06:51:18 -0500

    Monocle parameters

--- jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/viewmodel/RecordScriptureViewModelTest.kt
+++ jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/viewmodel/RecordScriptureViewModelTest.kt
@@ -136,10 +136,10 @@ class RecordScriptureViewModelTest {
 
         @BeforeClass
         @JvmStatic fun setup() {
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
+            /*System.setProperty("testfx.robot", "glass")
+            System.setProperty("testfx.headless", "true")
+            System.setProperty("prism.order", "sw")
+            System.setProperty("prism.text", "t2k")*/
 
             FxToolkit.registerPrimaryStage()
             FxToolkit.setupApplication { testApp }
