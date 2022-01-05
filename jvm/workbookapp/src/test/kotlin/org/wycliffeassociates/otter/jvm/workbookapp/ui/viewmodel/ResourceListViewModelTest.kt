d1c7806acd8321b4f4ba16d0f4061ba49c1eaa92
mxaln <murmax82@gmail.com>
2022-01-05 09:59:20 -0500

    Fixed tests

--- jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/viewmodel/ResourceListViewModelTest.kt
+++ jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/viewmodel/ResourceListViewModelTest.kt
@@ -24,10 +24,10 @@ import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Observable
-import java.io.File
-import java.time.LocalDate
 import org.junit.Assert
+import org.junit.BeforeClass
 import org.junit.Test
+import org.testfx.api.FxToolkit
 import org.wycliffeassociates.otter.common.data.primitives.ContainerType
 import org.wycliffeassociates.otter.common.data.primitives.ContentType
 import org.wycliffeassociates.otter.common.data.primitives.Language
@@ -38,63 +38,199 @@ import org.wycliffeassociates.otter.common.data.workbook.Chapter
 import org.wycliffeassociates.otter.common.data.workbook.Chunk
 import org.wycliffeassociates.otter.common.data.workbook.Resource
 import org.wycliffeassociates.otter.common.data.workbook.ResourceGroup
-import org.wycliffeassociates.otter.common.data.workbook.Take
 import org.wycliffeassociates.otter.common.data.workbook.TextItem
+import org.wycliffeassociates.otter.jvm.device.ConfigureAudioSystem
 import tornadofx.*
+import java.io.File
+import java.time.LocalDate
 
-class ResourceListViewModelTest : ViewModel() {
+class ResourceListViewModelTest {
 
-    private val testApp = TestApp()
-    private val resourceListViewModel: ResourceListViewModel
-    private val workbookDataStore: WorkbookDataStore
-    private val recordResourceViewModel: RecordResourceViewModel
+    companion object {
+        private val testApp = TestApp()
+        private lateinit var resourceListViewModel: ResourceListViewModel
+        private lateinit var workbookDataStore: WorkbookDataStore
+        private lateinit var recordResourceViewModel: RecordResourceViewModel
 
-    init {
-        FX.setApplication(FX.defaultScope, testApp)
-        resourceListViewModel = find()
-        workbookDataStore = find()
-        recordResourceViewModel = find()
-    }
+        private val english = Language(
+            "en",
+            "English",
+            "English",
+            "ltr",
+            isGateway = true,
+            region = "Europe"
+        )
+        private val resourceMetadataTn = ResourceMetadata(
+            conformsTo = "rc0.2",
+            creator = "Door43 World Missions Community",
+            description = "Description",
+            format = "text/markdown",
+            identifier = "tn",
+            issued = LocalDate.now(),
+            language = english,
+            modified = LocalDate.now(),
+            publisher = "unfoldingWord",
+            subject = "Translator Notes",
+            type = ContainerType.Help,
+            title = "translationNotes",
+            version = "1",
+            license = "",
+            path = File(".")
+        )
 
-    private val english = Language(
-        "en",
-        "English",
-        "English",
-        "ltr",
-        isGateway = true,
-        region = "Europe"
-    )
-    private val resourceMetadataTn = ResourceMetadata(
-        conformsTo = "rc0.2",
-        creator = "Door43 World Missions Community",
-        description = "Description",
-        format = "text/markdown",
-        identifier = "tn",
-        issued = LocalDate.now(),
-        language = english,
-        modified = LocalDate.now(),
-        publisher = "unfoldingWord",
-        subject = "Translator Notes",
-        type = ContainerType.Help,
-        title = "translationNotes",
-        version = "1",
-        license = "",
-        path = File(".")
-    )
+        private val chapterResourceGroup = ResourceGroup(
+            resourceMetadataTn,
+            Observable.fromIterable(
+                listOf(
+                    Resource(
+                        title = createTitleComponent(1, "gen_1_s1"),
+                        body = null
+                    ),
+                    Resource(
+                        title = createTitleComponent(3, "gen_1_s3"),
+                        body = createBodyComponent(4, "gen_1_s4")
+                    ),
+                    Resource(
+                        title = createTitleComponent(5, "gen_1_s5"),
+                        body = createBodyComponent(6, "gen_1_s6")
+                    )
+                )
+            )
+        )
 
-    init {
-        workbookDataStore.activeResourceMetadataProperty.set(resourceMetadataTn)
+        private val chunk1ResourceGroup = ResourceGroup(
+            resourceMetadataTn,
+            Observable.fromIterable(
+                listOf(
+                    Resource(
+                        title = createTitleComponent(1, "gen_1_v1_s1"),
+                        body = null
+                    ),
+                    Resource(
+                        title = createTitleComponent(3, "gen_1_v1_s3"),
+                        body = createBodyComponent(4, "gen_1_v1_s4")
+                    )
+                )
+            )
+        )
+
+        private val chunk2ResourceGroup = ResourceGroup(
+            resourceMetadataTn,
+            Observable.fromIterable(
+                listOf(
+                    Resource(
+                        title = createTitleComponent(1, "gen_1_v2_s1"),
+                        body = createBodyComponent(2, "gen_1_v2_s2")
+                    ),
+                    Resource(
+                        title = createTitleComponent(3, "gen_1_v2_s3"),
+                        body = createBodyComponent(4, "gen_1_v2_s4")
+                    ),
+                    Resource(
+                        title = createTitleComponent(5, "gen_1_v2_s5"),
+                        body = createBodyComponent(6, "gen_1_v2_s6")
+                    )
+                )
+            )
+        )
+
+        private val chunk1 = Chunk(
+            sort = 1,
+            audio = createAssociatedAudio(),
+            textItem = TextItem("Chunk 1", MimeType.USFM),
+            start = 1,
+            end = 1,
+            contentType = ContentType.TEXT,
+            resources = listOf(chunk1ResourceGroup),
+            label = "Chunk"
+        )
+
+        private val chunk2 = Chunk(
+            sort = 2,
+            audio = createAssociatedAudio(),
+            textItem = TextItem("Chunk 2", MimeType.USFM),
+            start = 2,
+            end = 2,
+            contentType = ContentType.TEXT,
+            resources = listOf(chunk2ResourceGroup),
+            label = "Chunk"
+        )
+
+        private val testChapter = Chapter(
+            sort = 1,
+            title = "gen_1",
+            audio = createAssociatedAudio(),
+            subtreeResources = listOf(resourceMetadataTn),
+            resources = listOf(chapterResourceGroup),
+            chunks = Observable.fromIterable(
+                listOf(
+                    chunk1,
+                    chunk2
+                )
+            ),
+            label = "Chapter"
+        )
+
+        private val testResourceNoBody = Resource(
+            title = createTitleComponent(1, "gen_1_v1_s1"),
+            body = null
+        )
+
+        private val testResourceWithBody = Resource(
+            title = createTitleComponent(1, "gen_1_v1_s1"),
+            body = createTitleComponent(2, "gen_1_v1_s2")
+        )
+
+        private fun getResourceGroupSize(idx: Int): Long {
+            return resourceListViewModel.resourceGroupCardItemList[idx].resources.count().blockingGet()
+        }
+
+        private fun createAssociatedAudio() = AssociatedAudio(ReplayRelay.create())
+        private fun createTitleComponent(sort: Int, title: String) = Resource.Component(
+            sort,
+            TextItem(title, MimeType.MARKDOWN),
+            createAssociatedAudio(),
+            ContentType.TITLE,
+            "Title"
+        )
+        private fun createBodyComponent(sort: Int, title: String) = Resource.Component(
+            sort,
+            TextItem(title, MimeType.MARKDOWN),
+            createAssociatedAudio(),
+            ContentType.BODY,
+            "Body"
+        )
+
+        @BeforeClass
+        @JvmStatic fun setup() {
+            FxToolkit.registerPrimaryStage()
+            FxToolkit.setupApplication { testApp }
+
+            val configureAudio = ConfigureAudioSystem(
+                testApp.dependencyGraph.injectConnectionFactory(),
+                testApp.dependencyGraph.injectAudioDeviceProvider(),
+                testApp.dependencyGraph.injectAppPreferencesRepository()
+            )
+            configureAudio.configure()
+
+            resourceListViewModel = find()
+
+            workbookDataStore = find()
+            workbookDataStore.activeResourceMetadataProperty.set(resourceMetadataTn)
+
+            recordResourceViewModel = find()
+        }
     }
 
     @Test
-    fun setActiveChunkAndRecordable_setsBookElement() {
+    fun `setActiveChunkAndRecordables sets BookElement`() {
         resourceListViewModel.setActiveChunkAndRecordables(chunk1, testResourceNoBody)
 
         Assert.assertEquals(chunk1, workbookDataStore.activeChunkProperty.value)
     }
 
     @Test
-    fun setActiveChunkAndRecordable_callsSetRecordableListItems() {
+    fun `setActiveChunkAndRecordable calls setRecordableListItems`() {
         val spiedRecordResourceViewModel = spy(recordResourceViewModel)
         val spiedResourcesViewModel = spy(resourceListViewModel)
         whenever(spiedResourcesViewModel.recordResourceViewModel).thenReturn(spiedRecordResourceViewModel)
@@ -117,141 +253,13 @@ class ResourceListViewModelTest : ViewModel() {
     }
 
     @Test
-    fun testLoadResourceGroups_putsAppropriateGroupsInList() {
+    fun `test LoadResourceGroups puts appropriate groups in list`() {
         resourceListViewModel.loadResourceGroups(testChapter)
-        Assert.assertEquals(3, resourceListViewModel.resourceGroupCardItemList.size)
 
         Assert.assertEquals(3, getResourceGroupSize(0))
         Assert.assertEquals(2, getResourceGroupSize(1))
         Assert.assertEquals(3, getResourceGroupSize(2))
+
+        Assert.assertEquals(3, resourceListViewModel.resourceGroupCardItemList.size)
     }
-
-    /**
-     *
-     *      Private Methods
-     *
-     */
-
-    private val testResourceNoBody = Resource(
-        title = createTitleComponent(1, "gen_1_v1_s1"),
-        body = null
-    )
-
-    private val testResourceWithBody = Resource(
-        title = createTitleComponent(1, "gen_1_v1_s1"),
-        body = createTitleComponent(2, "gen_1_v1_s2")
-    )
-
-    private fun getResourceGroupSize(idx: Int): Long {
-        return resourceListViewModel.resourceGroupCardItemList[idx].resources.count().blockingGet()
-    }
-
-    private fun createAssociatedAudio() = AssociatedAudio(ReplayRelay.create<Take>())
-    private fun createTitleComponent(sort: Int, title: String) = Resource.Component(
-        sort,
-        TextItem(title, MimeType.MARKDOWN),
-        createAssociatedAudio(),
-        ContentType.TITLE,
-        "Title"
-    )
-    private fun createBodyComponent(sort: Int, title: String) = Resource.Component(
-        sort,
-        TextItem(title, MimeType.MARKDOWN),
-        createAssociatedAudio(),
-        ContentType.BODY,
-        "Body"
-    )
-
-    private val chapterResourceGroup = ResourceGroup(
-        resourceMetadataTn,
-        Observable.fromIterable(
-            listOf(
-                Resource(
-                    title = createTitleComponent(1, "gen_1_s1"),
-                    body = null
-                ),
-                Resource(
-                    title = createTitleComponent(3, "gen_1_s3"),
-                    body = createBodyComponent(4, "gen_1_s4")
-                ),
-                Resource(
-                    title = createTitleComponent(5, "gen_1_s5"),
-                    body = createBodyComponent(6, "gen_1_s6")
-                )
-            )
-        )
-    )
-
-    private val chunk1ResourceGroup = ResourceGroup(
-        resourceMetadataTn,
-        Observable.fromIterable(
-            listOf(
-                Resource(
-                    title = createTitleComponent(1, "gen_1_v1_s1"),
-                    body = null
-                ),
-                Resource(
-                    title = createTitleComponent(3, "gen_1_v1_s3"),
-                    body = createBodyComponent(4, "gen_1_v1_s4")
-                )
-            )
-        )
-    )
-
-    private val chunk2ResourceGroup = ResourceGroup(
-        resourceMetadataTn,
-        Observable.fromIterable(
-            listOf(
-                Resource(
-                    title = createTitleComponent(1, "gen_1_v2_s1"),
-                    body = createBodyComponent(2, "gen_1_v2_s2")
-                ),
-                Resource(
-                    title = createTitleComponent(3, "gen_1_v2_s3"),
-                    body = createBodyComponent(4, "gen_1_v2_s4")
-                ),
-                Resource(
-                    title = createTitleComponent(5, "gen_1_v2_s5"),
-                    body = createBodyComponent(6, "gen_1_v2_s6")
-                )
-            )
-        )
-    )
-
-    private val chunk1 = Chunk(
-        sort = 1,
-        audio = createAssociatedAudio(),
-        textItem = TextItem("Chunk 1", MimeType.USFM),
-        start = 1,
-        end = 1,
-        contentType = ContentType.TEXT,
-        resources = listOf(chunk1ResourceGroup),
-        label = "Chunk"
-    )
-
-    private val chunk2 = Chunk(
-        sort = 2,
-        audio = createAssociatedAudio(),
-        textItem = TextItem("Chunk 2", MimeType.USFM),
-        start = 2,
-        end = 2,
-        contentType = ContentType.TEXT,
-        resources = listOf(chunk2ResourceGroup),
-        label = "Chunk"
-    )
-
-    private val testChapter = Chapter(
-        sort = 1,
-        title = "gen_1",
-        audio = createAssociatedAudio(),
-        subtreeResources = listOf(resourceMetadataTn),
-        resources = listOf(chapterResourceGroup),
-        chunks = Observable.fromIterable(
-            listOf(
-                chunk1,
-                chunk2
-            )
-        ),
-        label = "Chapter"
-    )
 }
