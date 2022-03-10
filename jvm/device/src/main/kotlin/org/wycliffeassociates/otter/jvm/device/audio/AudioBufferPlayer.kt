97b921a1aaaf2dd3e1172eed04a6de04cd16f80b
Joe <jsarabia1247@gmail.com>
2022-03-10 08:27:45 -0500

    Update AudioBufferPlayer.kt

--- jvm/device/src/main/kotlin/org/wycliffeassociates/otter/jvm/device/audio/AudioBufferPlayer.kt
+++ jvm/device/src/main/kotlin/org/wycliffeassociates/otter/jvm/device/audio/AudioBufferPlayer.kt
@@ -23,6 +23,7 @@ import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.sound.sampled.LineUnavailableException
 import javax.sound.sampled.SourceDataLine
+import kotlin.math.abs
 import org.wycliffeassociates.otter.common.audio.AudioFile
 import org.wycliffeassociates.otter.common.audio.AudioFileReader
 import org.wycliffeassociates.otter.common.device.AudioPlayerEvent
@@ -116,7 +117,8 @@ class AudioBufferPlayer(
                             player.start()
                             while (_reader.hasRemaining() && !pause.get() && !playbackThread.isInterrupted) {
                                 synchronized(monitor) {
-                                    val adjustedPlaybackRate = processor.playbackRate == 1.0
+                                    // if the User did not change the playback rate, skip playback rate processing
+                                    val adjustedPlaybackRate = abs(processor.playbackRate - 1.0) < 0.0001
                                     if (!adjustedPlaybackRate && _reader.framePosition > bytes.size / 2) {
                                         _reader.seek(_reader.framePosition - processor.overlap)
                                     }
