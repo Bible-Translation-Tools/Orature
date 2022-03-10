7911934354db913d11d93ce0e1d0e6dd242f6758
Maksim Murzamuratov <murzamuratovm@wa.wycliffe.org>
2022-03-10 17:20:28 +0400

    Replace MappedByteBuffer with DirectByteBuffer to prevent file locks

--- common/audio/src/main/kotlin/org/wycliffeassociates/otter/common/audio/wav/WavFileReader.kt
+++ common/audio/src/main/kotlin/org/wycliffeassociates/otter/common/audio/wav/WavFileReader.kt
@@ -21,12 +21,9 @@ package org.wycliffeassociates.otter.common.audio.wav
 import org.slf4j.LoggerFactory
 import org.wycliffeassociates.otter.common.audio.AudioFileReader
 import java.io.RandomAccessFile
-import java.lang.Exception
-import java.lang.IllegalStateException
 import java.lang.Integer.max
 import java.lang.Integer.min
 import java.nio.ByteBuffer
-import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
 
 internal class WavFileReader(val wav: WavFile, val start: Int? = null, val end: Int? = null) : AudioFileReader {
@@ -38,23 +35,21 @@ internal class WavFileReader(val wav: WavFile, val start: Int? = null, val end: 
     override val channels: Int = wav.channels
     override val sampleSize: Int = wav.bitsPerSample
     override val framePosition: Int
-        get() = (mappedFile?.position() ?: 0) / wav.frameSizeInBytes
+        get() = (byteBuffer?.position() ?: 0) / wav.frameSizeInBytes
 
-    private var mappedFile: MappedByteBuffer? = null
+    private var byteBuffer: ByteBuffer? = null
     private var channel: FileChannel? = null
 
     override fun open() {
-        mappedFile?.let { release() }
+        byteBuffer?.let { release() }
         val (begin, end) = computeBounds(wav)
-        mappedFile =
-            RandomAccessFile(wav.file, "r").use {
-                channel = it.channel
-                channel!!.map(
-                    FileChannel.MapMode.READ_ONLY,
-                    begin.toLong(),
-                    (end - begin).toLong()
-                )
-            }
+        byteBuffer = RandomAccessFile(wav.file, "r").use {
+            val buffer = ByteBuffer.allocateDirect(end - begin)
+            channel = it.channel
+            channel?.read(buffer)
+            buffer.position(0)
+            buffer.asReadOnlyBuffer()
+        }
     }
 
     fun computeBounds(wav: WavFile): Pair<Int, Int> {
@@ -87,7 +82,7 @@ internal class WavFileReader(val wav: WavFile, val start: Int? = null, val end: 
     }
 
     override fun getPcmBuffer(bytes: ByteArray): Int {
-        mappedFile?.let { _mappedFile ->
+        byteBuffer?.let { _mappedFile ->
             val written = _mappedFile.remaining().coerceAtMost(bytes.size)
             _mappedFile.get(bytes, 0, written)
             return written
@@ -98,7 +93,7 @@ internal class WavFileReader(val wav: WavFile, val start: Int? = null, val end: 
 
     @Throws(ArrayIndexOutOfBoundsException::class)
     override fun seek(sample: Int) {
-        mappedFile?.let { _mappedFile ->
+        byteBuffer?.let { _mappedFile ->
             val index = min(wav.sampleIndex(sample), _mappedFile.limit())
             _mappedFile.position(index)
         } ?: run {
@@ -107,25 +102,13 @@ internal class WavFileReader(val wav: WavFile, val start: Int? = null, val end: 
     }
 
     override fun hasRemaining(): Boolean {
-        return mappedFile?.hasRemaining() ?: throw IllegalStateException("hasRemaining called before opening file")
+        return byteBuffer?.hasRemaining() ?: throw IllegalStateException("hasRemaining called before opening file")
     }
 
     override fun release() {
-        if (mappedFile != null) {
-            try {
-                // https://stackoverflow.com/questions/25238110/how-to-properly-close-mappedbytebuffer/25239834#25239834
-                // TODO: Replace with https://docs.oracle.com/en/java/javase/14/docs/api/jdk.incubator.foreign/jdk/incubator/foreign/MemorySegment.html#ofByteBuffer(java.nio.ByteBuffer)
-                val unsafeClass = Class.forName("sun.misc.Unsafe")
-                val unsafeField = unsafeClass.getDeclaredField("theUnsafe")
-                unsafeField.isAccessible = true
-                val unsafe: Any = unsafeField.get(null)
-                val invokeCleaner = unsafeClass.getMethod("invokeCleaner", ByteBuffer::class.java)
-                invokeCleaner.invoke(unsafe, mappedFile)
-            } catch (e: Exception) {
-                logger.error("Error releasing memory mapped file: ${wav.file.name}", e)
-            }
+        if (byteBuffer != null) {
             channel?.close()
-            mappedFile = null
+            byteBuffer = null
             channel = null
             System.gc()
         }
