25e2a365247e5df718a451b2d1883a05a281c516
Joe <jsarabia1247@gmail.com>
2022-03-16 13:15:45 -0400

    Update InitializeMarker.kt

--- assets/src/main/kotlin/org/wycliffeassociates/otter/assets/initialization/InitializeMarker.kt
+++ assets/src/main/kotlin/org/wycliffeassociates/otter/assets/initialization/InitializeMarker.kt
@@ -39,7 +39,7 @@ class InitializeMarker @Inject constructor(
 ) : Installable {
 
     override val name = "MARKER"
-    override val version = 195
+    override val version = 19
 
     val log = LoggerFactory.getLogger(InitializeMarker::class.java)
 
