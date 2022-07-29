9ff3245a286406bcddf8848bfd733f92900767aa
anonymouswalker <hoanganhtran1998@gmail.com>
2022-07-29 13:10:01 -0500

    recorder version

--- assets/src/main/kotlin/org/wycliffeassociates/otter/assets/initialization/InitializeRecorder.kt
+++ assets/src/main/kotlin/org/wycliffeassociates/otter/assets/initialization/InitializeRecorder.kt
@@ -39,7 +39,7 @@ class InitializeRecorder @Inject constructor(
 ) : Installable {
 
     override val name = "RECORDER"
-    override val version = 23
+    override val version = 24
 
     val log = LoggerFactory.getLogger(InitializeRecorder::class.java)
 
