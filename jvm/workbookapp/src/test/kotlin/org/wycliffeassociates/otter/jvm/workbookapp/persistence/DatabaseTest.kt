0491d76ffa40f79752c2d6e0d616a3f4326fa3cf
anonymouswalker <hoanganhtran1998@gmail.com>
2022-08-16 14:23:22 -0500

    test when multiple db exist in different locations

---
+++ jvm/workbookapp/src/test/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/DatabaseTest.kt
@@ -0,0 +1,84 @@
+package org.wycliffeassociates.otter.jvm.workbookapp.persistence
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import jooq.tables.InstalledEntity
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import org.junit.After
+import org.junit.Assert
+import org.junit.Test
+import org.sqlite.SQLiteDataSource
+import org.wycliffeassociates.otter.jvm.workbookapp.persistence.database.AppDatabase
+import org.wycliffeassociates.otter.jvm.workbookapp.persistence.database.DATABASE_INSTALLABLE_NAME
+import java.io.File
+import kotlin.io.path.createTempDirectory
+
+class DatabaseTest {
+
+    private val databaseDir = createTempDirectory("otter-database").toFile()
+    private val databaseFile = databaseDir.resolve("test-db-v8.sqlite")
+        .apply { deleteOnExit() }
+    private val databaseArchiveDir = databaseDir.resolve("archive").apply(File::mkdirs)
+
+    private val oldDatabaseDir = createTempDirectory("otter-old-database").toFile()
+    private val oldDbFile = oldDatabaseDir.resolve("content.sqlite")
+        .apply { deleteOnExit() }
+
+    private val schemaFile = File(javaClass.classLoader.getResource("sql/AppDatabaseSchema_v8.sql").file)
+    private val oldSchemaFile = File(javaClass.classLoader.getResource("sql/AppDatabaseSchema0.sql").file)
+
+    private val directoryProviderMock = mock<DirectoryProvider> {
+        on { databaseDirectory } doReturn databaseDir
+        on { getUserDataDirectory(any()) } doReturn oldDatabaseDir
+    }
+
+    @After
+    fun cleanUp() {
+        databaseDir.deleteRecursively()
+        oldDatabaseDir.deleteRecursively()
+    }
+
+    @Test
+    fun `handle multiple databases exist`() {
+        setUpDatabases()
+
+        Assert.assertTrue(oldDbFile.exists())
+        Assert.assertFalse(databaseArchiveDir.list().any())
+
+        AppDatabase(databaseFile, directoryProviderMock)
+
+        Assert.assertFalse(oldDbFile.exists())
+        Assert.assertTrue(databaseArchiveDir.list().any())
+    }
+
+    private fun setUpDatabases() {
+        JooqTestConfiguration.createDatabase(databaseFile.absolutePath, schemaFile)
+        JooqTestConfiguration.createDatabase(oldDbFile.absolutePath, oldSchemaFile)
+            .dsl()
+            .use {
+                it.fetch("""INSERT INTO "installed_entity" VALUES ('DATABASE',1);""")
+            }
+
+        Assert.assertTrue(databaseFile.exists() && databaseFile.length() > 0)
+        Assert.assertEquals(8, getDatabaseVersion(databaseFile))
+        Assert.assertTrue(oldDbFile.exists() && oldDbFile.length() > 0)
+        Assert.assertEquals(1, getDatabaseVersion(oldDbFile))
+    }
+
+    private fun getDatabaseVersion(dbFile: File): Int {
+        val sqLiteDataSource = SQLiteDataSource()
+        sqLiteDataSource.url = "jdbc:sqlite:${dbFile.path}"
+        sqLiteDataSource.config.toProperties().setProperty("foreign_keys", "true")
+
+        DSL.using(sqLiteDataSource, SQLDialect.SQLITE).use { _dsl ->
+            return _dsl
+                .select()
+                .from(InstalledEntity.INSTALLED_ENTITY)
+                .where(InstalledEntity.INSTALLED_ENTITY.NAME.eq(DATABASE_INSTALLABLE_NAME))
+                .fetchSingle()
+                .getValue(InstalledEntity.INSTALLED_ENTITY.VERSION)
+        }
+    }
+}
