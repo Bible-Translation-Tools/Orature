431fe7e44c4f54c196ea9519475788b1299bd1d2
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2022-08-18 15:21:34 -0500

    Database migration from multiple locations (#655)
    * handle multiple databases between builds
    
    * fix tests
    
    * bug
    
    * test when multiple db exist in different locations
    
    * add test and refactor
    
    * bug fix
    
    * add test for newer database version than installed
    
    * fix failed tests
    
    * rename & add license
    
    * wip
    
    * refactor to platform
    
    * license
    
    * fix bug
    
    * rename
    
    * refactor & cleanup
    
    * remove import
    
    * change return value to null

---
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/database/DatabaseInitializer.kt
@@ -0,0 +1,54 @@
+package org.wycliffeassociates.otter.jvm.workbookapp.persistence.database
+
+import org.wycliffeassociates.otter.common.persistence.IDirectoryProvider
+import java.io.File
+import java.nio.file.Files
+
+const val DB_FILE_NAME = "app_db_sqlite"
+const val OLD_DB_FILE_NAME = "content.sqlite"
+
+class DatabaseInitializer(
+    private val directoryProvider: IDirectoryProvider
+) {
+
+    fun initialize() {
+        val databaseFile = directoryProvider.databaseDirectory
+            .resolve(DB_FILE_NAME)
+        val oldDbFile = directoryProvider.getUserDataDirectory()
+            .resolve(OLD_DB_FILE_NAME)
+        val oldDbExist = oldDbFile.exists() && oldDbFile.length() > 0
+        val currentDbExists = databaseFile.exists() && databaseFile.length() > 0
+
+        when {
+            oldDbExist && currentDbExists -> {
+                archiveDb(databaseFile)
+                Files.move(oldDbFile.toPath(), databaseFile.toPath())
+            }
+            oldDbExist -> {
+                Files.move(oldDbFile.toPath(), databaseFile.toPath())
+            }
+            currentDbExists -> {
+                val existingDbVersion = AppDatabase.getDatabaseVersion(databaseFile)
+                if (existingDbVersion == null || existingDbVersion > SCHEMA_VERSION) {
+                    archiveDb(databaseFile)
+                }
+            }
+        }
+    }
+
+    private fun archiveDb(dbFile: File) {
+        val archive = directoryProvider.databaseDirectory
+            .resolve("archive")
+            .let {
+                it.mkdirs()
+                it.resolve(
+                    "app_db-${System.currentTimeMillis()}.sqlite"
+                )
+            }
+
+        Files.move(
+            dbFile.toPath(),
+            archive.toPath()
+        )
+    }
+}
