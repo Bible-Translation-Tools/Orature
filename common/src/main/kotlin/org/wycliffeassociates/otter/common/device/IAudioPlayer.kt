00b584933b8fc5619960b8b04995c4f85a56dcaa
anonymouswalker <hoanganhtran1998@gmail.com>
2022-10-11 13:31:02 -0500

    doc for IAudioPlayer

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/device/IAudioPlayer.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/device/IAudioPlayer.kt
@@ -19,12 +19,13 @@
 package org.wycliffeassociates.otter.common.device
 
 import org.wycliffeassociates.otter.common.audio.AudioFileReader
+import java.io.Closeable
 import java.io.File
 
 /**
  * A player which interacts with the audio.
  */
-interface IAudioPlayer {
+interface IAudioPlayer : Closeable {
     val frameStart: Int
     val frameEnd: Int
     fun addEventListener(listener: IAudioPlayerListener)
@@ -36,20 +37,92 @@ interface IAudioPlayer {
         }))
     }
 
+    /**
+     * Loads the audio file into the player. This will open the file for reading.
+     *
+     * @param file the target audio file
+     */
     fun load(file: File)
+
+    /**
+     * Loads part of the audio file into the player. This will open the file for reading.
+     *
+     * @param file the target audio file
+     */
     fun loadSection(file: File, frameStart: Int, frameEnd: Int)
+
+    /**
+     * Returns the reader of the audio player.
+     */
     fun getAudioReader(): AudioFileReader?
+
+    /**
+     * Changes the playback speed of the player.
+     */
     fun changeRate(rate: Double)
+
+    /**
+     * Starts playing the audio.
+     */
     fun play()
+
+    /**
+     * Pauses the playback.
+     */
     fun pause()
+
+    /**
+     * Toggles between play() and pause().
+     */
     fun toggle()
+
+    /**
+     * Stops and resets the playback.
+     */
     fun stop()
-    fun close()
+
+    /**
+     * Closes the current player. This method must be called
+     * after using the player. Otherwise, it may leak the resource as the file
+     * was held opened.
+     */
+    override fun close()
+
+    /**
+     * Releases the player and clean up resources. This may include any player
+     * that is currently playing.
+     */
     fun release()
+
+    /**
+     * Jumps to a specific location of the audio.
+     *
+     * @param position the target location (in frame)
+     */
     fun seek(position: Int)
+
+    /**
+     * Returns the playing status of the player.
+     */
     fun isPlaying(): Boolean
+
+    /**
+     * Return the duration of the audio in frames (total frame number).
+     */
     fun getDurationInFrames(): Int
+
+    /**
+     * Returns the duration of the audio in milliseconds.
+     */
     fun getDurationMs(): Int
+
+    /**
+     * Returns the current position of the playback thumb in frames.
+     */
     fun getLocationInFrames(): Int
+
+    /**
+     * Returns the current location of the playback thumb in milliseconds.
+     */
     fun getLocationMs(): Int
 }
