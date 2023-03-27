f2224110e1225d630b9c22293b5d324e8d1feef4
Joe <jsarabia1247@gmail.com>
2023-03-27 15:44:10 -0400

    Preallocate projects (#724)
    * Update text if versification matches
    
    * Update TestExistingSourceImporter.kt
    
    * Update the RC Metadata when the source is updated.
    
    * Merge text
    
    * Update MergeTextContent.kt
    
    * Update MergeTextContent.kt
    
    * Update source collection titles/sort if preallocated
    
    If a book was allocated because of versification, it may not have a title or sort number (due to lacking the localized text that would need to be in the RC).
    
    If an update then adds this book to the RC, the collection data should be updated to reflect this.
    
    * Update derivative collections when a RC is updated
    
    * Update IProjectReader.kt
    
    * Better naming and documentation
    
    * Update NewSourceImporter.kt
    
    * Add Test for MergeTextContent
    
    * Test Exising Source Import will overwrite with the same versification.
    
    * Update TestExistingSourceImporter.kt
    
    * Update TestExistingSourceImporter.kt

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/project/importer/ExistingSourceImporter.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/project/importer/ExistingSourceImporter.kt
@@ -2,27 +2,36 @@ package org.wycliffeassociates.otter.common.domain.project.importer
 
 import io.reactivex.Single
 import org.slf4j.LoggerFactory
+import org.wycliffeassociates.otter.common.collections.OtterTree
+import org.wycliffeassociates.otter.common.data.primitives.CollectionOrContent
 import org.wycliffeassociates.otter.common.data.primitives.ResourceMetadata
 import org.wycliffeassociates.otter.common.domain.project.ImportProjectUseCase
 import org.wycliffeassociates.otter.common.domain.resourcecontainer.DeleteResourceContainer
 import org.wycliffeassociates.otter.common.domain.resourcecontainer.DeleteResult
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.ImportException
 import org.wycliffeassociates.otter.common.domain.resourcecontainer.ImportResult
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.IProjectReader
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.project.IZipEntryTreeBuilder
 import org.wycliffeassociates.otter.common.domain.resourcecontainer.projectimportexport.MediaMerge
+import org.wycliffeassociates.otter.common.domain.resourcecontainer.projectimportexport.MergeTextContent
 import org.wycliffeassociates.otter.common.persistence.IDirectoryProvider
+import org.wycliffeassociates.otter.common.persistence.repositories.IResourceContainerRepository
 import org.wycliffeassociates.otter.common.persistence.repositories.IResourceMetadataRepository
 import org.wycliffeassociates.resourcecontainer.ResourceContainer
 import java.io.File
+import java.io.IOException
 import javax.inject.Inject
 
 class ExistingSourceImporter @Inject constructor(
     directoryProvider: IDirectoryProvider,
     private val resourceMetadataRepository: IResourceMetadataRepository,
+    private val resourceContainerRepository: IResourceContainerRepository,
+    private val zipEntryTreeBuilder: IZipEntryTreeBuilder,
     private val deleteUseCase: DeleteResourceContainer,
     private val importUseCase: ImportProjectUseCase
 ) : RCImporter(directoryProvider, resourceMetadataRepository) {
 
     private val logger = LoggerFactory.getLogger(this.javaClass)
-
     override fun import(
         file: File,
         callback: ProjectImporterCallback?,
@@ -32,29 +41,89 @@ class ExistingSourceImporter @Inject constructor(
             ?: return super.passToNextImporter(file, callback, options)
 
         var sameVersion: Boolean
-        ResourceContainer.load(file).use { rc ->
-            sameVersion = (rc.manifest.dublinCore.version == existingSource.version)
+        var sameVersification: Boolean
+
+        try {
+            ResourceContainer.load(file).use { rc ->
+                sameVersion = (rc.manifest.dublinCore.version == existingSource.version)
+                ResourceContainer.load(existingSource.path).use { existingRC ->
+                    val sourceVersification = rc.manifest.projects.firstOrNull()?.versification
+                    val existingVersification = existingRC.manifest.projects.firstOrNull()?.versification
+                    sameVersification = sourceVersification == existingVersification
+                }
+            }
+        } catch (e: IOException) {
+            logger.error("Error loading RC", e)
+            return Single.just(ImportResult.LOAD_RC_ERROR)
         }
 
         return if (sameVersion) {
+            logger.info("RC ${file.name} already imported, merging media...")
             mergeMedia(file, existingSource.path)
+        } else if (sameVersification) {
+            logger.info("RC ${file.name} already imported but uses the same versification, updating source...")
+            updateSource(existingSource, file).blockingGet()
+            mergeMedia(file, existingSource.path)
+            mergeText(file, existingSource.path)
         } else {
             // existing resource has a different version, confirms overwrite/delete
+            logger.info("RC ${file.name} already imported, but with a different version and different versification.")
+            logger.info("Requesting user input to overwrite/delete existing source...")
             val confirmDelete = callback?.onRequestUserInput()
                 ?.blockingGet()
                 ?.confirmed ?: true
 
             when {
-                !confirmDelete -> Single.just(ImportResult.ABORTED)
+                !confirmDelete -> {
+                    logger.info("User chose to abort import.")
+                    Single.just(ImportResult.ABORTED)
+                }
+
                 deleteUseCase.deleteSync(existingSource.path) == DeleteResult.SUCCESS -> {
                     // re-import the file after deleting the existing source
+                    logger.info("Deleted existing source, re-importing ${file.name}...")
                     importUseCase.import(file)
                 }
+
                 else -> {
+                    logger.info("User chose to delete existing source, but delete failed.")
                     Single.just(ImportResult.DEPENDENCY_CONSTRAINT)
                 }
             }
         }
+    }
+
+    private fun updateSource(metadata: ResourceMetadata, file: File): Single<ImportResult> {
+        return Single
+            .fromCallable {
+                try {
+                    ResourceContainer.load(file).use { rc ->
+                        val tree = try {
+                            IProjectReader.constructContainerTree(rc, zipEntryTreeBuilder)
+                        } catch (e: ImportException) {
+                            logger.error("Error constructing container tree, file: $file", e)
+                            return@fromCallable ImportResult.FAILED
+                        }
+                        return@fromCallable resourceContainerRepository.updateContent(
+                            rc,
+                            tree
+                        ).map {
+                            if (it == ImportResult.SUCCESS) {
+                                resourceMetadataRepository.update(metadata, rc).blockingAwait()
+                            }
+                            it
+                        }.map {
+                            if (it == ImportResult.SUCCESS) {
+                                return@map updateCollections(rc, tree).blockingGet()
+                            }
+                            it
+                        }.blockingGet()
+                    }
+                } catch (e: IOException) {
+                    logger.error("Error loading RC", e)
+                    return@fromCallable ImportResult.LOAD_RC_ERROR
+                }
+            }
     }
 
     fun mergeMedia(
@@ -76,6 +145,38 @@ class ExistingSourceImporter @Inject constructor(
                 ImportResult.FAILED
             }
     }
+
+    fun mergeText(
+        newRC: File,
+        existingRC: File
+    ): Single<ImportResult> {
+        logger.info("RC already imported, merging text...")
+        return Single
+            .fromCallable {
+                MergeTextContent.merge(
+                    ResourceContainer.load(newRC),
+                    ResourceContainer.load(existingRC)
+                )
+                logger.info("Merge text completed.")
+                ImportResult.SUCCESS
+            }
+            .onErrorReturn {
+                logger.error("Merge text failed!", it)
+                ImportResult.FAILED
+            }
+    }
+
+    private fun updateCollections(
+        container: ResourceContainer,
+        tree: OtterTree<CollectionOrContent>
+    ): Single<ImportResult> {
+        return resourceContainerRepository
+            .updateCollectionTitles(
+                container,
+                tree
+            )
+    }
+
 
     private fun findExistingResourceMetadata(file: File): ResourceMetadata? {
         ResourceContainer.load(file, true).use { rc ->
