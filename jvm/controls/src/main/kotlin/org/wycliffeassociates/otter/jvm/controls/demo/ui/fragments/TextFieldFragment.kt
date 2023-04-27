1460500d42b8ad0ac48926f0c3f101728403133a
anonymouswalker <hoanganhtran1998@gmail.com>
2023-04-27 16:08:45 -0500

    remove unused

--- jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/demo/ui/fragments/TextFieldFragment.kt
+++ jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/demo/ui/fragments/TextFieldFragment.kt
@@ -1,7 +1,6 @@
 package org.wycliffeassociates.otter.jvm.controls.demo.ui.fragments
 
 import org.wycliffeassociates.otter.jvm.controls.bar.searchBar
-import org.wycliffeassociates.otter.jvm.controls.demo.ui.components.HeaderWithSearchBar
 import org.wycliffeassociates.otter.jvm.controls.styles.tryImportStylesheet
 import tornadofx.*
 
