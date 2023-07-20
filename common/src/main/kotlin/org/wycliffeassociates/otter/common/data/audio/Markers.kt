17d5815d2f01af5f6c3cba2d05ff51f1c89bc1f9
Joe <jsarabia1247@gmail.com>
2023-07-20 15:52:29 -0400

    Update Markers.kt

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/data/audio/Markers.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/data/audio/Markers.kt
@@ -3,23 +3,30 @@ package org.wycliffeassociates.otter.common.data.audio
 import org.wycliffeassociates.otter.common.audio.AudioCue
 
 interface AudioMarker {
+    /**
+     * The marker label which does not contain any namespacing, most often a verse number or verse range
+     */
     val label: String
     val location: Int
 
-    fun formatMarkerText(): String
+    /**
+     * The marker label with the appropriate namespacing (such as "orature-vm-{number}"
+     */
+    val formattedLabel: String
 
     fun toCue(): AudioCue {
-        return AudioCue(location, formatMarkerText())
+        return AudioCue(location, formattedLabel)
     }
 }
 
 data class UnknownMarker(override val location: Int, override val label: String) : AudioMarker {
     constructor(cue: AudioCue) : this(cue.location, cue.label)
 
-    override fun formatMarkerText() = label
+    override val formattedLabel
+        get() = label
 
     override fun toString(): String {
-        return formatMarkerText()
+        return formattedLabel
     }
 }
 
@@ -27,18 +34,20 @@ data class VerseMarker(val start: Int, val end: Int, override val location: Int)
     override val label: String
         get() = if (end != start) "$start-$end" else "$start"
 
-    override fun formatMarkerText() = "orature-vm-${label}"
+    override val formattedLabel
+        get() = "orature-vm-${label}"
 
     override fun toString(): String {
-        return formatMarkerText()
+        return formattedLabel
     }
 }
 
 data class ChunkMarker(val chunk: Int, override val location: Int) : AudioMarker {
     override val label = "$chunk"
-    override fun formatMarkerText() = "orature-chunk-${label}"
+    override val formattedLabel
+        get() = "orature-chunk-${label}"
 
     override fun toString(): String {
-        return formatMarkerText()
+        return formattedLabel
     }
 }
