e023b22c66b902e707558ec6eb892752e54af56a
Tony <hoanganhtran1998@gmail.com>
2023-08-03 16:40:46 -0400

    wip

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/dev/ChunkingDemoView.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/dev/ChunkingDemoView.kt
@@ -3,7 +3,11 @@ package org.wycliffeassociates.otter.jvm.workbookapp.ui.dev
 import javafx.beans.property.IntegerProperty
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.scene.layout.VBox
+import org.kordamp.ikonli.javafx.FontIcon
+import org.kordamp.ikonli.materialdesign.MaterialDesign
 import org.wycliffeassociates.otter.jvm.controls.styles.tryImportStylesheet
+import org.wycliffeassociates.otter.jvm.utils.bindSingleChild
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.components.grid.chunkGrid
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.model.ChunkViewData
 import tornadofx.*
@@ -22,10 +26,22 @@ class ChunkingDemoView : View() {
     )
 
     override val root = vbox {
-        chunkGrid(list)
+
+
+//        chunkGrid(list)
     }
 
     init {
         tryImportStylesheet("/css/chunk-item.css")
+    }
+}
+
+class ChunkStep() : VBox() {
+    init {
+        button("Blind Draft") {
+            graphic = FontIcon(MaterialDesign.MDI_HEADSET)
+        }
+
+        bindSingleChild()
     }
 }
