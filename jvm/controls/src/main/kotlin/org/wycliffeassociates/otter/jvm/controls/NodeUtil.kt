dec6fc4a2be68a8fff38610458f7b1e7e90600a5
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2023-08-24 08:11:52 -0400

    Add custom scrollbar skin in Home page (#811)
    * chunk items
    
    * refactor
    
    * refactor
    
    * wip
    
    * clean up
    
    * chunking steps
    
    * accessibility + stylings
    
    * disable unreachable steps
    
    * show/hide completed steps
    
    * show/hide completed icon
    
    * scrollable side pane
    
    * completed steps graphic
    
    * toggle collapsed pane
    
    * stretch grid items horizontally
    
    * clean up
    
    * const for grid cols
    
    * use const
    
    * use variable for status completed
    
    * update hide completed logic
    
    * custom scrollbar graphic & styling
    
    * style for scroll bar arrows
    
    * refactor
    
    * refactor
    
    * comment
    
    * Merge branch 'Orature-2' into aw-custom-scrollbar
    
    * adjust size
    
    * Merge branch 'Orature-2' into aw-custom-scrollbar
    
    * add scrollbar to scroll pane in home

--- jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/NodeUtil.kt
+++ jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/NodeUtil.kt
@@ -3,6 +3,7 @@ package org.wycliffeassociates.otter.jvm.controls
 import javafx.geometry.Orientation
 import javafx.scene.Parent
 import javafx.scene.control.ScrollBar
+import javafx.scene.layout.StackPane
 import org.kordamp.ikonli.javafx.FontIcon
 import org.kordamp.ikonli.material.Material
 import tornadofx.*
@@ -18,13 +19,17 @@ fun Parent.customizeScrollbarSkin() {
     val scrollBars = lookupAll(".scroll-bar")
     scrollBars
         .mapNotNull { it as? ScrollBar }
-        .forEach {
-            val thumb = it.lookup(".thumb")
-            thumb?.add(
-                FontIcon(Material.DRAG_INDICATOR).apply {
-                    addClass("thumb-icon")
-                    if (it.orientation == Orientation.HORIZONTAL) rotate = 90.0
+        .forEach { bar ->
+            val thumb = bar.lookup(".thumb")
+            (thumb as? StackPane)?.let { t ->
+                if (t.children.size == 0) {
+                    t.add(
+                        FontIcon(Material.DRAG_INDICATOR).apply {
+                            addClass("thumb-icon")
+                            if (bar.orientation == Orientation.HORIZONTAL) rotate = 90.0
+                        }
+                    )
                 }
-            )
+            }
         }
 }
