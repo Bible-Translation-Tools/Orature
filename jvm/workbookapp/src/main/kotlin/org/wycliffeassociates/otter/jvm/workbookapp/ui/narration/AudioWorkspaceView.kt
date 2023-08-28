1eb64ba618f67521c218ec437c038d3e71bb3c9a
Darrell Hill <89662191+darrellcolehill@users.noreply.github.com>
2023-08-28 16:32:17 -0400

    Moved logic to "draw" verse markers to AudioWorkspaceView
    Doing this in preparation to separate the ScrollPane area from the area to be used to show VerseMarkers.

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/narration/AudioWorkspaceView.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/narration/AudioWorkspaceView.kt
@@ -1,20 +1,31 @@
 package org.wycliffeassociates.otter.jvm.workbookapp.ui.narration
 
+import javafx.application.Platform
+import javafx.beans.binding.Bindings
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleDoubleProperty
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.collections.ObservableList
+import javafx.scene.canvas.Canvas
+import javafx.scene.canvas.GraphicsContext
+import javafx.scene.control.Button
 import org.slf4j.LoggerFactory
 import org.wycliffeassociates.otter.common.data.audio.VerseMarker
+import org.wycliffeassociates.otter.jvm.controls.model.framesToPixels
 import org.wycliffeassociates.otter.jvm.controls.model.pixelsToFrames
+import org.wycliffeassociates.otter.jvm.controls.waveform.Drawable
 import org.wycliffeassociates.otter.jvm.controls.waveform.VolumeBar
+import org.wycliffeassociates.otter.jvm.workbookapp.ui.narration.markers.VerseMarkerControl
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.narration.markers.VerseMarkersLayer
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.narration.waveform.ExistingAndIncomingAudioRenderer
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.narration.waveform.Waveform
 import org.wycliffeassociates.otter.jvm.workbookapp.ui.narration.waveform.WaveformLayer
 import tornadofx.*
 
-class AudioWorkspaceView : View() {
+class AudioWorkspaceView : View(), Drawable {
     private val viewModel: AudioWorkspaceViewModel by inject()
-    var narrationWaveformLayer = WaveformLayer()
+    private var narrationWaveformLayer = WaveformLayer()
+    val layerWidthProperty = SimpleDoubleProperty(0.0)
 
     override val root =
         stackpane {
@@ -24,7 +35,6 @@ class AudioWorkspaceView : View() {
                 // TODO: Don't do this initialization here, or in this file. It is awkward.
                 viewModel.isWaveformRendererInitialized.addListener {_, old, new ->
                     if(new == true) {
-
                         waveform = Waveform(viewModel.waveformRenderer!!)
                         waveform!!.isRecordingProperty.bind(viewModel.isRecordingProperty)
                         volumeBar = VolumeBar(viewModel.waveformRenderer!!.incomingAudioStream)
@@ -32,7 +42,6 @@ class AudioWorkspaceView : View() {
 
                         waveform?.heightProperty?.bind(this.heightProperty())
                         waveform?.widthProperty?.bind(this.widthProperty())
-                        canvasFragment.drawableProperty.set(waveform)
 
                         volumeBarCanavsFragment.drawableProperty.set(volumeBar)
                         volumeBarCanavsFragment.isDrawingProperty.set(true)
@@ -45,6 +54,26 @@ class AudioWorkspaceView : View() {
                 isRecordingProperty.bind(viewModel.isRecordingProperty)
                 markers.bind(viewModel.mockRecordedVerseMarkers) { it }
                 rightOffset.bind(viewModel.pxFromIncomingAudio)
+                verseMarkersControls.bind(viewModel.verseMarkersControls) { it }
+
+                layerWidthProperty.bind(this.widthProperty())
+
+                // Updates the VerseMarkerControl list when the mockRecorderverseMarkers changes
+                viewModel.mockRecordedVerseMarkers.onChange {
+                    viewModel.verseMarkersControls.clear()
+
+                    for(i in 0 until viewModel.mockRecordedVerseMarkers.size) {
+                        val newVerseMarkerControl = VerseMarkerControl()
+                        newVerseMarkerControl.verseProperty.set(viewModel.mockRecordedVerseMarkers[i])
+                        newVerseMarkerControl.verseIndexProperty.set(i)
+                        newVerseMarkerControl.labelProperty.set((i+1).toString())
+
+                        newVerseMarkerControl.isRecordingProperty.bind(isRecordingProperty)
+
+                        viewModel.verseMarkersControls.add(newVerseMarkerControl)
+                    }
+                }
+
 
                 // Updates the audioFilePosition property when the scrollbar position changes
                 scrollBarPositionProperty.addListener { _, old, new ->
@@ -55,10 +84,52 @@ class AudioWorkspaceView : View() {
 
         }
 
+    val rangeOfAudioToShowStart = Bindings.createIntegerBinding(
+        {
+            return@createIntegerBinding viewModel.audioFilePositionProperty.value + pixelsToFrames((viewModel.pxFromIncomingAudio.value.toDouble()))
+        }, // TODO: finish
+        viewModel.audioFilePositionProperty, viewModel.pxFromIncomingAudio
+    )
+    val rangeOfAudioToShowEnd = Bindings.createIntegerBinding(
+        {
+            (rangeOfAudioToShowStart.value + pixelsToFrames(layerWidthProperty.value))
+        },
+        layerWidthProperty, rangeOfAudioToShowStart
+    )
+
+    fun updateVerseMarkersInFrameRange(start: Int, end: Int) {
+        Platform.runLater {
+            println("===== start frame: ${start}, end frame: ${end} =====")
+            for (i in 0 until viewModel.verseMarkersControls.size) {
+                val currentVerseMarkerControl = viewModel.verseMarkersControls[i]
+                if (currentVerseMarkerControl.verseProperty.value.location in start..end) {
+
+                    println("showing ${currentVerseMarkerControl.verseProperty.value.label} at frame position: ${currentVerseMarkerControl.verseProperty.value.location }")
+                    currentVerseMarkerControl.visibleProperty().set(true)
+
+                    // TODO: update all markers position on the
+
+                } else {
+                    currentVerseMarkerControl.visibleProperty().set(false)
+                }
+            }
+        }
+    }
+
+    override fun draw(context: GraphicsContext, canvas: Canvas){
+        println("drawing waveform")
+        narrationWaveformLayer.waveform?.draw(context, canvas)
+
+        // Updating verse markers X position here
+        println("Updating verse markers positions")
+        updateVerseMarkersInFrameRange(rangeOfAudioToShowStart.value, rangeOfAudioToShowEnd.value)
+
+    }
 
     override fun onDock() {
         super.onDock()
         viewModel.onDock()
+        narrationWaveformLayer.canvasFragment.drawableProperty.set(this)
     }
 
     override fun onUndock() {
@@ -81,6 +152,7 @@ class AudioWorkspaceViewModel : ViewModel() {
     var audioFilePositionProperty = SimpleIntegerProperty(0)
     var waveformRenderer : ExistingAndIncomingAudioRenderer? = null
     var isWaveformRendererInitialized = SimpleBooleanProperty(false)
+    val verseMarkersControls: ObservableList<VerseMarkerControl> = observableListOf()
 
     fun onDock() {
 
--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/narration/markers/VerseMarkersLayer.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/narration/markers/VerseMarkersLayer.kt
@@ -1,8 +1,5 @@
 package org.wycliffeassociates.otter.jvm.workbookapp.ui.narration.markers
 
-import com.sun.glass.ui.Screen
-import javafx.animation.KeyFrame
-import javafx.animation.Timeline
 import javafx.application.Platform
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleDoubleProperty
@@ -12,14 +9,11 @@ import javafx.collections.ObservableList
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.StackPane
 import org.wycliffeassociates.otter.common.data.audio.VerseMarker
-import org.wycliffeassociates.otter.jvm.controls.model.SECONDS_ON_SCREEN
 import org.wycliffeassociates.otter.jvm.controls.model.framesToPixels
 import org.wycliffeassociates.otter.jvm.controls.model.pixelsToFrames
 import org.wycliffeassociates.otter.jvm.controls.styles.tryImportStylesheet
 import tornadofx.*
-import javafx.util.Duration
 import javafx.beans.binding.Bindings
-import javafx.beans.binding.DoubleBinding
 import javafx.collections.ListChangeListener
 
 /**
@@ -33,7 +27,6 @@ class VerseMarkersLayer : StackPane() {
 
     val isRecordingProperty = SimpleBooleanProperty()
     val markers = observableListOf<VerseMarker>()
-    val markersSubset = observableListOf<VerseMarker>()
     val rightOffset = SimpleIntegerProperty(0) // corresponds to pixels from incoming audio
 
     // gets the last verseMarker's location and sets that to the total frames
@@ -42,64 +35,16 @@ class VerseMarkersLayer : StackPane() {
 
     private val layerWidthProperty = SimpleDoubleProperty()
 
-    // TODO: Examine if I need to update this because framesToPixels uses the total screen size, and does not max out at 1920.
     private val markersTotalWidthProperty = totalFramesProperty.doubleBinding { it?.let {
         framesToPixels(it.toInt()).toDouble() } ?: 1.0
     }
 
-
     // Stores the relative start/end positions in the audio file that are being shown.
-    val scrollBarPositionProperty = SimpleDoubleProperty(0.0) // TODO: mulitply this by the width of the content window
-    val rangeOfAudioToShowStart = Bindings.createIntegerBinding(
-        {
-            return@createIntegerBinding pixelsToFrames((scrollBarPositionProperty.value + rightOffset.value))
-        }, // TODO: finish
-        rightOffset, scrollBarPositionProperty
-    )
-    val rangeOfAudioToShowEnd = Bindings.createIntegerBinding(
-        {
-            (rangeOfAudioToShowStart.value + pixelsToFrames(layerWidthProperty.value))
-        },
-        layerWidthProperty, rangeOfAudioToShowStart
-    )
-    val verseMarkersControlsInView: ObservableList<VerseMarkerControl> = observableListOf()
-
-    fun getVerseMarkersInFrameRange(start: Int, end: Int) {
-        println("start: ${start}, end: ${end}")
-        Platform.runLater {
-            for (i in 0 until markers.size) {
-                if (markers[i].location in start..end) {
-                    println("showing ${markers[i].label} at frame position: ${markers[i].location}")
-
-                    if (!markersSubset.contains(markers[i])) {
-                        markersSubset.add(markers[i])
-                    }
-                } else {
-                    if (markersSubset.contains(markers[i])) {
-                        markersSubset.remove(markers[i])
-                    }
-                }
-            }
-        }
-    }
+    val scrollBarPositionProperty = SimpleDoubleProperty(0.0)
+    var verseMarkersControls: ObservableList<VerseMarkerControl> = observableListOf()
 
 
     init {
-//        rangeOfAudioToShowEnd.onChange {
-//            getVerseMarkersInFrameRange(rangeOfAudioToShowStart.value, rangeOfAudioToShowEnd.value)
-//        }
-//
-//        rangeOfAudioToShowStart.onChange {
-//            getVerseMarkersInFrameRange(rangeOfAudioToShowStart.value, rangeOfAudioToShowEnd.value)
-//        }
-
-
-        markersSubset.addListener(ListChangeListener<VerseMarker> { change ->
-            println("===== Showing markers in view =====")
-            for(i in 0 until markersSubset.size) {
-                println("${markersSubset[i].location}")
-            }
-        })
 
         maxWidth = 1920.0 // TODO: fix to account for arrows of scrollbar
 
@@ -118,16 +63,6 @@ class VerseMarkersLayer : StackPane() {
                 hvalue += it / markersTotalWidthProperty.value
             }
 
-//            hvalueProperty().addListener { _, old, newValue ->
-//                val contentWidth = content.prefWidth(-1.0) // Get the width of the content
-//                val viewportWidth = viewportBounds.width // Get the width of the visible area
-//
-//                val totalWidth = contentWidth - viewportWidth // Calculate the total width
-//                println("Total width including content: $totalWidth")
-//                println("Scrolled pixels: ${newValue.toDouble() * markersTotalWidthProperty.value}")
-//            }
-
-
             hbox {
                 isFitToHeight = true
                 var scrollOldPos = 0.0
@@ -142,7 +77,6 @@ class VerseMarkersLayer : StackPane() {
                 setOnMouseDragged { event ->
                     val point = localToParent(event.x, event.y)
                     scrollDelta = scrollOldPos - point.x
-//                    println("point: ${point}, scrollDelta: ${scrollDelta}")
                     onScrollProperty.value?.invoke(scrollDelta)
                 }
 
@@ -150,34 +84,30 @@ class VerseMarkersLayer : StackPane() {
                     val contentWidth = markersTotalWidthProperty.value // Get the width of the content
                     val scrollbarPositionRatio : Double = hvalueProperty().value * contentWidth
 
-//                    println("Scrollbar position ratio: $scrollbarPositionRatio (out of $contentWidth)")
                     scrollBarPositionProperty.set(scrollbarPositionRatio)
                 }
-
+                totalFramesProperty.addListener { _, old, new ->
+                    this.minWidth = framesToPixels(totalFramesProperty.value).toDouble()
+                    this.maxWidth = framesToPixels(totalFramesProperty.value).toDouble()
+                }
                 anchorpane {
                     totalFramesProperty.addListener { _, old, new ->
                         this.minWidth = framesToPixels(totalFramesProperty.value).toDouble()
                         this.maxWidth = framesToPixels(totalFramesProperty.value).toDouble()
                     }
-                    bindChildren(markers) { verse ->
 
-                        val verseLabel = getVerseLabel(verse)
-                        val prevVerse = getPrevVerse(verse)
-                        val nextVerse = getNextVerse(verse)
+
+                    bindChildren(verseMarkersControls) { verseMarkerControl ->
+
+                        val prevVerse = getPrevVerse(verseMarkerControl.verseProperty.value)
+                        val nextVerse = getNextVerse(verseMarkerControl.verseProperty.value)
                         val previousMarkerPosition = framesToPixels(prevVerse.location)
                         val nextMarkerPosition = framesToPixels(nextVerse.location)
 
-                        val currentMarkerPosition = verse.location
+                        val currentMarkerPosition = verseMarkerControl.verseProperty.value.location
                         val endPosInPixels = framesToPixels(currentMarkerPosition)
 
-                        VerseMarkerControl().apply {
-
-                            // add node to list
-                            verseProperty.set(verse)
-                            verseIndexProperty.set(markers.indexOf(verse))
-                            labelProperty.set(verseLabel)
-
-                            isRecordingProperty.bind(this@VerseMarkersLayer.isRecordingProperty)
+                        verseMarkerControl.apply {
 
                             val dragTarget = dragAreaProperty.value
 
@@ -212,8 +142,8 @@ class VerseMarkersLayer : StackPane() {
                                     val frameDelta = pixelsToFrames(delta)
                                     // TODO: I need to update the relative and actual location of the marker.
                                     // Pretty sure that this is done by firing a VerseMarkerAction, but not sure
-//                                    println("frameDelta: ${frameDelta}, delta: ${delta}")
-                                    FX.eventbus.fire(NarrationMarkerChangedEvent(markers.indexOf(verse), frameDelta))
+                                    println("frameDelta: ${frameDelta}, delta: ${delta}")
+                                    FX.eventbus.fire(NarrationMarkerChangedEvent(markers.indexOf(verseMarkerControl.verseProperty.value), frameDelta))
                                 }
                                 event.consume()
                             }
@@ -221,24 +151,9 @@ class VerseMarkersLayer : StackPane() {
                             anchorpaneConstraints {
                                 topAnchor = 0.0
                                 bottomAnchor = 0.0
-                                leftAnchor = endPosInPixels - MARKER_OFFSET//endPosInPixels - MARKER_OFFSET
+                                leftAnchor = endPosInPixels - MARKER_OFFSET
                             }
 
-                            // Manually shifts all verse markers (that are in view) to the left
-                            // NOTE: this seems sketchy because I am not freeing up listeners.
-                            // I am not sure how these are freed up, but if they are not freed up properly,
-                            // then I am creating a memory leak everytime I change markersSubset.
-//                            rightOffset.addListener{_, old, new ->
-//                                    if(new == 0) {
-//                                        AnchorPane.setLeftAnchor(this,
-//                                            framesToPixels(this.verseProperty.value.location).toDouble() - MARKER_OFFSET
-//                                        )
-//                                    } else {
-//                                        val currentLeftAnchor = AnchorPane.getLeftAnchor(this) ?: 0.0
-//                                        val newLeftAnchor = currentLeftAnchor - (maxOf(0, new.toInt() - old.toInt()))
-//                                        AnchorPane.setLeftAnchor(this, newLeftAnchor)
-//                                    }
-//                            }
                         }
                     }
                 }
@@ -248,15 +163,6 @@ class VerseMarkersLayer : StackPane() {
 
     private fun setOnLayerScroll(op: (Double) -> Unit) {
         onScrollProperty.set(op)
-    }
-
-    private fun absolutePositionToRelative(verse: VerseMarker): Pair<Int, Int> {
-        val index = markers.indexOf(verse)
-        val prevVerses = markers.slice(0 until index)
-        val relStart = prevVerses.sumOf { it.end - it.start }
-        val relEnd = relStart + (verse.end - verse.start)
-
-        return Pair(relStart, relEnd)
     }
 
     private fun getVerseLabel(verse: VerseMarker): String {
