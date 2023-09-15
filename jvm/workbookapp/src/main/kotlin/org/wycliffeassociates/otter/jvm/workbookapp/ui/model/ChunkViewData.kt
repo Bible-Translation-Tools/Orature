103c20d64bec03956e78701eef1aac2c55e6e8e9
anonymouswalker <hoanganhtran1998@gmail.com>
2023-09-15 16:40:38 -0400

    revert chunk items css - chapter page

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/model/ChunkViewData.kt
+++
@@ -1,6 +0,0 @@
-package org.wycliffeassociates.otter.jvm.workbookapp.ui.model
-
-import javafx.beans.property.BooleanProperty
-import javafx.beans.property.IntegerProperty
-
-class ChunkViewData(val number: Int, val completedProperty: BooleanProperty, val selectedChunkProperty: IntegerProperty)
