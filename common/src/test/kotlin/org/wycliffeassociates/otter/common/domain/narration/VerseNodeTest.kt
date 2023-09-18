779a1e90275a611885e2d8d0d2a4e5bf9752849d
Darrell Hill <89662191+darrellcolehill@users.noreply.github.com>
2023-09-18 14:06:06 -0400

    Refactored
    Removed unnecessary comments and accounted for inclusive end

--- common/src/test/kotlin/org/wycliffeassociates/otter/common/domain/narration/VerseNodeTest.kt
+++ common/src/test/kotlin/org/wycliffeassociates/otter/common/domain/narration/VerseNodeTest.kt
@@ -259,7 +259,6 @@ class VerseNodeTest {
         expectedSectorsTaken.add(1000.. 1999)
         expectedSectorsTaken.add(2000 .. 2999)
 
-        // TODO: update takeFramesFromStart to account for inclusive ends
         Assert.assertTrue(sectorsTaken.equals(expectedSectorsTaken))
 
         val expectedVerseNodeSectors = mutableListOf<IntRange>()
@@ -284,7 +283,6 @@ class VerseNodeTest {
         expectedSectorsTaken.add(2000 .. 2999)
         expectedSectorsTaken.add(3000 .. 3499)
 
-        // TODO: update takeFramesFromStart to account for inclusive ends
         Assert.assertTrue(sectorsTaken.equals(expectedSectorsTaken))
 
         val expectedVerseNodeSectors = mutableListOf<IntRange>()
@@ -382,7 +380,6 @@ class VerseNodeTest {
         expectedSectorsTaken.add(2000 .. 2999)
         expectedSectorsTaken.add(3000 .. 3999)
 
-        // TODO: update takeFramesFromEnd to account for inclusive ends
         Assert.assertTrue(sectorsTaken.equals(expectedSectorsTaken))
 
         val expectedVerseNodeSectors = mutableListOf<IntRange>()
@@ -610,7 +607,6 @@ class VerseNodeTest {
 
 
         try {
-            // TODO: update framesToPosition to account for inclusive end
             Assert.assertEquals(2500, verseNode.framesToPosition(6499))
         } catch (ise: IndexOutOfBoundsException) {
             Assert.fail("Not expecting IndexOutOfBoundsException")
@@ -663,7 +659,6 @@ class VerseNodeTest {
 
         try {
             val absoluteFrame = verseNode.absoluteFrameFromOffset(1500)
-            // TODO: update absolgeFrameFromOffset to account for inclusive end
             Assert.assertEquals(5499, absoluteFrame)
         } catch (ise: IndexOutOfBoundsException) {
             Assert.fail("Not expecting IndexOutOfBoundsException")
@@ -744,7 +739,6 @@ class VerseNodeTest {
 
         val sectorsFromOffset = verseNode.getSectorsFromOffset(1000, 300)
         Assert.assertEquals(1, sectorsFromOffset.size)
-        // TODO: update getSectorsFromOffset to account for inclusive ends
         Assert.assertEquals(1000 .. 1299, sectorsFromOffset.first())
 
     }
@@ -760,10 +754,9 @@ class VerseNodeTest {
         val verseNode = VerseNode(0,0, true, verseMarker, sectors)
 
         val sectorsFromOffset = verseNode.getSectorsFromOffset(1000, 1300)
-        // TODO: This error seems to be partially caused by not updating the "start" value for each iteration in the for loop.
         Assert.assertEquals(2, sectorsFromOffset.size)
         Assert.assertEquals(1000 .. 1999, sectorsFromOffset[0])
-        Assert.assertEquals(5000 .. 5300, sectorsFromOffset[1])
+        Assert.assertEquals(5000 .. 5299, sectorsFromOffset[1])
     }
 
     @Test
@@ -779,7 +772,6 @@ class VerseNodeTest {
         val sectorsFromOffset = verseNode.getSectorsFromOffset(1000, 4000)
         Assert.assertEquals(3, sectorsFromOffset.size)
         Assert.assertEquals(1000 .. 1999, sectorsFromOffset[0])
-        // TODO: This error seems to be partially caused by not updating the "start" value for each iteration in the for loop.
         Assert.assertEquals(5000 .. 5999, sectorsFromOffset[1])
         Assert.assertEquals(8000 .. 8999, sectorsFromOffset[2])
     }
@@ -796,10 +788,9 @@ class VerseNodeTest {
         val verseNode = VerseNode(0,0, true, verseMarker, sectors)
 
         val sectorsFromOffset = verseNode.getSectorsFromOffset(1500, 1000)
-        // TODO: This error seems to be partially caused by not updating the "start" value for each iteration in the for loop.
         Assert.assertEquals(2, sectorsFromOffset.size)
         Assert.assertEquals(1500 .. 1999, sectorsFromOffset[0])
-        Assert.assertEquals(5000 .. 5500, sectorsFromOffset[1])
+        Assert.assertEquals(5000 .. 5499, sectorsFromOffset[1])
     }
 }
 
