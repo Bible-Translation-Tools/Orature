e812ff968eabf1021828523b3a3eded0d9804df0
Tony <hoanganhtran1998@gmail.com>
2023-09-26 16:22:07 -0400

    text

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/model/ChunkingStep.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/ui/model/ChunkingStep.kt
@@ -3,7 +3,7 @@ package org.wycliffeassociates.otter.jvm.workbookapp.ui.model
 enum class ChunkingStep(val titleKey: String) {
     CONSUME_AND_VERBALIZE("consume_and_verbalize"),
     CHUNKING("chunking"),
-    BLIND_DRAFT("blind_draft"),
+    BLIND_DRAFT("blind_draft_and_self_edit"),
     PEER_EDIT("peer_edit"),
     KEYWORD_CHECK("keyword_check"),
     VERSE_CHECK("verse_check")
