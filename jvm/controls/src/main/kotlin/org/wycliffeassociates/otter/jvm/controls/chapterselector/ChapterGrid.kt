fb5ffec51c206b1100e9668cebf6f60e74c2fe93
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-10-30 15:43:54 -0400

    Reverted back to a normal list of Chapters and refactor

--- jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/chapterselector/ChapterGrid.kt
+++ jvm/controls/src/main/kotlin/org/wycliffeassociates/otter/jvm/controls/chapterselector/ChapterGrid.kt
@@ -12,46 +12,48 @@ import tornadofx.*
 
 private const val GRID_COLUMNS = 5
 
-class ChapterGrid(list: ObservableList<ChapterGridItemData>) : GridPane() {
+class ChapterGrid(val list: List<ChapterGridItemData>) : GridPane() {
 
     init {
         addClass("chapter-grid")
-        list.onChange {
-            Platform.runLater {
-                children.clear()
-            }
-            list.forEachIndexed { index, chapter ->
-                val node = StackPane().apply {
-                    button(chapter.number.toString()) {
-                        addClass(
-                            "btn", "btn--secondary", "btn--borderless", "chapter-grid__btn"
-                        )
-                        prefWidthProperty().bind(
-                            this@ChapterGrid.widthProperty().divide(GRID_COLUMNS.toDouble())
-                        )
-                        setOnAction {
-                            selectChapter(index)
-                        }
-                    }
-                    hbox {
-                        addClass("chapter-grid__icon-alignment-box")
-                        add(
-                            FontIcon(MaterialDesign.MDI_CHECK_CIRCLE).apply { addClass("complete-icon") }
-                        )
-                        isMouseTransparent = true
-                        isPickOnBounds = false
-                        visibleWhen { chapter.completedProperty }
-                        managedWhen(visibleProperty())
-                    }
-                }
-                runLater {
-                    this.add(node, index % GRID_COLUMNS, index / GRID_COLUMNS)
-                }
-            }
-        }
+        addChaptersToGrid()
     }
 
-    fun selectChapter(chapterIndex: Int) {
+    private fun selectChapter(chapterIndex: Int) {
         FX.eventbus.fire(OpenChapterEvent(chapterIndex))
+    }
+
+    fun updateChapterList() {
+        children.clear()
+        addChaptersToGrid()
+    }
+
+    private fun addChaptersToGrid() {
+        list.forEachIndexed { index, chapter ->
+            val node = StackPane().apply {
+                button(chapter.number.toString()) {
+                    addClass(
+                        "btn", "btn--secondary", "btn--borderless", "chapter-grid__btn"
+                    )
+                    prefWidthProperty().bind(
+                        this@ChapterGrid.widthProperty().divide(GRID_COLUMNS.toDouble())
+                    )
+                    setOnAction {
+                        selectChapter(index)
+                    }
+                }
+                hbox {
+                    addClass("chapter-grid__icon-alignment-box")
+                    add(
+                        FontIcon(MaterialDesign.MDI_CHECK_CIRCLE).apply { addClass("complete-icon") }
+                    )
+                    isMouseTransparent = true
+                    isPickOnBounds = false
+                    visibleWhen { chapter.completedProperty }
+                    managedWhen(visibleProperty())
+                }
+            }
+            this.add(node, index % GRID_COLUMNS, index / GRID_COLUMNS)
+        }
     }
 }
