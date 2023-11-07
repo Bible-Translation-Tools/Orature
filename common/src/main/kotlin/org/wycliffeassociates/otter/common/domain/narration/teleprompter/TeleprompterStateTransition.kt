34a18a5d7435ae52387a04545dc57b2c1167feed
Joe <jsarabia1247@gmail.com>
2023-11-07 08:17:13 -0500

    Enable Record-Again/Record on Save action

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/teleprompter/TeleprompterStateTransition.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/teleprompter/TeleprompterStateTransition.kt
@@ -140,6 +140,11 @@ object SaveRecordingAction {
         if (index != 0) {
             for (i in 0 until index) {
                 contexts[i].restore()
+                if (contexts[i].state.type == TeleprompterItemState.RECORD_AGAIN_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD_AGAIN)
+                } else if (contexts[i].state.type == TeleprompterItemState.RECORD_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD)
+                }
             }
         }
 
@@ -148,6 +153,11 @@ object SaveRecordingAction {
         if (index < contexts.lastIndex) {
             for (i in index + 1..contexts.lastIndex) {
                 contexts[i].restore()
+                if (contexts[i].state.type == TeleprompterItemState.RECORD_AGAIN_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD_AGAIN)
+                } else if (contexts[i].state.type == TeleprompterItemState.RECORD_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD)
+                }
             }
         }
     }
