51c8d76c576bef2d1202d8cdade28d79e7348303
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-12-06 17:50:45 -0500

    Initial NarrationTakeModifier
    Still need to finish modifyAudioData

---
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationTakeModifier.kt
@@ -0,0 +1,42 @@
+package org.wycliffeassociates.otter.common.domain.narration
+
+import org.wycliffeassociates.otter.common.audio.DEFAULT_BITS_PER_SAMPLE
+import org.wycliffeassociates.otter.common.audio.DEFAULT_CHANNELS
+import org.wycliffeassociates.otter.common.audio.DEFAULT_SAMPLE_RATE
+import org.wycliffeassociates.otter.common.data.audio.AudioMarker
+import org.wycliffeassociates.otter.common.data.audio.BookMarker
+import org.wycliffeassociates.otter.common.data.audio.ChapterMarker
+import org.wycliffeassociates.otter.common.data.audio.VerseMarker
+import org.wycliffeassociates.otter.common.data.workbook.Take
+import org.wycliffeassociates.otter.common.domain.audio.OratureAudioFile
+
+class NarrationTakeModifier(take: Take) {
+
+    lateinit var audioFile : OratureAudioFile
+
+    init {
+        audioFile = OratureAudioFile(take.file, DEFAULT_CHANNELS, DEFAULT_SAMPLE_RATE, DEFAULT_BITS_PER_SAMPLE)
+    }
+
+    fun modifyAudioData() {
+        // TODO: finish
+    }
+
+    fun modifyMetaData(markers: List<AudioMarker>) {
+        clearNarrationMarkers()
+        addNarrationMarkers(markers)
+        audioFile.update()
+    }
+
+    private fun clearNarrationMarkers() {
+        audioFile.clearMarkersOfType<VerseMarker>()
+        audioFile.clearMarkersOfType<ChapterMarker>()
+        audioFile.clearMarkersOfType<BookMarker>()
+    }
+
+    private fun addNarrationMarkers(markers: List<AudioMarker>) {
+        markers.forEach { marker ->
+            audioFile.addMarker(marker)
+        }
+    }
+}
