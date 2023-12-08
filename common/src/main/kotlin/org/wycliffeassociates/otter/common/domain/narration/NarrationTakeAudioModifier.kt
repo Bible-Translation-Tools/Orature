89a2445c01ac1de5542042ae3b27612561ca90c2
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-12-08 14:40:32 -0500

    Generates Wav file header when createNewAudioFile is true

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationTakeAudioModifier.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationTakeAudioModifier.kt
@@ -1,6 +1,9 @@
 package org.wycliffeassociates.otter.common.domain.narration
 
 import org.wycliffeassociates.otter.common.audio.AudioFileReader
+import org.wycliffeassociates.otter.common.audio.DEFAULT_BITS_PER_SAMPLE
+import org.wycliffeassociates.otter.common.audio.DEFAULT_CHANNELS
+import org.wycliffeassociates.otter.common.audio.DEFAULT_SAMPLE_RATE
 import org.wycliffeassociates.otter.common.data.audio.AudioMarker
 import org.wycliffeassociates.otter.common.data.audio.BookMarker
 import org.wycliffeassociates.otter.common.data.audio.ChapterMarker
@@ -8,9 +11,18 @@ import org.wycliffeassociates.otter.common.data.audio.VerseMarker
 import org.wycliffeassociates.otter.common.data.workbook.Take
 import org.wycliffeassociates.otter.common.domain.audio.OratureAudioFile
 
-class NarrationTakeAudioModifier(val take: Take) {
+class NarrationTakeAudioModifier(val take: Take, createNewAudioFile: Boolean = false) {
 
-    private var audioFile: OratureAudioFile = OratureAudioFile(take.file)
+    private var audioFile: OratureAudioFile = if (createNewAudioFile) {
+        OratureAudioFile(
+            take.file,
+            DEFAULT_CHANNELS,
+            DEFAULT_SAMPLE_RATE,
+            DEFAULT_BITS_PER_SAMPLE
+        )
+    } else {
+        OratureAudioFile(take.file)
+    }
 
     fun modifyAudioData(reader: AudioFileReader, markers: List<AudioMarker>) {
         bounceAudio(
