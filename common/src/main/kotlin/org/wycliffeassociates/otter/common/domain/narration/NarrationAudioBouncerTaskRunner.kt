428965d14d86b848e16ecafe0a342455a4c23873
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-12-27 10:12:33 -0500

    Switch from Disposable to Completable
    Does not interrupt bounceAudio as abruptly

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationAudioBouncerTaskRunner.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationAudioBouncerTaskRunner.kt
@@ -1,40 +1,44 @@
 package org.wycliffeassociates.otter.common.domain.narration
 
 import io.reactivex.Completable
+import io.reactivex.CompletableEmitter
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.PublishSubject
+import org.slf4j.LoggerFactory
 import org.wycliffeassociates.otter.common.audio.AudioFileReader
 import org.wycliffeassociates.otter.common.data.audio.AudioMarker
 import org.wycliffeassociates.otter.common.domain.audio.AudioBouncer
 import java.io.File
-import java.nio.channels.ClosedByInterruptException
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit
 
 object NarrationAudioBouncerTaskRunner {
 
+    private val logger = LoggerFactory.getLogger(NarrationAudioBouncerTaskRunner::class.java)
+
     private val audioBouncer = AudioBouncer()
     private var currentTask: Disposable? = null
+    private lateinit var emitter: CompletableEmitter
+
+    private fun cancelPreviousTask() {
+        if (currentTask != null) {
+            audioBouncer.interrupt()
+            emitter.onComplete()
+        }
+    }
 
     private fun bounceAudioTask(file: File, reader: AudioFileReader, markers: List<AudioMarker>): Completable {
-        return Completable.fromAction {
-            audioBouncer.bounceAudio(file, reader, markers)
-        }
-            .doOnDispose {
-                audioBouncer.interrupt()
-                println("Cancelled!")
+        return Completable
+            .create { emitter ->
+                audioBouncer.bounceAudio(file, reader, markers)
+                this.emitter = emitter
             }
             .doOnComplete {
-                println("Successfully completed!")
+                logger.info("Successfully completed!")
             }
             .subscribeOn(Schedulers.io())
     }
 
     fun bounce(file: File, reader: AudioFileReader, markers: List<AudioMarker>) {
-        currentTask?.dispose()
+        cancelPreviousTask()
         currentTask = bounceAudioTask(file, reader, markers).subscribe()
     }
 
