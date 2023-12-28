acdedb22a4dc65bb8707b1fd254d810313779314
Tony <hoanganhtran1998@gmail.com>
2023-12-28 12:03:42 -0500

    test delete undoable

--- common/src/test/kotlin/org/wycliffeassociates/otter/common/domain/translation/TranslationUndoableActionTest.kt
+++ common/src/test/kotlin/org/wycliffeassociates/otter/common/domain/translation/TranslationUndoableActionTest.kt
@@ -1,8 +1,10 @@
 package org.wycliffeassociates.otter.common.domain.translation
 
 import com.jakewharton.rxrelay2.BehaviorRelay
+import com.nhaarman.mockitokotlin2.spy
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
 import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
@@ -44,15 +46,42 @@ class TranslationUndoableActionTest {
 
         verify(exactly = 1) { audio.insertTake(take) }
 
-        recordAction.undo() // delete the current take and select the old take
+        recordAction.undo() // "delete" the current take and select the old take
 
         verify(exactly = 1) { deleteTimestampRelay.accept(any()) }
         verify(exactly = 1) { audio.selectTake(oldSelectedTake) }
+        verify(exactly = 0) { audio.selectTake(take) }
 
         recordAction.redo() // bring back the original take and re-select it
 
         verify { audio.getAllTakes() }
-        verify { deleteTimestampRelay.accept(DateHolder.empty) }
+        verify(exactly = 1) { deleteTimestampRelay.accept(DateHolder.empty) }
         verify(exactly = 1) { audio.selectTake(take) }
+    }
+
+    @Test
+    fun testDeleteAction() {
+        val deleteAction = TranslationTakeDeleteAction(chunk, take, true) { _, _ ->
+            /* mock callback */
+        }
+        val deleteTimestampRelay = mockk<BehaviorRelay<DateHolder>>()
+
+        every { deleteTimestampRelay.accept(any()) } returns Unit
+        every { take.deletedTimestamp } returns deleteTimestampRelay
+
+        deleteAction.execute()
+
+        verify(exactly = 1) { deleteTimestampRelay.accept(any()) }
+        verify(exactly = 0) { audio.selectTake(take) }
+
+        deleteAction.undo() // bring back the take and re-select it if it has been selected
+
+        verify { audio.getAllTakes() }
+        verify(exactly = 1) { deleteTimestampRelay.accept(DateHolder.empty) }
+        verify(exactly = 1) { audio.selectTake(take) }
+
+        deleteAction.redo()
+
+        verify(exactly = 3) { deleteTimestampRelay.accept(any()) }
     }
 }
