3887f0b2c535e9b49cf84fea11a4c76f955cffb2
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-01-02 11:30:56 -0500

    Cleaned up marker creation/modification

--- common/src/test/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationTakeModifierTest.kt
+++ common/src/test/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationTakeModifierTest.kt
@@ -12,6 +12,8 @@ import org.wycliffeassociates.otter.common.audio.DEFAULT_SAMPLE_RATE
 import org.wycliffeassociates.otter.common.data.audio.*
 import org.wycliffeassociates.otter.common.data.primitives.Marker
 import org.wycliffeassociates.otter.common.data.primitives.MimeType
+import org.wycliffeassociates.otter.common.data.workbook.Book
+import org.wycliffeassociates.otter.common.data.workbook.Chapter
 import org.wycliffeassociates.otter.common.data.workbook.Take
 import org.wycliffeassociates.otter.common.domain.audio.OratureAudioFile
 import org.wycliffeassociates.otter.common.persistence.IDirectoryProvider
@@ -27,6 +29,8 @@ val tempAudioFile = File(testDirWithAudio, "tempAudio.pcm")
 
 class NarrationTakeModifierTest {
     lateinit var chapterTake: Take
+    val testBookSlug = "gen"
+    val testChapterNumber = 1
 
     private fun mockTake(): Take {
         return mockk<Take> {
@@ -102,19 +106,67 @@ class NarrationTakeModifierTest {
         audioFile.update()
     }
 
+    private fun makeAudioMarkers(secondsOfAudioPerMarker: Int, numberOfMarkers: Int): List<AudioMarker> {
+        val audioMarkers = mutableListOf<AudioMarker>()
+        var verseCount = 1
+        for (i in 0 until numberOfMarkers) {
+            if (i == 0) {
+                audioMarkers.add(BookMarker(testBookSlug, 0))
+            } else if (i == 1) {
+                audioMarkers.add(
+                    ChapterMarker(testChapterNumber, secondsOfAudioPerMarker * DEFAULT_SAMPLE_RATE * i)
+                )
+            } else {
+                audioMarkers.add(
+                    VerseMarker(verseCount, verseCount, secondsOfAudioPerMarker * DEFAULT_SAMPLE_RATE * i)
+                )
+                verseCount++
+            }
+        }
+        return audioMarkers
+    }
 
+    private fun moveAudioMarker(markers: List<AudioMarker>, markerToMove: AudioMarker, delta: Int): List<AudioMarker> {
+        val newAudioMarkers = mutableListOf<AudioMarker>()
+
+        markers.forEach {
+            if (it == markerToMove) {
+                val newLocation = it.location + delta
+                if (it::class == BookMarker::class) {
+                    newAudioMarkers.add(BookMarker(testBookSlug, newLocation))
+                } else if (it::class == ChapterMarker::class) {
+                    newAudioMarkers.add(ChapterMarker(testChapterNumber, newLocation))
+                } else if (it::class == VerseMarker::class) {
+                    val verseStart = (it as VerseMarker).start
+                    val verseEnd = it.end
+                    newAudioMarkers.add(VerseMarker(verseStart, verseEnd, newLocation))
+                } else {
+                    // no op, only considering book, chapter, and verse markers
+                }
+            } else {
+                newAudioMarkers.add(it)
+            }
+        }
+
+        return newAudioMarkers
+    }
+
+    private fun moveAllAudioMarkers(markers: List<AudioMarker>, delta: Int): List<AudioMarker> {
+        var newMarkers = markers
+        newMarkers.forEach { audioMarker ->
+            newMarkers = moveAudioMarker(newMarkers, audioMarker, delta)
+        }
+        return newMarkers
+    }
+
+
+    // Test that Wav file metadata is updated properly when keeping the same markers, but changing their location
     @Test
     fun `modifyMetadata with same amount of markers, same labels, and with different locations`() {
         val takeModifier = NarrationTakeAudioModifier(chapterTake)
         val secondsOfAudio = 10
 
-        val originalAudioMarkers = listOf<AudioMarker>(
-            BookMarker("gen", 0),
-            ChapterMarker(1, 44100),
-            VerseMarker(1, 1, 88200),
-            VerseMarker(2, 2, 132300),
-            VerseMarker(3, 3, 176400)
-        )
+        val originalAudioMarkers = makeAudioMarkers(1, 5)
 
         makeTestChapterTakeRecording(
             takeModifier.audioFile,
@@ -130,13 +182,8 @@ class NarrationTakeModifierTest {
             Assert.assertTrue(originalAudioMarkers.map { it.toCue() }.contains(cue))
         }
 
-        val newAudioMarkers = listOf<AudioMarker>(
-            BookMarker("gen", 100),
-            ChapterMarker(1, 44500),
-            VerseMarker(1, 1, 88600),
-            VerseMarker(2, 2, 132700),
-            VerseMarker(3, 3, 176800)
-        )
+        // Simulates moving each marker by 400 samples
+        val newAudioMarkers = moveAllAudioMarkers(originalAudioMarkers, 400)
 
         takeModifier.modifyMetaData(newAudioMarkers)
 
@@ -149,18 +196,14 @@ class NarrationTakeModifierTest {
         }
     }
 
+    // Test that Wav file metadata is updated properly when using a different amount of markers with different locations
     @Test
     fun `modifyMetadata with different amount of markers, same labels, and with different locations`() {
         val takeModifier = NarrationTakeAudioModifier(chapterTake)
         val secondsOfAudio = 10
 
-        val originalAudioMarkers = listOf<AudioMarker>(
-            BookMarker("gen", 0),
-            ChapterMarker(1, 44100),
-            VerseMarker(1, 1, 88200),
-            VerseMarker(2, 2, 132300),
-            VerseMarker(3, 3, 176400)
-        )
+        val originalAudioMarkers = makeAudioMarkers(1, 5)
+        val markersToKeep = 3
 
         makeTestChapterTakeRecording(
             takeModifier.audioFile,
@@ -176,16 +219,13 @@ class NarrationTakeModifierTest {
             Assert.assertTrue(originalAudioMarkers.map { it.toCue() }.contains(cue))
         }
 
-        val newAudioMarkers = listOf<AudioMarker>(
-            BookMarker("gen", 100),
-            ChapterMarker(1, 44200),
-            VerseMarker(1, 1, 88300),
-        )
+        // Simulates moving all markers by 100 and removing all but the first markersToKeep number of markers
+        val newAudioMarkers = moveAllAudioMarkers(originalAudioMarkers, 100).subList(0, markersToKeep)
 
         takeModifier.modifyMetaData(newAudioMarkers)
 
         // Verify that we have the expected amount of cues / Markers in the Wav file
-        Assert.assertEquals(newAudioMarkers.size, takeModifier.audioFile.getCues().size)
+        Assert.assertEquals(markersToKeep, takeModifier.audioFile.getCues().size)
 
         // Verify that we have the expected cues specified by newAudioMarkers
         takeModifier.audioFile.getCues().forEachIndexed { idx, cue ->
@@ -198,13 +238,7 @@ class NarrationTakeModifierTest {
         val takeModifier = NarrationTakeAudioModifier(chapterTake)
         val secondsOfAudio = 10
 
-        val originalAudioMarkers = listOf(
-            BookMarker("gen", 0),
-            ChapterMarker(1, 44100),
-            VerseMarker(1, 1, 88200),
-            VerseMarker(2, 2, 132300),
-            VerseMarker(3, 3, 176400)
-        )
+        val originalAudioMarkers = makeAudioMarkers(1, 5)
 
         // Initializes audio and adds markers
         makeTestChapterTakeRecording(
