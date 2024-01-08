c443507e9542e1b523b16e972585883970843945
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-01-08 14:11:05 -0500

    Added observable to indicate when the modifier is busy
    isBusy is true when the audio is bouncing or when the markers are being updated

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationTakeAudioModifier.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/NarrationTakeAudioModifier.kt
@@ -1,22 +1,29 @@
 package org.wycliffeassociates.otter.common.domain.narration
 
+import io.reactivex.Observable
 import org.wycliffeassociates.otter.common.audio.AudioFileReader
-import org.wycliffeassociates.otter.common.audio.DEFAULT_BITS_PER_SAMPLE
-import org.wycliffeassociates.otter.common.audio.DEFAULT_CHANNELS
-import org.wycliffeassociates.otter.common.audio.DEFAULT_SAMPLE_RATE
-import org.wycliffeassociates.otter.common.data.audio.*
+import org.wycliffeassociates.otter.common.data.audio.AudioMarker
 import org.wycliffeassociates.otter.common.data.workbook.Take
-import org.wycliffeassociates.otter.common.domain.audio.OratureAudioFile
 
 class NarrationTakeAudioModifier(val take: Take) {
 
-    private val audioBounceTaskRunner = NarrationAudioBouncerTaskRunner
+    private val audioBounceTaskRunner = NarrationTakeModifierTaskRunner
+    lateinit var isBusy: Observable<Boolean>
+
+    init {
+        isBusy = Observable.combineLatest(
+            audioBounceTaskRunner.audioBouncerBusy.startWith(false),
+            audioBounceTaskRunner.markerUpdateBusy.startWith(false)
+        ) { audioBounceBusy, markerUpdateBusy ->
+            audioBounceBusy || markerUpdateBusy
+        }
+            .distinctUntilChanged()
+    }
 
     fun modifyAudioData(reader: AudioFileReader, markers: List<AudioMarker>) {
         // May need to examine the history. The reader is a shared resource, and we may need to get a snapshot of it
         // when bouncing the audio.
         audioBounceTaskRunner.bounce(take.file, reader, markers)
-
     }
 
     fun modifyMetadata(markers: List<AudioMarker>) {
