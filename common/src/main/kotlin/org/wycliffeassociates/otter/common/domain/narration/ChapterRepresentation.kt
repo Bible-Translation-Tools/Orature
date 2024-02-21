f652831be8485a0cf8a8106cb1e02f0e78bacc05
Tony <hoanganhtran1998@gmail.com>
2024-02-20 20:22:46 -0500

    avoid flooding the log

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/ChapterRepresentation.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/ChapterRepresentation.kt
@@ -133,7 +133,7 @@ internal class ChapterRepresentation(
                 nodes.getOrNull(idx)?.let { totalVerses[idx] = it }
             }
         } catch (e: JsonMappingException) {
-            logger.error("Error in loadFromSerializedVerses: ${e.message}")
+            // file is empty, no-op
         }
     }
 
