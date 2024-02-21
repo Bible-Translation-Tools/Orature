72ef279a37855351870496a04dad583ed4d350ba
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2024-02-21 11:08:28 -0500

    Fix Back-up project missing selected takes (#1047)
    * retrieve chapter chunks correctly when exporting
    
    solved the issue when chunk selected takes are not properly written out to the file.
    
    * update selected.txt when leaving Blind Draft

--- common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/ProjectFilesAccessor.kt
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/resourcecontainer/project/ProjectFilesAccessor.kt
@@ -702,7 +702,16 @@ class ProjectFilesAccessor(
 
         val bookElements: Observable<BookElement> = when {
             chaptersOnly -> chapters.cast()
-            else -> chapters.concatMap { chapter -> chapter.children.startWith(chapter) }
+            else -> {
+                chapters.flatMap { chapter ->
+                    chapter.chunks
+                        .take(1)
+                        .flatMap {
+                            it.toObservable().cast<BookElement>()
+                        }
+                        .startWith(chapter)
+                }
+            }
         }
 
         return bookElements
