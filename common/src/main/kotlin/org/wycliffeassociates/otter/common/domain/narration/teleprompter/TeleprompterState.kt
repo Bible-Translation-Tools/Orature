1a953951ea3d686e3daf6cfa5908f5d6196bcc79
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-03-27 09:17:55 -0400

    restored the teleprompter package from dev

---
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/teleprompter/TeleprompterState.kt
@@ -0,0 +1,273 @@
+/**
+ * Copyright (C) 2020-2024 Wycliffe Associates
+ *
+ * This file is part of Orature.
+ *
+ * Orature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Orature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Orature.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.wycliffeassociates.otter.common.domain.narration.teleprompter
+
+enum class TeleprompterItemState {
+    BEGIN_RECORDING,
+    RECORD,
+    RECORDING_PAUSED,
+    RECORD_DISABLED,
+    RECORD_ACTIVE,
+    RECORD_AGAIN,
+    RECORD_AGAIN_ACTIVE,
+    RECORD_AGAIN_PAUSED,
+    RECORD_AGAIN_DISABLED
+}
+
+interface TeleprompterState {
+    val type: TeleprompterItemState
+    val validStateTransitions: Set<TeleprompterItemState>
+    val disabledState: TeleprompterState
+
+    fun changeState(request: TeleprompterItemState): TeleprompterState
+}
+
+object BeginRecordingState : TeleprompterState {
+    override val type = TeleprompterItemState.BEGIN_RECORDING
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD,
+        TeleprompterItemState.RECORD_DISABLED
+    )
+
+    override val disabledState = RecordDisabledState
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD -> RecordState
+            TeleprompterItemState.RECORD_DISABLED -> RecordDisabledState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+}
+
+object RecordState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORD
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD,
+        TeleprompterItemState.RECORD_ACTIVE,
+        TeleprompterItemState.RECORD_DISABLED,
+    )
+
+    override val disabledState = RecordDisabledState
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD -> RecordState
+            TeleprompterItemState.RECORD_ACTIVE -> RecordActiveState
+            TeleprompterItemState.RECORD_DISABLED -> RecordDisabledState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+}
+
+object RecordDisabledState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORD_DISABLED
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD,
+        TeleprompterItemState.RECORD_ACTIVE
+    )
+
+    override val disabledState = RecordDisabledState
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD -> RecordState
+            TeleprompterItemState.RECORD_ACTIVE -> RecordActiveState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+}
+
+object RecordActiveState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORD_ACTIVE
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORDING_PAUSED,
+        TeleprompterItemState.RECORD_AGAIN,
+        TeleprompterItemState.RECORD_AGAIN_DISABLED
+    )
+
+    override val disabledState: TeleprompterState
+        get() = throw IllegalStateException("Tried to disable an active recording")
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            // NarrationTextItemState.RECORD_ACTIVE -> RecordActiveState
+            TeleprompterItemState.RECORDING_PAUSED -> RecordPausedState
+            TeleprompterItemState.RECORD_AGAIN -> RecordAgainState
+            TeleprompterItemState.RECORD_AGAIN_DISABLED -> RecordAgainDisabledState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+}
+
+object RecordPausedState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORDING_PAUSED
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD_ACTIVE,
+        TeleprompterItemState.RECORD_AGAIN,
+        TeleprompterItemState.RECORD_AGAIN_DISABLED
+    )
+
+    override val disabledState = RecordDisabledState
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD_ACTIVE -> RecordActiveState
+            TeleprompterItemState.RECORD_AGAIN -> RecordAgainState
+            TeleprompterItemState.RECORD_AGAIN_DISABLED -> RecordAgainDisabledState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+}
+
+object RecordAgainState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORD_AGAIN
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD_AGAIN_ACTIVE,
+        TeleprompterItemState.RECORD_AGAIN_DISABLED,
+    )
+
+    override val disabledState = RecordAgainDisabledState
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD_AGAIN_ACTIVE -> RecordAgainActiveState
+            TeleprompterItemState.RECORD_AGAIN_DISABLED -> RecordAgainDisabledState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+}
+
+object RecordAgainDisabledState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORD_AGAIN_DISABLED
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD_AGAIN
+    )
+
+    override val disabledState = RecordAgainDisabledState
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD_AGAIN -> RecordAgainState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+}
+
+object RecordAgainActiveState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORD_AGAIN_ACTIVE
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD_AGAIN_PAUSED,
+        TeleprompterItemState.RECORD_AGAIN
+    )
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD_AGAIN_PAUSED -> RecordAgainPausedState
+            TeleprompterItemState.RECORD_AGAIN -> RecordAgainState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+
+    override val disabledState = RecordAgainDisabledState
+}
+
+object RecordAgainPausedState : TeleprompterState {
+    override val type = TeleprompterItemState.RECORD_AGAIN_PAUSED
+
+    override val validStateTransitions = setOf(
+        TeleprompterItemState.RECORD_AGAIN_ACTIVE,
+        TeleprompterItemState.RECORD_AGAIN,
+        TeleprompterItemState.RECORD_AGAIN_DISABLED
+    )
+
+    override fun changeState(request: TeleprompterItemState): TeleprompterState {
+        if (request !in validStateTransitions) {
+            throw IllegalStateException("State: $type tried to transition to state: $request")
+        }
+
+        return when (request) {
+            TeleprompterItemState.RECORD_AGAIN_ACTIVE -> RecordAgainActiveState
+            TeleprompterItemState.RECORD_AGAIN -> RecordAgainState
+            TeleprompterItemState.RECORD_AGAIN_DISABLED -> RecordAgainDisabledState
+            else -> {
+                throw IllegalStateException("State: $type tried to transition to state: $request")
+            }
+        }
+    }
+
+    override val disabledState: TeleprompterState
+        get() = throw IllegalStateException("Tried to disable a paused re-recording")
+}
---
+++ common/src/main/kotlin/org/wycliffeassociates/otter/common/domain/narration/teleprompter/TeleprompterStateTransition.kt
@@ -0,0 +1,197 @@
+/**
+ * Copyright (C) 2020-2024 Wycliffe Associates
+ *
+ * This file is part of Orature.
+ *
+ * Orature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Orature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Orature.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.wycliffeassociates.otter.common.domain.narration.teleprompter
+
+enum class TeleprompterStateTransition {
+    RECORD,
+    PAUSE_RECORDING,
+    RESUME_RECORDING,
+    NEXT,
+    RECORD_AGAIN,
+    RESUME_RECORD_AGAIN,
+    PAUSE_RECORD_AGAIN,
+    SAVE
+}
+
+object RecordAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, index: Int) {
+        if (index !in contexts.indices) return
+
+        for (i in 0 until index) {
+            if (i != index) {
+                contexts[i].disable()
+            }
+        }
+
+        contexts[index].changeState(TeleprompterItemState.RECORD_ACTIVE)
+
+        if (index < contexts.lastIndex) {
+            var i = index + 1
+
+            while (i < contexts.size) {
+                // enable the next unrecorded node
+                if (contexts[i].state.type == TeleprompterItemState.RECORD_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD)
+                    break
+                }
+                contexts[i].disable()
+                i++
+            }
+
+            // disable the remaining nodes
+            (i until contexts.size).forEach {
+                contexts[it].disable()
+            }
+        }
+    }
+}
+
+object PauseRecordingAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, index: Int) {
+        if (index !in contexts.indices) return
+
+        if (0 != index) {
+            for (i in 0 until index) {
+                contexts[i].restore()
+                if (contexts[i].state.type == TeleprompterItemState.RECORD_AGAIN_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD_AGAIN)
+                }
+            }
+        }
+
+        contexts[index].changeState(TeleprompterItemState.RECORDING_PAUSED)
+    }
+}
+
+object ResumeRecordAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, index: Int) {
+        if (index !in contexts.indices) return
+
+        if (0 != index) {
+            for (i in 0 until index) {
+                contexts[i].disable()
+            }
+        }
+
+        contexts[index].changeState(TeleprompterItemState.RECORD_ACTIVE)
+
+        if (index < contexts.lastIndex) {
+            var i = index + 1
+
+            while (i < contexts.size) {
+                // enable the next unrecorded node
+                if (contexts[i].state.type == TeleprompterItemState.RECORD_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD)
+                    break
+                }
+                contexts[i].disable()
+                i++
+            }
+
+            // disable the remaining nodes
+            (i until contexts.size).forEach {
+                contexts[it].disable()
+            }
+        }
+    }
+}
+
+object NextVerseAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, currentIndex: Int) {
+        val wasActive = contexts[currentIndex].state.type == TeleprompterItemState.RECORD_ACTIVE
+
+        if (wasActive) {
+            contexts[currentIndex].changeState(TeleprompterItemState.RECORD_AGAIN_DISABLED)
+            contexts.firstOrNull { it.state.type == TeleprompterItemState.RECORD_DISABLED }?.changeState(TeleprompterItemState.RECORD_ACTIVE)
+        } else {
+            contexts[currentIndex].changeState(TeleprompterItemState.RECORD_AGAIN)
+            contexts.firstOrNull { it.state.type == TeleprompterItemState.RECORD_DISABLED }?.changeState(TeleprompterItemState.RECORD)
+        }
+    }
+}
+
+object RecordAgainAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, index: Int) {
+        if (index !in contexts.indices) return
+
+        if (index != 0) {
+            for (i in 0 until index) {
+                contexts[i].disable()
+            }
+        }
+
+        contexts[index].changeState(TeleprompterItemState.RECORD_AGAIN_ACTIVE)
+
+        if (index < contexts.lastIndex) {
+            for (i in index + 1..contexts.lastIndex) {
+                contexts[i].disable()
+            }
+        }
+    }
+}
+
+object PauseRecordAgainAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, index: Int) {
+        contexts[index].changeState(TeleprompterItemState.RECORD_AGAIN_PAUSED)
+    }
+}
+
+object ResumeRecordAgainAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, index: Int) {
+        if (index !in contexts.indices) return
+
+        if (0 != index) {
+            for (i in 0 until index) {
+                contexts[i].disable()
+            }
+        }
+
+        contexts[index].changeState(TeleprompterItemState.RECORD_AGAIN_ACTIVE)
+
+        // Make next item available to record
+        if (index < contexts.lastIndex) {
+            for (i in index + 1..contexts.lastIndex) {
+                contexts[i].disable()
+            }
+        }
+    }
+}
+
+object SaveRecordingAction {
+    fun apply(contexts: MutableList<TeleprompterStateContext>, index: Int) {
+        if (index !in contexts.indices) return
+
+        if (index != 0) {
+            for (i in 0 until index) {
+                contexts[i].restore()
+                if (contexts[i].state.type == TeleprompterItemState.RECORD_AGAIN_DISABLED) {
+                    contexts[i].changeState(TeleprompterItemState.RECORD_AGAIN)
+                }
+            }
+        }
+
+        contexts[index].changeState(TeleprompterItemState.RECORD_AGAIN)
+
+        if (index < contexts.lastIndex) {
+            for (i in index + 1..contexts.lastIndex) {
+                contexts[i].restore()
+            }
+        }
+    }
+}
