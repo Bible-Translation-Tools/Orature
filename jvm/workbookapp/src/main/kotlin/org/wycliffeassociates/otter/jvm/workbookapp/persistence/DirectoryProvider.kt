9de9db3adb9cae783c8a9b5864b609a303f9a628
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2025-04-03 14:27:57 -0500

    upgrade mac build to use beta of i4j

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/DirectoryProvider.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/DirectoryProvider.kt
@@ -18,7 +18,6 @@
  */
 package org.wycliffeassociates.otter.jvm.workbookapp.persistence
 
-
 import org.wycliffeassociates.otter.common.data.OratureFileFormat
 import org.wycliffeassociates.otter.common.data.primitives.Collection
 import org.wycliffeassociates.otter.common.data.primitives.ContainerType
@@ -31,6 +30,7 @@ import org.wycliffeassociates.otter.jvm.workbookapp.io.zip.NioZipFileWriter
 import org.wycliffeassociates.resourcecontainer.ResourceContainer
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.IOError
 import java.io.IOException
 import java.nio.file.FileSystems
 
@@ -266,12 +266,8 @@ class DirectoryProvider(
                 Runtime.getRuntime().exec(command)
             }
             osName.contains("MAC") -> {
-//                revealInFinder(path)
-//                val command = arrayOf("open", "-R", path)
-//                Runtime.getRuntime().exec(command)
-                println("Trying to invoke processBuilder")
-                val pb = ProcessBuilder("/usr/bin/open", "/Applications/ocenaudio.app")
-                pb.start()
+                val command = arrayOf("open", "-R", path)
+                Runtime.getRuntime().exec(command)
             }
             osName.contains("LINUX") -> {
                 val command = arrayOf("xdg-open", path)
