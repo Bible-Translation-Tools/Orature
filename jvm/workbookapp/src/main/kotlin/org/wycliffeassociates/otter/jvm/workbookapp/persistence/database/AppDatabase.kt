c7075e68ec25d7423c3774e2404c7d02391b359b
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2025-05-07 13:17:55 -0400

    Change paths in DB for sandboxed mac's (#1210)
    * change paths in for sandboxed mac's
    
    * comments and refactor
    
    * bump version for app store build
    
    Co-Authored-By: Will Kelly <67284402+wkelly17@users.noreply.github.com>

--- jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/database/AppDatabase.kt
+++ jvm/workbookapp/src/main/kotlin/org/wycliffeassociates/otter/jvm/workbookapp/persistence/database/AppDatabase.kt
@@ -62,6 +62,11 @@ class AppDatabase(
             setup()
         }
         DatabaseMigrator(directoryProvider).migrate(dsl)
+
+        val isMacOS = System.getProperty("orature.isPkgMac")
+        if (isMacOS != null) {
+            migratePathsForSandboxedMac()
+        }
     }
 
     private fun setup() {
@@ -128,6 +133,50 @@ class AppDatabase(
 
     fun close() {
         connection.close()
+    }
+
+    /**
+     * Updates the absolute paths in the database tables to the sand-boxed paths (if needed).
+     * This fixes an error when installing the new version from the App Store
+     * over the existing dmg-installed build.
+     */
+    private fun migratePathsForSandboxedMac() {
+        // dublin_core_entity
+        dsl.execute(
+            """
+                UPDATE dublin_core_entity 
+                SET path = REPLACE(path, 
+                   '/Library/Application Support/Orature/', 
+                   '/Library/Containers/org.wycliffeassociates.otter/Data/Library/Application Support/Orature/'
+                   )
+                WHERE path LIKE '%/Library/Application Support/Orature/%' 
+                AND path NOT LIKE '%/Library/Containers/%';
+            """.trimIndent()
+        )
+        // take_entity
+        dsl.execute(
+            """
+                UPDATE take_entity 
+                SET path = REPLACE(path, 
+                   '/Library/Application Support/Orature/', 
+                   '/Library/Containers/org.wycliffeassociates.otter/Data/Library/Application Support/Orature/'
+                   )
+                WHERE path LIKE '%/Library/Application Support/Orature/%' 
+                AND path NOT LIKE '%/Library/Containers/%';
+            """.trimIndent()
+        )
+        // versification_entity
+        dsl.execute(
+            """
+                UPDATE versification_entity 
+                SET path = REPLACE(path, 
+                   '/Library/Application Support/Orature/', 
+                   '/Library/Containers/org.wycliffeassociates.otter/Data/Library/Application Support/Orature/'
+                   )
+                WHERE path LIKE '%/Library/Application Support/Orature/%' 
+                AND path NOT LIKE '%/Library/Containers/%';
+            """.trimIndent()
+        )
     }
 
     companion object {
